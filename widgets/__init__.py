fb94c2d2e6c1bf671636a78ba611387d836c1997
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2013-05-13 16:04:33 +0200

    Fix minor bug

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,11 +1,13 @@
 from src.gui.config import CONF
+from src.plugins.survey.aggregates import AggregatesWidget
 
 def get_config(widget):
-    '''
+    """
     Sets some country specific parameters of the widgets
-    '''
+    TODO:seems to be unused
+    """
     country = CONF.get('simulation', 'country')
     if country == 'france':
-        if isinstance(widget, AggregateOutputWidget):
-            widget.varlist = ['irpp', 'ppe', 'af', 'cf', 'ars', 'aeeh', 'asf', 'aspa', 'aah', 'caah', 'rsa', 'aefa', 'api', 'logt']
+        if isinstance(widget, AggregatesWidget):
+            widget.varlist = ['irpp', 'ppe', 'af', 'cf', 'ars', 'aeeh', 'asf', 'aspa', 'aah', 'caah', 'rsa', 'psa', 'aefa', 'api', 'logt']
             widget.selected_vars = set(['revdisp', 'nivvie'])
