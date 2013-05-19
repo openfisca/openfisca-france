56e5227f6ae2ad492a10a0960369689a8b7b44e4
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2013-05-19 17:38:27 +0200

    Renaming totaux.xml and work for Cecilia
    The revenue decomposition trees are now in the decompositions folder of
    the countries
    The revenues types for graphics are no more in utils but in the __init__
    of the country
    graph, simulation are modified accordingly

--- __init__.py
+++ __init__.py
@@ -29,6 +29,10 @@ DATA_SOURCES_DIR = os.path.join(SRC_PATH,"countries","france","data","sources")
 
 CURRENCY = u"€"
 
+
+
+# Some variables needed by the test case graph widget
+
 REVENUES_CATEGORIES = {'superbrut' : ['salsuperbrut', 'chobrut', 'rstbrut', 'alr', 'alv',
                        'rev_cap_brut', 'fon'],
        'brut': ['salbrut', 'chobrut', 'rstbrut', 'alr', 'alv',
@@ -36,6 +40,70 @@ REVENUES_CATEGORIES = {'superbrut' : ['salsuperbrut', 'chobrut', 'rstbrut', 'alr
        'imposable' : ['sal', 'cho', 'rst', 'alr', 'alv', 'rev_cap_brut', 'fon', 'cotsoc_cap'],
        'net'      : ['salnet', 'chonet', 'rstnet', 'alr', 'alv', 'rev_cap_net', 'fon',
                       ]}        
+
+
+XAXIS_PROPERTIES = { 'sali': {
+                              'name' : 'sal',
+                              'typ_tot' : {'salsuperbrut' : 'Salaire super brut',
+                                           'salbrut': 'Salaire brut',
+                                           'sal':  'Salaire imposable',
+                                           'salnet': 'Salaire net'},
+                              'typ_tot_default' : 'sal'},
+                    'choi': {
+                             'name' : 'cho',
+                             'col_name' : 'choi', 
+                             'typ_tot' : {'chobrut': u"Chômage brut",
+                                          'cho':     u"Chômage",
+                                          'chonet':  u"Chômage net"},
+                             'typ_tot_default' : 'cho' },
+                    'rsti': {
+                             'name' : 'rst',
+                             'col_name' : 'rsti', 
+                             'typ_tot' : {'rstbrut': u"Retraite brut",
+                                          'rst':     u"Retraite",
+                                          'rstnet':  u"Retraite net"},
+                             'typ_tot_default' : 'rst'},
+                    'f2da': {
+                             'name': 'divpfl',
+                             'col_name' : 'f2da', 
+                             'typ_tot' : {'rev_cap_brut': u"Revenus des capitaux", 
+                                          'rev_cap_net': u"Revenus des capitaux nets"},
+                             'typ_tot_default' : 'rev_cap_brut'},
+                    'f2ee': {
+                             'name' : 'intpfl',
+                             'col_name' : 'f2ee', 
+                             'typ_tot' : {'rev_cap_brut': "Revenus des capitaux", 
+                                          'rev_cap_net': "Revenus des capitaux nets"},
+                             'typ_tot_default' : 'rev_cap_brut'},
+                    'f2dc': {
+                             'name' : 'divb',
+                             'col_name' : 'f2dc',
+                             'typ_tot' : {'rev_cap_brut': "Revenus des capitaux", 
+                                          'rev_cap_net': "Revenus des capitaux nets"},
+                             'typ_tot_default' : 'rev_cap_brut'},
+                    'f2tr': {
+                             'name' : 'intb',
+                             'col_name' : 'f2tr', 
+                             'typ_tot' : {'rev_cap_brut': "Revenus des capitaux", 
+                                          'rev_cap_net': "Revenus des capitaux nets"},
+                             'typ_tot_default' : 'rev_cap_brut'},
+                    'alr' : {
+                             'name' : 'alr',
+                             'col_name' : 'alr',
+                             'typ_tot' : {'pen': "Pensions"},
+                             'typ_tot_default' : 'pen'},
+                    'f6gu' : {
+                             'name' : 'f6gu',
+                             'col_name' : 'f6gu',
+                             'typ_tot' : {'pen': "Pensions"},
+                             'typ_tot_default' : 'pen'},
+                    'f4ba' : {
+                             'name' : 'f4ba',
+                             'col_name' : 'f4ba',
+                             'typ_tot' : {'fon': "Revenus fonciers"},
+                             'typ_tot_default' : 'fon'},
+                    
+                    }
 
 
 # Some variables used by other plugins
--- model/pfam.py
+++ model/pfam.py
@@ -107,7 +107,7 @@ def _af_base(af_nbenf, _P):
     'fam'
     '''
     P = _P.fam
-    bmaf = P.af.bmaf    
+    bmaf = P.af.bmaf
     # prestations familiales (brutes de crds)
     af_1enf = round(bmaf * P.af.taux.enf1, 2)
     af_2enf = round(bmaf * P.af.taux.enf2, 2)
@@ -204,6 +204,8 @@ def _asf(age, rst, isol, asf_elig, smic55, alr, _P,
     '''
     Allocation de soutien familial
     '''
+    # TODO: what is rst doing here ?
+    
     # L’ASF permet d’aider le conjoint survivant ou le parent isolé ayant la garde 
     # d’un enfant et les familles ayant à la charge effective et permanente un enfant 
     # orphelin.
