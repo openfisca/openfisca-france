0f5a54d779e1b98434421876ea08e7938fd2e0a4
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2013-06-06 17:09:07 +0100

    create an rsa_act individual
    according to legislation for PPE

--- model/irpp.py
+++ model/irpp.py
@@ -1094,12 +1094,12 @@ def _ppe_brute(ppe_elig, ppe_elig_i, ppe_rev, ppe_base, ppe_coef, ppe_coef_tp, n
     ppe_tot = (ppe_tot!=0)*max_(P.versmin, ppe_tot)
     return ppe_tot
 
-def _ppe(ppe_brute, rsa_act, _option = {'rsa_act': [VOUS, CONJ]} ):
+def _ppe(ppe_brute, rsa_act_i, _option = {'rsa_act_i': [VOUS, CONJ]} ):
     """
     PPE effectivement versé
     """
 #   On retranche le RSA activité de la PPE
 #   Dans les agrégats officiels de la DGFP, c'est la PPE brute qu'il faut comparer
-    ppe = max_(ppe_brute - rsa_act[VOUS] - rsa_act[CONJ],0)
+    ppe = max_(ppe_brute - rsa_act_i[VOUS] - rsa_act_i[CONJ],0)
     return ppe 
     
