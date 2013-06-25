79a41f102549629c3229829268aebcc8916c0daa
bouvard <laurence.bouvard@univ-amu.fr>
2013-06-25 16:15:51 +0200

    majo_rsa introduction

--- model/mini.py
+++ model/mini.py
@@ -413,6 +413,75 @@ def _rsa(rsa_socle, ra_rsa, forf_log, br_rmi, _P, _option = {'ra_rsa': [CHEF, PA
     return rsa
 
 
+def _majo_rsa(rsa_socle, agem, age, smic55, isol, forf_log, br_rmi, af_majo, rsa, _P, _option = {'age': ENFS, 'agem': ENFS, 'smic55': ENFS}):
+    ''' 
+    Cacule le montant du RSA majoré pour parent isolé
+    'fam'
+    '''   
+    P = _P    
+    rsa_socle = P.minim.rmi.rmi 
+    
+    
+#    Majoration du RSA socle, mécanisme similaire à l'API: Pour les personnes ayant la charge 
+#    d’au moins un enfant né ou à naître et se retrouvant en situation d’isolement, 
+#    le montant forfaitaire est majoré pendant 12 mois, continus ou non, dans la 
+#    limite de 18 mois à compter de la date du fait générateur de l’isolement. Le 
+#    cas échéant, la durée de majoration est prolongée jusqu’à ce que le plus jeune 
+#    enfant atteigne trois ans.          
+    
+#    Majoration pour isolement
+#    Si vous êtes parent isolé, c’est-à-dire célibataire, divorcé(e), séparé(e) ou veuf(ve) avec des enfants à charge ou enceinte, le montant forfaitaire garanti est majoré.
+#    Ce montant forfaitaire majoré est accordé à partir du mois au cours duquel survient l'un des événements suivants :
+#    - déclaration de grossesse,
+#    - naissance d'un enfant,
+#    - prise en charge d'un enfant,
+#    - séparation, veuvage,
+#    - dépôt de la demande si l’événement est antérieur.
+#
+#    Le montant forfaitaire majoré peut être accordé pendant 12 mois, continus ou discontinus, au cours d’une période de 18 mois suivant l’événement.
+#    Si votre plus jeune enfant à charge a moins de 3 ans, le montant forfaitaire majoré vous est accordé jusqu'à ses 3 ans.
+#    
+    
+    benjamin = age_en_mois_benjamin(agem)  
+    enceinte = (benjamin<0)*(benjamin>-6)
+    # TODO quel mois mettre ?
+    # TODO pas complètement exact
+    # L'allocataire perçoit la majoration :
+    # jusqu'à ce que le plus jeune enfant ait 3 ans,
+    # ou pendant 12 mois consécutifs si les enfants sont âgés de plus de 3 ans 
+    #    et s'il a présenté sa demande dans les 6 mois à partir du moment où il 
+    #    assure seul la charge de l'enfant. 
+    # TODO: API courte gens pour les gens qui ont divorcés dans l'année
+    # Le droit à l'allocation est réétudié tous les 3 mois.
+    ## Calcul de l'année et mois de naissance du benjamin 
+    
+    condition = (floor(benjamin/12) <= P.minim.rmi.majo_rsa.age-1)
+    eligib = isol*( (enceinte!=0) | (nb_enf(age, smic55, 0,P.minim.rmi.majo_rsa.age-1)>0) )*condition;
+
+    # moins de 20 ans avant inclusion dans rsa
+    # moins de 25 ans après inclusion dans rsa
+    majo1 = eligib*rsa_socle*(P.minim.rmi.majo_rsa.majo_rsa_pac0 + P.minim.rmi.majo_rsa.majo_rsa_pac_enf_sup*nb_enf(age, smic55, P.fam.af.age1,P.minim.rmi.majo_rsa.age_pac-1) )
+    rsa = (P.minim.rmi.majo_rsa.age_pac >= 25) # dummy passage au rsa majoré
+    br_api = br_rmi + af_majo*not_(rsa)
+    # On pourrait mensualiser RMI, BRrmi et forfait logement
+    majo_rsa  = max_(0, majo1 - forf_log/12 - br_api/12 - rsa/12) 
+    # L'API est exonérée de CRDS
+    return 12*majo_rsa # annualisé
+    # TODO API: temps partiel qui modifie la base ressource
+    # Cumul
+    # Cumul avec un revenu
+    # Si l'allocataire reprend une activité ou suit une formation professionnelle rémunérée, les revenus sont cumulables intégralement au cours des 3 premiers mois de reprise d'activité.
+    # Du 4e au 12e mois qui suit, le montant de l'allocation varie en fonction de la durée de l'activité ou de la formation.
+    # Durée d'activité de 78 heures ou plus par mois ou activité non salariée
+    # Lorsque la durée d'activité est de 78 heures minimum par mois, le montant de l'API perçu par l'allocataire est diminué de la totalité du salaire. Tous les revenus d'activité sont pris en compte pour le calcul de l'API, sauf si l'allocataire perçoit des revenus issus d'un contrat insertion-revenu minimum d'activité (CIRMA) ou d'un contrat d'avenir (CAV).
+    # L'allocataire peut bénéficier, sous certaines conditions :
+    # • de la prime de retour à l'emploi si son activité est d'une durée d'au moins 4 mois consécutifs, sauf s'il effectue un stage de formation professionnelle,
+    # • de la prime forfaitaire pendant 9 mois, sauf s'il exerce une activité salariée dans le cadre d'un CIRMA ou d'un CAV.
+    # Durée d'activité de moins de 78 heures par mois
+    # Lorsque la durée d'activité est inférieure à 78 heures par mois, le montant de l'API perçu par l'allocataire est diminué de la moitié du salaire.
+    # Si l'allocataire exerce une activité dans le cadre d'un CIRMA ou d'un CAV, ses revenus d'activité ne sont pas pris en compte pour le calcul de son API.
+   
+
 def _psa(api, rsa, _P):
     '''
     Prime de solidarité active (exceptionnelle, 200€ versés une fois en avril 2009)
@@ -458,15 +527,7 @@ def _rsa_act_i(rsa_act):
 
 def _crds_mini(rsa_act, _P):
     return _P.fam.af.crds*rsa_act
-        
-        
-        
-        
-        
-        
-        
-        
-        
+                
         
 def _api(agem, age, smic55, isol, forf_log, br_rmi, af_majo, rsa, _P, _option = {'age': ENFS, 'agem': ENFS, 'smic55': ENFS}):
     """
