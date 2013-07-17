55caf16277f00d0f2a3644d625913628f48e52c9
Victor Le Breton <vicversa@wanadoo.fr>
2013-07-17 19:49:42 +0200

    test_laurence sorts aggregates, debugger modified to try to get erf data
    test_laurence shows erf data, rows sorted, columns not sorted

--- data/erf/aggregates.py
+++ data/erf/aggregates.py
@@ -10,49 +10,57 @@ from src.lib.simulation import SurveySimulation
 from src.plugins.survey.aggregates import Aggregates
 from src.countries.france.data.erf.datatable import DataCollection
 from src.countries.france.data.erf import get_of2erf, get_erf2of
+import numpy as np
 
-def build_erf_aggregates(variables, year):
+def build_erf_aggregates(variables = None, year = 2006):
     """
     Fetch the relevant aggregates from erf data
     """
     country = 'france'         
     erf = DataCollection(year=year)
-    if "wprm" not in variables:
+    if variables is not None and "wprm" not in variables:
         variables.append("wprm")
     print 'Fetching aggregates from erf %s data' %str(year)
-    menage = erf.get_of_values(variables=variables, table = "erf_menage")
+    df = erf.get_of_values(variables=variables, table = "erf_menage")
     
-    print menage.columns
-    cols = []
+#     print menage.columns
+#     cols = []
 
     of2erf = get_of2erf()
     erf2of = get_erf2of()
-    for col in variables:
-        try:
-            erf_var = of2erf[col]
-        except:
-            print "coucouc"
-            erf_var = None
-        if erf_var in menage.columns:
-#                 print col, erf_var
-            cols += [erf_var]
-        else:
-            print col + " not found"
+#     for col in variables:
+#         try:
+#             erf_var = of2erf[col]
+#         except:
+#             print "coucouc"
+#             erf_var = None
+#         if erf_var in menage.columns:
+# #                 print col, erf_var
+#             cols += [erf_var]
+#         else:
+#             print col + " not found"
         
-    df = menage[cols]
+#     df = menage[cols]
     print df
     df.rename(columns = erf2of, inplace = True)
-    wprm = menage["wprm"]
+    wprm = df["wprm"]
     for col in df.columns:
-        df[col] = (df[col]*wprm).sum()/1e9
+        try:
+            df[col] = df[col].astype(np.float64)
+        except:
+            pass
+    df = df.mul(wprm, axis = 0)
+    for col in list(set(df.columns) - set(['ident', 'wprm'])):
+        try:
+            df[col] = df[col].sum()/1e9
+        except:
+            pass
 #             print col, tot
-    return df
+    return df.ix[0:1] # Aggregate so we only need 1 row
     
     
 
 
 if __name__ == '__main__':
-    # Doesnt work anymroe
-    dfs = build_erf_aggregates()
-    dfs[0] = dfs[0][:1]
-    print dfs[0].to_string()
+    df = build_erf_aggregates()
+    print df.to_string()
