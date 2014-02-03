910983ecfe77cbd3d0b578a6cab18632ce6eef3d
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-02-03 08:12:06 +0100

    Working on fonctionnaire

--- tests/test_brut_to_imposable.py
+++ tests/test_brut_to_imposable.py
@@ -22,8 +22,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
+from __future__ import division
 
+import sys
 import logging
 
 import openfisca_france
@@ -37,10 +38,15 @@ def test_case_study(year = 2013, verbose = False):
     for type_sal_category in ['prive_non_cadre', 'prive_cadre', 'public_titulaire_etat']:
 
         simulation = ScenarioSimulation()
-        simulation.set_config(year = year, reforme = False, nmen = 5, maxrev = 20000, x_axis = 'salbrut')
+        maxrev = 24000
+        simulation.set_config(year = year, reforme = False, nmen = 1, maxrev = maxrev, x_axis = 'salbrut')
         # Add husband/wife on the same tax sheet (foyer).
     #    simulation.scenario.addIndiv(1, datetime.date(1975, 1, 1), 'conj', 'part')
         simulation.scenario.indiv[0]['type_sal'] = CAT[type_sal_category]
+        if type_sal_category == 'public_titulaire_etat':
+            from openfisca_france.model.cotisations_sociales.travail import TAUX_DE_PRIME
+            simulation.scenario.indiv[0]['primes'] = TAUX_DE_PRIME * maxrev
+
         simulation.set_param()
 
         # The aefa prestation can be disabled by uncommenting the following line:
@@ -57,10 +63,11 @@ def test_case_study(year = 2013, verbose = False):
         sali = df_b2i['sal'].get_values()
         hsup = simulation.input_table.table['hsup'].get_values()
         type_sal = simulation.input_table.table['type_sal'].get_values()
+        primes = simulation.input_table.table['hsup'].get_values()
 
         defaultP = simulation.P_default
         from pandas import DataFrame
-        df_i2b = DataFrame({'sal': sali, 'salbrut' : _salbrut(sali, hsup, type_sal, defaultP)})
+        df_i2b = DataFrame({'sal': sali, 'salbrut' : _salbrut(sali, hsup, type_sal, defaultP) })
 
         if verbose:
             print df_i2b.to_string()
@@ -70,8 +77,8 @@ def test_case_study(year = 2013, verbose = False):
             test = ((df_b2i[var] - df_i2b[var]).abs() < .01).all()
 
             if not test:
-                print df_b2i.to_string()
-                print df_i2b.to_string()
+                print (df_b2i / 12).to_string()
+                print (df_i2b / 12).to_string()
 
             assert test, "difference in %s for %s" % (var, type_sal_category)
 
