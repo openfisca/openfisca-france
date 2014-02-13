3f699f1a0105c81f34b28bd8b9e2960a4ff2e56c
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-02-13 11:45:52 +0100

    Working on income inversion

--- tests/test_brut_to_imposable.py
+++ tests/test_brut_to_imposable.py
@@ -34,7 +34,7 @@ from openfisca_core.simulations import ScenarioSimulation
 
 from openfisca_france.model.cotisations_sociales.travail import CAT
 
-def test_case_study(year = 2013, verbose = False):
+def test_sal(year = 2013, verbose = False):
     '''
     Tests that _salbrut which computes "salaire brut" from "imposable" yields an amount compatbe
     with the one obtained from running openfisca satrting with a "salaire brut"
@@ -49,11 +49,7 @@ def test_case_study(year = 2013, verbose = False):
         if type_sal_category == 'public_titulaire_etat':
             from openfisca_france.model.cotisations_sociales.travail import TAUX_DE_PRIME
             simulation.scenario.indiv[0]['primes'] = TAUX_DE_PRIME * maxrev
-
         simulation.set_param()
-
-        # The aefa prestation can be disabled by uncommenting the following line:
-        # simulation.disable_prestations( ['aefa'])
         df = simulation.get_results_dataframe(index_by_code = True)
 
         from openfisca_france.model.inversion_revenus import _salbrut
@@ -85,6 +81,46 @@ def test_case_study(year = 2013, verbose = False):
             assert passed, "difference in %s for %s" % (var, type_sal_category)
 
 
+def test_cho_rst(year = 2013, verbose = False):
+    '''
+    Tests that _chobrut which computes "chômage brut" from "imposable" yields an amount compatbe
+    with the one obtained from running openfisca satrting with a "chômage brut"
+    '''
+    remplacement = {'cho' : 'chobrut', 'rst': 'rstbrut'}
+
+    for var, varbrut in remplacement.iteritems():
+        simulation = ScenarioSimulation()
+        maxrev = 24000
+        simulation.set_config(year = year, reforme = False, nmen = 11, maxrev = maxrev, x_axis = varbrut)
+        simulation.set_param()
+        df = simulation.get_results_dataframe(index_by_code = True)
+
+        from openfisca_france.model.inversion_revenus import _chobrut
+        df_b2i = df.transpose()
+        if verbose:
+            print df_b2i.to_string()
+
+            vari = df_b2i[var].get_values()
+            csg_rempl = vari * 0 + 1
+
+            defaultP = simulation.P_default
+            from pandas import DataFrame
+            df_i2b = DataFrame({var: vari, varbrut : _chobrut(vari, csg_rempl, defaultP) })
+
+            if verbose:
+                print df_i2b.to_string()
+
+
+            for variable in [var, varbrut]:
+                passed = ((df_b2i[variable] - df_i2b[variable]).abs() < .01).all()
+
+                if (not passed):
+                    print (df_b2i / 12).to_string()
+                    print (df_i2b / 12).to_string()
+
+                assert passed, "difference in %s " % (var)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level = logging.ERROR, stream = sys.stdout)
-    test_case_study(2013, verbose = True)
+    test_cho_rst(2013, verbose = True)
