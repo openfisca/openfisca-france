afe1697908f9010fc86d4a55ec941e0d69e7a066
Emmanuel Raviart <emmanuel@raviart.com>
2014-03-25 22:16:39 +0100

    Cleanup new_simulation_from_survey_data_frame.

--- openfisca_france/surveys.py
+++ openfisca_france/surveys.py
@@ -28,33 +28,9 @@ import datetime
 import numpy as np
 from openfisca_core import simulations
 
-from . import entities
 
-
-def new_simulation_from_survey_data_frame(compact_legislation = None, debug = False, survey = None, tax_benefit_system = None, year = None):
-    simulation = simulations.Simulation(
-        compact_legislation = compact_legislation,
-        date = datetime.date(year, 5, 1),
-        debug = debug,
-        tax_benefit_system = tax_benefit_system,
-        )
-
-    column_by_name = tax_benefit_system.column_by_name
-    for column_name, series in survey.iteritems():
-        assert column_name in column_by_name, column_name
-    entity_by_key_plural = simulation.entity_by_key_plural
-
-    familles = entity_by_key_plural[u'familles']
-    familles.count = familles.step_size = familles_step_size = (survey.quifam == 0).sum()
-    foyers_fiscaux = entity_by_key_plural[u'foyers_fiscaux']
-    foyers_fiscaux.count = foyers_fiscaux.step_size = foyers_fiscaux_step_size = (survey.quifoy == 0).sum()
-    individus = entity_by_key_plural[u'individus']
-    individus.count = individus.step_size = individus_step_size = len(survey)
-    menages = entity_by_key_plural[u'menages']
-    menages.count = menages.step_size = menages_step_size = (survey.quimen == 0).sum()
-
-    assert 'age' in survey.columns
-    assert 'agem' in survey.columns
+def new_simulation_from_survey_data_frame(compact_legislation = None, debug = False, survey = None,
+        tax_benefit_system = None, year = None):
     assert 'idfam' in survey.columns
     assert 'idfoy' in survey.columns
     assert 'idmen' in survey.columns
@@ -63,9 +39,29 @@ def new_simulation_from_survey_data_frame(compact_legislation = None, debug = Fa
     assert 'quifoy' in survey.columns
     assert 'quimen' in survey.columns
 
+    column_by_name = tax_benefit_system.column_by_name
+    for column_name in survey:
+        assert column_name in column_by_name, 'Unknown column "{}" in survey'.format(column_name)
+
+    simulation = simulations.Simulation(
+        compact_legislation = compact_legislation,
+        date = datetime.date(year, 5, 1),
+        debug = debug,
+        tax_benefit_system = tax_benefit_system,
+        )
+    entity_by_key_plural = simulation.entity_by_key_plural
+
+    familles = entity_by_key_plural[u'familles']
+    familles.count = familles.step_size = familles_step_size = (survey.quifam == 0).sum()
     familles.roles_count = survey['quifam'].max() + 1
-    menages.roles_count = survey['quimen'].max() + 1
+    foyers_fiscaux = entity_by_key_plural[u'foyers_fiscaux']
+    foyers_fiscaux.count = foyers_fiscaux.step_size = foyers_fiscaux_step_size = (survey.quifoy == 0).sum()
     foyers_fiscaux.roles_count = survey['quifoy'].max() + 1
+    individus = entity_by_key_plural[u'individus']
+    individus.count = individus.step_size = individus_step_size = len(survey)
+    menages = entity_by_key_plural[u'menages']
+    menages.count = menages.step_size = menages_step_size = (survey.quimen == 0).sum()
+    menages.roles_count = survey['quimen'].max() + 1
 
     for column_name, column_series in survey.iteritems():
         holder = simulation.new_holder(column_name)
@@ -118,8 +114,6 @@ def new_simulation_from_array_dict(compact_legislation = None, debug = False, ar
     menages = entity_by_key_plural[u'menages']
     menages.count = menages.step_size = menages_step_size = (array_dict['quimen'] == 0).sum()
 
-    assert 'age' in array_dict.keys()
-    assert 'agem' in array_dict.keys()
     assert 'idfam' in array_dict.keys()
     assert 'idfoy' in array_dict.keys()
     assert 'idmen' in array_dict.keys()
