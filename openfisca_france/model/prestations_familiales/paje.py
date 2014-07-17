b1b98818af2cb8b821a2cabd344be3047ff9c77a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-07-17 14:24:14 +0200

    Move paje to paje.py + clean

--- openfisca_france/model/prestations_familiales/paje.py
+++ openfisca_france/model/prestations_familiales/paje.py
@@ -28,13 +28,246 @@ from numpy import (floor, maximum as max_, logical_not as not_, logical_and as a
 from openfisca_core.accessors import law
 
 from ..input_variables.base import QUIFAM, QUIFOY
-from ..pfam import nb_enf
+from ..pfam import nb_enf, age_en_mois_benjamin
 
 CHEF = QUIFAM['chef']
 PART = QUIFAM['part']
 ENFS = [QUIFAM['enf1'], QUIFAM['enf2'], QUIFAM['enf3'], QUIFAM['enf4'], QUIFAM['enf5'], QUIFAM['enf6'], QUIFAM['enf7'], QUIFAM['enf8'], QUIFAM['enf9'], ]
 VOUS = QUIFOY['vous']
 CONJ = QUIFOY['conj']
+
+
+def _paje(paje_base, paje_nais, paje_clca, paje_clmg, paje_colca):
+    '''
+    Prestation d'accueil du jeune enfant
+    '''
+    return paje_base + paje_nais + paje_clca + paje_clmg + paje_colca
+
+
+def _paje_base(self, age_holder, af_nbenf, br_pf, isol, biact, smic55_holder, P = law.fam):
+    '''
+    Prestation d'accueil du jeune enfant - allocation de base
+    '''
+    # TODO cumul des paje si et seulement si naissance multiples
+
+    # TODO : théorie, il faut comparer les revenus de l'année n-2 à la bmaf de
+    # l'année n-2 pour déterminer l'éligibilité avec le cf_seuil. Il faudrait
+    # pouvoir déflater les revenus de l'année courante pour en tenir compte.
+    age = self.split_by_roles(age_holder, roles = ENFS)
+    smic55 = self.split_by_roles(smic55_holder, roles = ENFS)
+
+    bmaf = P.af.bmaf
+    bmaf2 = P.af.bmaf_n_2
+
+    base = round(P.paje.base.taux * bmaf, 2)
+    base2 = round(P.paje.base.taux * bmaf2, 2)
+
+    # L'allocation de base est versée jusqu'au dernier jour du mois civil précédant
+    # celui au cours duquel l'enfant atteint l'âge de 3 ans.
+
+    nbenf = nb_enf(age, smic55, 0, P.paje.base.age - 1)
+
+    plaf_tx = (nbenf > 0) + P.paje.base.plaf_tx1 * min_(af_nbenf, 2) + P.paje.base.plaf_tx2 * max_(af_nbenf - 2, 0)
+    majo = isol | biact
+    plaf = P.paje.base.plaf * plaf_tx + (plaf_tx > 0) * P.paje.base.plaf_maj * majo
+    plaf2 = plaf + 12 * base2  # TODO vérifier l'aspect différentielle de la PAJE et le plaf2 de la paje
+
+    paje_base = (nbenf > 0) * ((br_pf < plaf) * base +
+                           (br_pf >= plaf) * max_(plaf2 - br_pf, 0) / 12)
+    # non cumulabe avec la CF, voir Paje_CumulCf
+    return 12 * paje_base  # annualisé
+
+
+def _paje_nais(self, agem_holder, age_holder, af_nbenf, br_pf, isol, biact, P = law.fam):
+    '''
+    Prestation d'accueil du jeune enfant - Allocation de naissance
+    '''
+    age = self.split_by_roles(age_holder, roles = ENFS)
+    agem = self.split_by_roles(agem_holder, roles = ENFS)
+
+    bmaf = P.af.bmaf
+    nais_prime = round(100 * P.paje.nais.prime_tx * bmaf) / 100
+    # Versée au 7e mois de grossesse dans l'année
+    # donc les enfants concernés sont les enfants qui ont -2 mois
+    nbnais = 0
+    for age_m in agem.itervalues():
+        # nbnais += (age_m == -2) cas mensuel
+        nbnais += (age_m >= -2) * (age_m < 10)
+
+    nbaf = 0  # Et on compte le nombre d'enfants AF présents  pour le seul mois de la prime
+    for age_m in agem.itervalues():
+        nbaf += (age_m >= 10)
+
+    nbenf = nbaf + nbnais  # On ajoute l'enfant à  naître;
+
+    paje_plaf = P.paje.base.plaf
+
+    plaf_tx = (nbenf > 0) + P.paje.base.plaf_tx1 * min_(af_nbenf, 2) + P.paje.base.plaf_tx2 * max_(af_nbenf - 2, 0)
+    majo = isol | biact
+    plaf = P.paje.base.plaf * plaf_tx + (plaf_tx > 0) * P.paje.base.plaf_maj * majo
+    elig = (br_pf <= plaf) * (nbnais != 0)
+    nais_brut = nais_prime * elig * (nbnais)
+    return nais_brut
+
+
+def _paje_clca(self, agem_holder, af_nbenf, paje_base, inactif, partiel1, partiel2, P = law.fam):
+    """
+    Prestation d'accueil du jeune enfant - Complément de libre choix d'activité
+    'fam'
+
+    Parameters:
+    -----------
+
+    age :  âge en mois
+    af_nbenf : nombre d'enfants aus sens des allocations familiales
+    paje_base : allocation de base de la PAJE
+    inactif : indicatrice d'inactivité
+    partiel1 : Salarié: Temps de travail ne dépassant pas 50 % de la durée du travail fixée dans l'entreprise pour les salariés
+               VRP ou non salarié travaillant à temps partiel: Temps de travail ne dépassant pas 76 heures par mois
+                  et un revenu professionnel mensuel inférieur ou égal à (smic_8.27*169*85 %)
+    partiel2 :  Salarié: Temps de travail compris entre 50 et 80 % de la durée du travail fixée dans l'entreprise.
+                VRP ou non salarié travaillant à temps partiel: Temps de travail compris entre 77 et 122 heures par mois et un revenu professionnel mensuel ne dépassant pas
+                                                                (smic_8.27*169*136 %)
+
+    http://www.caf.fr/wps/portal/particuliers/catalogue/metropole/paje
+    """
+    agem = self.split_by_roles(agem_holder, roles = ENFS)
+
+    paje = paje_base >= 0
+    # durée de versement :
+    # Pour un seul enfant à charge, le CLCA est versé pendant une période de 6 mois (P.paje.clca.duree1)
+    # à partir de la naissance ou de la cessation des IJ maternité et paternité.
+    # A partir du 2ème enfant, il est versé jusqu’au mois précédant le 3ème anniversaire
+    # de l’enfant.
+
+    # Calcul de l'année et mois de naisage_in_months( du cadet
+    # TODO: ajuster en fonction de la cessation des IJ etc
+    age_m_benjamin = age_en_mois_benjamin(agem)
+    condition1 = (af_nbenf == 1) * (age_m_benjamin >= 0) * (age_m_benjamin < P.paje.clca.duree1)
+    age_benjamin = floor(age_m_benjamin / 12)
+    condition2 = (age_benjamin <= (P.paje.base.age - 1))
+    condition = (af_nbenf >= 2) * condition2 + condition1
+    paje_clca = (condition * P.af.bmaf) * (
+                (not_(paje)) * (inactif * P.paje.clca.sansab_tx_inactif +
+                            partiel1 * P.paje.clca.sansab_tx_partiel1 +
+                            partiel2 * P.paje.clca.sansab_tx_partiel2) +
+                (paje) * (inactif * P.paje.clca.avecab_tx_inactif +
+                            partiel1 * P.paje.clca.avecab_tx_partiel1 +
+                            partiel2 * P.paje.clca.avecab_tx_partiel2))
+    return 12 * paje_clca  # annualisé
+
+
+def _paje_clca_taux_plein(paje_clca, inactif):
+    return (paje_clca > 0) * inactif
+
+
+def _paje_clca_taux_partiel(paje_clca, partiel1):
+    return (paje_clca > 0) * partiel1
+
+    # TODO gérer les cumuls avec autres revenus et colca voir site caf
+
+
+def _paje_clmg(self, aah, age_holder, smic55_holder, etu_holder, sal_holder, hsup_holder, concub, af_nbenf, br_pf, empl_dir, ass_mat, gar_dom,
+        paje_clca_taux_partiel, paje_clca_taux_plein, P = law.fam):
+    '''
+    Prestation d accueil du jeune enfant - Complément de libre choix du mode de garde
+
+    Les conditions
+
+    Vous devez :
+
+        avoir un enfant de moins de 6 ans né, adopté ou recueilli en vue d'adoption à partir du 1er janvier 2004
+        employer une assistante maternelle agréée ou une garde à domicile.
+        avoir une activité professionnelle minimale
+            si vous êtes salarié cette activité doit vous procurer un revenu minimum de :
+                si vous vivez seul : une fois la BMAF
+                si vous vivez en couple  soit 2 fois la BMAF
+            si vous êtes non salarié, vous devez être à jour de vos cotisations sociales d'assurance vieillesse
+
+    Vous n'avez pas besoin de justifier d'une activité min_ si vous êtes :
+
+        bénéficiaire de l'allocation aux adultes handicapés (Aah)
+        au chômage et bénéficiaire de l'allocation d'insertion ou de l'allocation de solidarité spécifique
+        bénéficiaire du Revenu de solidarité active (Rsa), sous certaines conditions de ressources étudiées par votre Caf, et inscrit dans une démarche d'insertion
+        étudiant (si vous vivez en couple, vous devez être tous les deux étudiants).
+
+    Autres conditions à remplir : Assistante maternelle agréée     Garde à domicile
+    Son salaire brut ne doit pas dépasser par jour de garde et par enfant 5 fois le montant du Smic horaire brut, soit au max 45,00 €.
+    Vous ne devez pas bénéficier de l'exonération des cotisations sociales dues pour la personne employée.
+    '''
+    age = self.split_by_roles(age_holder, roles = ENFS)
+    etu = self.split_by_roles(etu_holder, roles = [CHEF, PART])
+    hsup = self.split_by_roles(hsup_holder, roles = [CHEF, PART])
+    sal = self.split_by_roles(sal_holder, roles = [CHEF, PART])
+    smic55 = self.split_by_roles(smic55_holder, roles = ENFS)
+
+    # condition de revenu minimal
+
+    cond_age_enf = (nb_enf(age, smic55, P.paje.clmg.age1, P.paje.clmg.age2 - 1) > 0)
+    cond_sal = (sal[CHEF] + sal[PART] + hsup[CHEF] + hsup[PART] > 12 * P.af.bmaf_n_2 * (1 + concub))
+# TODO:    cond_rpns    =
+    cond_act = cond_sal  # | cond_rpns
+
+    cond_nonact = (aah > 0) | (etu[CHEF] & etu[PART])  # | (ass>0)
+#  TODO: RSA insertion, alloc insertion, ass
+    elig = cond_age_enf & (cond_act | cond_nonact)
+    nbenf = af_nbenf
+    seuil1 = P.paje.clmg.seuil11 * (nbenf == 1) + P.paje.clmg.seuil12 * (nbenf >= 2) + max_(nbenf - 2, 0) * P.paje.clmg.seuil1sup
+    seuil2 = P.paje.clmg.seuil21 * (nbenf == 1) + P.paje.clmg.seuil22 * (nbenf >= 2) + max_(nbenf - 2, 0) * P.paje.clmg.seuil2sup
+
+#        Si vous bénéficiez du Clca taux partiel (= vous travaillez entre 50 et 80% de la durée du travail fixée dans l'entreprise),
+#        vous cumulez intégralement le Clca et le Cmg.
+#        Si vous bénéficiez du Clca taux partiel (= vous travaillez à 50% ou moins de la durée
+#        du travail fixée dans l'entreprise), le montant des plafonds Cmg est divisé par 2.
+    seuil1 = seuil1 * (1 - .5 * paje_clca_taux_partiel)
+    seuil2 = seuil2 * (1 - .5 * paje_clca_taux_partiel)
+
+    clmg = P.af.bmaf * ((nb_enf(age, smic55, 0, P.paje.clmg.age1 - 1) > 0) +
+                           0.5 * (nb_enf(age, smic55, P.paje.clmg.age1, P.paje.clmg.age2 - 1) > 0)
+                           ) * (
+        empl_dir * (
+            (br_pf < seuil1) * P.paje.clmg.empl_dir1 +
+            ((br_pf >= seuil1) & (br_pf < seuil2)) * P.paje.clmg.empl_dir2 +
+            (br_pf >= seuil2) * P.paje.clmg.empl_dir3) +
+        ass_mat * (
+            (br_pf < seuil1) * P.paje.clmg.ass_mat1 +
+            ((br_pf >= seuil1) & (br_pf < seuil2)) * P.paje.clmg.ass_mat2 +
+            (br_pf >= seuil2) * P.paje.clmg.ass_mat3) +
+        gar_dom * (
+            (br_pf < seuil1) * P.paje.clmg.domi1 +
+            ((br_pf >= seuil1) & (br_pf < seuil2)) * P.paje.clmg.domi2 +
+            (br_pf >= seuil2) * P.paje.clmg.domi3))
+    # TODO: connecter avec le crédit d'impôt
+    # Si vous bénéficiez du Clca taux plein (= vous ne travaillez plus ou interrompez votre activité professionnelle),
+    # vous ne pouvez pas bénéficier du Cmg.
+    paje_clmg = elig * not_(paje_clca_taux_plein) * clmg
+    # TODO vérfiez les règles de cumul
+    return 12 * paje_clmg  # annualisé
+
+
+def _paje_colca(self, af_nbenf, agem_holder, opt_colca, paje_base, P = law.fam):
+    '''
+    Prestation d'accueil du jeune enfant - Complément optionnel de libre choix du mode de garde
+    '''
+    agem = self.split_by_roles(agem_holder, roles = ENFS)
+
+    age_m_benjamin = age_en_mois_benjamin(agem)
+    condition = (age_m_benjamin < 12 * P.paje.colca.age) * (age_m_benjamin >= 0)
+    nbenf = af_nbenf
+    paje = (paje_base > 0)
+    paje_colca = opt_colca * condition * (nbenf >= 3) * P.af.bmaf * (
+        (paje) * P.paje.colca.avecab + not_(paje) * P.paje.colca.sansab)
+    return 12 * paje_colca  # annualisé
+    # TODO: cumul avec clca self.colca_tot_m
+
+
+def _paje_cumul(paje_base_temp, cf_temp):
+    '''
+    L'allocation de base de la paje n'est pas cumulable avec le complément familial
+    '''
+    # On regarde ce qui est le plus intéressant pour la famille, chaque mois
+    paje_base = (paje_base_temp >= cf_temp) * paje_base_temp
+    return round(paje_base, 2)
 
 
 def _afeama(self, age_holder, smic55_holder, ape, af_nbenf, br_pf, P = law.fam):
