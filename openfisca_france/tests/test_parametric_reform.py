cb23a7b64f93e81fdb287e75930beb1cbd678d40
Christophe Benz <christophe.benz@data.gouv.fr>
2014-10-13 15:48:28 +0200

    Fix parametric reform test

--- openfisca_france/tests/test_parametric_reform.py
+++ openfisca_france/tests/test_parametric_reform.py
@@ -23,8 +23,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import copy
 import datetime
+
+from nose.tools import assert_less
 
 from openfisca_core import periods, reforms
 import openfisca_france
@@ -34,14 +35,20 @@ TaxBenefitSystem = openfisca_france.init_country()
 tax_benefit_system = TaxBenefitSystem()
 
 
-def test_parametric_reform(year = 2014):
+def test_parametric_reform():
+    simulation_year = 2014
+    simulation_period = periods.period('year', simulation_year)
     reference_legislation_json = tax_benefit_system.legislation_json
-    reform_legislation_json = copy.deepcopy(reference_legislation_json)
-    assert reform_legislation_json['children']['ir']['children']['bareme']['slices'][0]['rate'] == 0
-    reform_legislation_json['children']['ir']['children']['bareme']['slices'][0]['rate'] = 1
+
+    reform_legislation_json = reforms.update_legislation(
+        legislation_json = reference_legislation_json,
+        path = ('children', 'ir', 'children', 'bareme', 'slices', 0, 'rate'),
+        period = simulation_period,
+        value = 1,
+        )
 
     reform = reforms.Reform(
-        name = "IR_100_tranche_1",
+        name = u'IR_100_tranche_1',
         label = u"Imposition à 100% dès le premier euro et jusqu'à la fin de la 1ère tranche",
         legislation_json = reform_legislation_json,
         reference_legislation_json = reference_legislation_json
@@ -56,20 +63,19 @@ def test_parametric_reform(year = 2014):
                 min = 0,
                 ),
             ],
-        period = periods.period('year', year),
-        parent1 = dict(birth = datetime.date(year - 40, 1, 1)),
+        period = simulation_period,
+        parent1 = dict(birth = datetime.date(simulation_year - 40, 1, 1)),
         )
 
     simulation = scenario.new_simulation(debug = True)
-    assert max(abs(simulation.calculate('impo') - [0, -7889.20019531, -23435.52929688])) < .01
+    assert_less(max(abs(simulation.calculate('impo') - [0, -7889.20019531, -23435.52929688])), .01)
 
     reform_simulation = reform.new_simulation(debug = True, scenario = scenario)
-    assert reform_simulation.compact_legislation is not None
-    assert max(abs(reform_simulation.calculate('impo') - [0., -13900.20019531, -29446.52929688])) < .0001
+    assert_less(max(abs(reform_simulation.calculate('impo') - [0., -13900.20019531, -29446.52929688])), .0001)
 
 
 if __name__ == '__main__':
     import logging
     import sys
     logging.basicConfig(level = logging.ERROR, stream = sys.stdout)
-    test_parametric_reform(2014)
+    test_parametric_reform()
