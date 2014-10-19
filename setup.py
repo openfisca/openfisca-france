a8b12bb162c9c8945a384897582a37b2f1ae46ce
Emmanuel Raviart <emmanuel@raviart.com>
2014-10-19 19:31:32 +0200

    Increase version number to 0.4.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ doc_lines = __doc__.split('\n')
 
 setup(
     name = 'OpenFisca-France',
-    version = '0.3dev',
+    version = '0.4dev',
 
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
@@ -66,7 +66,7 @@ setup(
         'Babel >= 0.9.4',
         'Biryani1[datetimeconv] >= 0.9dev',
         'numpy',
-        'OpenFisca-Core >= 0.3dev',
+        'OpenFisca-Core >= 0.4dev',
         'scipy >= 0.12',
         ],
     message_extractors = {'openfisca_france': [
