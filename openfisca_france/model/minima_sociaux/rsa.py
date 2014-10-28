c4e50214437f9a5ddbc6958405eac36bbb53c95f
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-10-28 14:46:43 +0100

    ajout de ressources prises en compte dans la base de ressources du rsa

--- openfisca_france/model/minima_sociaux/rsa.py
+++ openfisca_france/model/minima_sociaux/rsa.py
@@ -78,8 +78,14 @@ class ra_rsa(SimpleFormulaColumn):
     entity_class = Individus
     period_unit = 'month'
 
-    def function(self, salnet, hsup, rpns, etr, indemnites_chomage_partiel, indemnites_journalieres_maternite, indemnites_journalieres_paternite, indemnites_journalieres_adoption, indemnites_journalieres_maladie, indemnites_journalieres_accident_travail, indemnites_journalieres_maladie_professionnelle):
-        return salnet + hsup + rpns + etr + indemnites_chomage_partiel + indemnites_journalieres_maternite + indemnites_journalieres_paternite + indemnites_journalieres_adoption + indemnites_journalieres_maladie + indemnites_journalieres_accident_travail + indemnites_journalieres_maladie_professionnelle
+    def function(self, salnet, hsup, rpns, etr, indemnites_chomage_partiel, indemnites_journalieres_maternite,
+                 indemnites_journalieres_paternite, indemnites_journalieres_adoption, indemnites_journalieres_maladie,
+                 indemnites_journalieres_accident_travail, indemnites_journalieres_maladie_professionnelle,
+                 indemnites_volontariat):
+        return (salnet + hsup + rpns + etr + indemnites_chomage_partiel + indemnites_journalieres_maternite +
+            indemnites_journalieres_paternite + indemnites_journalieres_adoption + indemnites_journalieres_maladie +
+            indemnites_journalieres_accident_travail + indemnites_journalieres_maladie_professionnelle +
+            indemnites_volontariat)
 
     def get_output_period(self, period):
         return period.start.offset('first-of', 'month').period('month')
@@ -146,11 +152,15 @@ class br_rmi_i(SimpleFormulaColumn):
     entity_class = Individus
     period_unit = 'month'
 
-    def function(self, ass_holder, ra_rsa, chonet, rstnet, alr, rto, rev_cap_bar_holder, rev_cap_lib_holder, rfon_ms, div_ms):
+    def function(self, ass_holder, ra_rsa, chonet, rstnet, alr, rto, rev_cap_bar_holder, rev_cap_lib_holder, rfon_ms, div_ms,
+                 gains_exceptionnels, dedommagement_victime_amiante, pensions_invalidite, allocation_aide_retour_emploi,
+                 allocation_securisation_professionnelle, prestation_compensatoire, retraite_combattant, bourse_enseignement_sup, bourse_recherche):
         rev_cap_bar = self.cast_from_entity_to_role(rev_cap_bar_holder, role = VOUS)
         rev_cap_lib = self.cast_from_entity_to_role(rev_cap_lib_holder, role = VOUS)
         ass = self.cast_from_entity_to_roles(ass_holder)
-        return ass + ra_rsa + chonet + rstnet + alr + rto + rev_cap_bar + rev_cap_lib + rfon_ms + div_ms
+        return (ass + ra_rsa + chonet + rstnet + alr + rto + rev_cap_bar + rev_cap_lib + rfon_ms + div_ms +
+            gains_exceptionnels + dedommagement_victime_amiante, pensions_invalidite + allocation_aide_retour_emploi +
+            allocation_securisation_professionnelle + prestation_compensatoire + retraite_combattant + bourse_enseignement_sup + bourse_recherche)
 
     def get_output_period(self, period):
         return period.start.offset('first-of', 'month').period('month')
