5974432ab6c496cef83dae8e28285a6d9167732e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-02-20 18:19:06 +0100

    CMU-C/ACS : prise en compte de la garde alternée

--- openfisca_france/model/minima_sociaux/cmu.py
+++ openfisca_france/model/minima_sociaux/cmu.py
@@ -134,29 +134,40 @@ class cmu_c_plafond(SimpleFormulaColumn):
         cmu_nbp_foyer = simulation.calculate('cmu_nbp_foyer', period)
         P = simulation.legislation_at(period.start).cmu
 
-        # Calcule le pourcentage de ressources dues aux enfants.
+        PAC = [PART] + ENFS
+
+        # Calcul du coefficient personnes à charge, avec prise en compte de la garde alternée
+
+        # Tableau des coefficients
         coefficients_array = array(
-            [P.coeff_p3_p4, P.coeff_p3_p4, P.coeff_p5_plus] + [0] * (len(ENFS) - 3)
+            [P.coeff_p2, P.coeff_p3_p4, P.coeff_p3_p4, P.coeff_p5_plus] + [0] * (len(PAC) - 4)
             )
-        # Trie les enfants par âge décroissant en mettant, à âge égal, les enfants en garde alternée avant.
-        age_by_role = self.split_by_roles(age, roles = ENFS)
-        alt_by_role = self.split_by_roles(alt, roles = ENFS)
+
+        # Tri des personnes à charge, le conjoint en premier, les enfants par âge décroissant en mettant
+        age_by_role = self.split_by_roles(age, roles = PAC)
+        alt_by_role = self.split_by_roles(alt, roles = PAC)
         age_and_alt_matrix = array(
             [
-                age_by_role[role] * 10 + alt_by_role[role]
+                (role == PART) * 10000 + age_by_role[role] * 10 + alt_by_role[role]
                 for role in sorted(age_by_role)
                 ]
             ).transpose()
+
+        # Calcul avec matrices intermédiaires
         reverse_sorted = partial(sorted, reverse = True)
+
         sorted_age_and_alt_matrix = apply_along_axis(reverse_sorted, 1, age_and_alt_matrix)
         # Calcule weighted_alt_matrix, qui vaut 0.5 pour les enfants en garde alternée, 1 sinon.
         sorted_present_matrix = sorted_age_and_alt_matrix >= 0
         sorted_alt_matrix = (sorted_age_and_alt_matrix % 10) * sorted_present_matrix
         weighted_alt_matrix = sorted_present_matrix - sorted_alt_matrix * 0.5
+
+        # Calcul final du coefficient
         coeff_pac = weighted_alt_matrix.dot(coefficients_array)
+
         return period, (P.plafond_base *
             (1 + cmu_eligible_majoration_dom * P.majoration_dom) *
-            (1 + (cmu_nbp_foyer >= 2) * P.coeff_p2 + coeff_pac)
+            (1 + coeff_pac)
             )
 
 
