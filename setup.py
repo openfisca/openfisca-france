983b6e8e7410bd876f41b292570d6fb3b5d7067c
Christophe Benz <christophe.benz@data.gouv.fr>
2015-03-31 16:49:23 +0200

    Follow PEP 440

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ doc_lines = __doc__.split('\n')
 
 setup(
     name = 'OpenFisca-France',
-    version = '0.5dev',
+    version = '0.5.dev0',
 
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
