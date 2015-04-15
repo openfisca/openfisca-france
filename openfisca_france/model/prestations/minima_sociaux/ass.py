b798a6617810898b7b464c5d8b2d5183176562f2
fpagnoux <fpagnoux@octo.com>
2015-04-15 18:38:45 +0200

    Do not allo ASS when user is perceiving ARE

--- openfisca_france/model/prestations/minima_sociaux/ass.py
+++ openfisca_france/model/prestations/minima_sociaux/ass.py
@@ -41,11 +41,16 @@ class ass_eligibilite_i(SimpleFormulaColumn):
 
     def function(self, simulation, period):
         period = period.start.offset('first-of', 'month').period('month')
+        
+        #1 si demandeur d'emploi
+        activite = simulation.calculate('activite', period) 
 
-        activite = simulation.calculate('activite', period)
-        ass_precondition_remplie = simulation.calculate('ass_precondition_remplie', period)
+        #Indique que l'user a travaillé 5 ans au cours des 10 dernieres années.
+        ass_precondition_remplie = simulation.calculate('ass_precondition_remplie', period) 
 
-        return period, and_(activite == 1, ass_precondition_remplie)
+        are_perceived_this_month = simulation.calculate('chonet', period)
+
+        return period, and_(and_(activite == 1, ass_precondition_remplie), are_perceived_this_month == 0)
 
 
 @reference_formula
