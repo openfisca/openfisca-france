06c3690d6a6166c5036e85c48b6f89a09a734977
SophieIPP <cottet.s@gmail.com>
2015-05-27 18:10:01 +0200

    Fix aah - functions and tests

--- openfisca_france/model/prestations/minima_sociaux/aah.py
+++ openfisca_france/model/prestations/minima_sociaux/aah.py
@@ -29,21 +29,25 @@ from numpy import (maximum as max_)
 from ...base import *  # noqa analysis:ignore
 
 
-build_column('aah', FloatCol(entity = 'ind', label = u"Allocation de l'adulte handicapé"))
-build_column('caah', FloatCol(entity = 'ind', label = u"Complément de l'allocation de l'adulte handicapé"))
+@reference_formula
+class br_aah(SimpleFormulaColumn):
+    column = FloatCol
+    label = u"Base ressources de l'allocation adulte handicapé"
+    entity_class = Familles
 
+    def function(self, simulation, period):
+        period = period.start.offset('first-of', 'month').period('month')
+        annee_fiscale_n_2 = period.start.offset('first-of', 'year').period('year').offset(-2)
 
-def _br_aah(br_pf, asi, aspa):
-    '''
-    Base ressources de l'allocation adulte handicapé
-    'fam'
-    '''
-    br_aah = br_pf + asi + aspa
-    return br_aah
+# inactifs ou travailleurs en ESAT :
+        br_pf_n_2 = simulation.calculate_add('br_pf', period)
+        asi_n_2 = simulation.calculate_add('asi', annee_fiscale_n_2)
+        aspa_n_2 = simulation.calculate_add('aspa', annee_fiscale_n_2)
+# TODO: travailleurs en milieu protégé : les ressources celles des trois derniers mois.
+# toujours la même base ressources ? http://www.guide-familial.fr/actualite-149654--nouvelle-etape-dans-la-reforme-de-l-aah--la-prise-en-compte-trimestrielle-des-ressources.html
 
+        return period, br_pf_n_2 + asi_n_2 + aspa_n_2
 
-def _aah(self, br_pf_i_holder, br_aah, invalide_holder, age_holder, smic55_holder, concub, af_nbenf, aah = law.minim.aah,
-        aeeh = law.fam.aeeh):
     '''
     Allocation adulte handicapé
 
@@ -59,7 +63,8 @@ def _aah(self, br_pf_i_holder, br_aah, invalide_holder, age_holder, smic55_holde
      Les personnes de nationalité étrangère doivent être en possession d'un titre de séjour
      régulier ou être titulaire d'un récépissé de renouvellement de titre de séjour.
     Condition d'âge
-    Age minimum : Le demandeur ne doit plus avoir l'âge de bénéficier de l'allocation d'éducation de l'enfant handicapé, c'est-à-dire qu'il doit être âgé :
+    Age minimum : Le demandeur ne doit plus avoir l'âge de bénéficier de l'allocation d'éducation de l'enfant
+    handicapé, c'est-à-dire qu'il doit être âgé :
     - de plus de vingt ans,
     - ou de plus de seize ans, s'il ne remplit plus les conditions pour ouvrir droit aux allocations familiales.
     Pour les montants http://www.handipole.org/spip.php?article666
@@ -68,7 +73,8 @@ def _aah(self, br_pf_i_holder, br_aah, invalide_holder, age_holder, smic55_holde
     Le versement de l'AAH prend fin à partir de l'âge minimum légal de départ à la retraite en cas d'incapacité
     de 50 % à 79 %. À cet âge, le bénéficiaire bascule dans le régime de retraite pour inaptitude.
     En cas d'incapacité d'au moins 80 %, une AAH différentielle (c'est-à-dire une allocation mensuelle réduite)
-    peut être versée au-delà de l'âge minimum légal de départ à la retraite en complément d'une retraite inférieure au minimum vieillesse.
+    peut être versée au-delà de l'âge minimum légal de départ à la retraite en complément d'une retraite inférieure
+    au minimum vieillesse.
 
     N'entrent pas en compte dans les ressources :
     L'allocation compensatrice tierce personne, les allocations familiales,
@@ -82,132 +88,181 @@ def _aah(self, br_pf_i_holder, br_aah, invalide_holder, age_holder, smic55_holde
     Notez, dans certaines situations, la Caf évalue forfaitairement vos
     ressources à partir de votre revenu mensuel.
     '''
-    age = self.split_by_roles(age_holder, roles = [CHEF, PART])
-    br_pf_i = self.split_by_roles(br_pf_i_holder, roles = [CHEF, PART])
-    invalide = self.split_by_roles(invalide_holder, roles = [CHEF, PART])
-    smic55 = self.split_by_roles(smic55_holder, roles = [CHEF, PART])
-
-#    TODO éligibilité AAH, notamment avoir le % d'incapacité ?
-
-    eligC = (((invalide[CHEF]) & (age[CHEF] <= aah.age_legal_retraite)) &
-              ((age[CHEF] >= aeeh.age) | ((age[CHEF] >= 16) & (smic55[CHEF]))))
-
-    eligP = (((invalide[PART]) & (age[PART] <= aah.age_legal_retraite)) &
-              ((age[PART] >= aeeh.age) | ((age[PART] >= 16) & (smic55[PART]))))
-
-    plaf_aah = 12 * aah.montant * (1 + concub + aah.tx_plaf_supp * af_nbenf)
-    eligib = (eligC | eligP)
-    # l'aah est exonérée de crds
-
-#        Cumul d'allocation
-# L'AAH peut être cumulée :
-#
-# - avec le complément d'AAH (à titre transitoire pour les derniers bénéficiaires,
-#  ce complément étant remplacé par la majoration pour la vie autonome depuis
-#  le 1er juillet 2005) ;
-# - avec la majoration pour la vie autonome ;
-# - avec le complément de ressources (dans le cadre de la garantie de ressources).
-#
-# L'AAH n'est pas cumulable avec la perception d'un avantage de vieillesse,
-# d'invalidité, ou d'accident du travail si cet avantage est d'un montant au
-# moins égal à ladite allocation.
-    return eligib * max_(plaf_aah - br_aah, 0)  # annualisé
 
 
-def _caah__2005(aah, asi, P = law.minim):
+@reference_formula
+class aah_eligible(SimpleFormulaColumn):
+    column = BoolCol
+    label = u"Eligibilité à  l'Allocation adulte handicapé"
+    entity_class = Individus
+
+    def function(self, simulation, period):
+        period = period.start.offset('first-of', 'month').period('month')
+        law = simulation.legislation_at(period.start)
+
+        invalide = simulation.calculate('invalide', period)
+        age = simulation.calculate('age', period)
+        smic55 = simulation.calculate('smic55', period)
+
+        eligible_aah = (
+            ((invalide) & (age <= law.minim.aah.age_legal_retraite)) &
+            ((age >= law.fam.aeeh.age) | ((age >= 16) & (smic55)))
+            )
+
+        return period, eligible_aah
+    # TODO: dated_function : avant 2008, il fallait ne pas avoir travaillé pendant les 12 mois précédant la demande.
+
+
+@reference_formula
+class nb_eligib_aah(SimpleFormulaColumn):
+    column = FloatCol
+    label = "Nombre d'allocataires de l'AAH dans la famille"
+    entity_class = Familles
+
+    def function(self, simulation, period):
+        period = period.start.offset('first-of', 'month').period('month')
+        br_aah = simulation.calculate('br_aah', period)
+        aah_eligible_holder = simulation.calculate('aah_eligible', period)
+
+        aah_eligible = self.split_by_roles(aah_eligible_holder)
+
+        nb_eligib_aah = 0.0 * br_aah
+        for eligible in aah_eligible.values():
+            nb_eligib_aah = nb_eligib_aah + eligible
+
+        return period, nb_eligib_aah
+
+
+@reference_formula
+class aah_famille(SimpleFormulaColumn):
+    column = FloatCol
+    label = u"Allocation adulte handicapé (Familles) mensualisée"
+    entity_class = Familles
+
+    def function(self, simulation, period):
+        period = period.start.offset('first-of', 'month').period('month')
+        law = simulation.legislation_at(period.start)
+
+        br_aah = simulation.calculate('br_aah', period)
+        concub = simulation.calculate('concub', period)
+        af_nbenf = simulation.calculate('af_nbenf', period)
+        nb_eligib_aah = simulation.calculate('nb_eligib_aah', period)
+
+        plaf_ress_aah = 12 * law.minim.aah.montant * (1 + concub + law.minim.aah.tx_plaf_supp * af_nbenf)
+
+        return period, nb_eligib_aah * max_(plaf_ress_aah - br_aah, 0) / 12
+
+
+@reference_formula
+class aah(SimpleFormulaColumn):
+    column = FloatCol
+    label = u"Allocation adulte handicapé (Individus) mensualisée"
+    entity_class = Individus
+
+    def function(self, simulation, period):
+        period = period.start.offset('first-of', 'month').period('month')
+
+        aah_eligible = simulation.calculate('aah_eligible', period)
+
+        aah_famille_holder = simulation.compute('aah_famille', period)
+        aah_famille = self.cast_from_entity_to_role(aah_famille_holder, role = VOUS)
+
+        nb_eligib_aah_holder = simulation.compute('nb_eligib_aah', period)
+        nb_eligib_aah = self.cast_from_entity_to_role(nb_eligib_aah_holder, role = VOUS)
+
+        if aah_eligible == 'False':
+            return period, 0 * aah_famille
+        else:
+            return period, aah_famille / nb_eligib_aah
+
+
+class caah(SimpleFormulaColumn):
+    column = FloatCol
+    label = u"Complément d'allocation adulte handicapé (mensualisé)"
+    entity_class = Individus
     '''
-    Complément d'allocation adulte handicapé
+    Complément d'allocation adulte handicapé : complément de ressources ou majoration vie autonome.
+
+    Complément de ressources
+
+    Pour bénéficier du complément de ressources, l’intéressé doit remplir les conditions
+    suivantes :
+    - percevoir l’allocation aux adultes handicapés à taux normal ou en
+       complément d’une pension d’invalidité, d’une pension de vieillesse ou
+       d’une rente accident du travail ;
+    - avoir un taux d’incapacité égal ou supérieur à 80 % ;
+    - avoir une capacité de travail, appréciée par la commission des droits et
+       de l’autonomie (CDAPH) inférieure à 5 % du fait du handicap ;
+    - ne pas avoir perçu de revenu à caractère professionnel depuis un an à la date
+       du dépôt de la demande de complément ;
+    - disposer d’un logement indépendant.
+    A noter : une personne hébergée par un particulier à son domicile n’est pas
+    considérée disposer d’un logement indépendant, sauf s’il s’agit de son conjoint,
+    de son concubin ou de la personne avec laquelle elle est liée par un pacte civil
+    de solidarité.
+
+    Le complément de ressources est destiné aux personnes handicapées dans l’incapacité de
+    travailler. Il est égal à la différence entre la garantie de ressources pour les personnes
+    handicapées (GRPH) et l’AAH.
+
+    Majoration pour la vie autonome
+
+    La majoration pour la vie autonome est destinée à permettre aux personnes, en capacité de travailler et
+    au chômage en raison de leur handicap, de pourvoir faire face à leur dépense de logement.
+
+    Conditions d'attribution
+    La majoration pour la vie autonome est versée automatiquement aux personnes qui remplissent les conditions
+    suivantes :
+    - percevoir l'AAH à taux normal ou en complément d'un avantage vieillesse ou d'invalidité ou d'une rente
+    accident du travail,
+    - avoir un taux d'incapacité au moins égal à 80 %,
+    - disposer d'un logement indépendant,
+    - bénéficier d'une aide au logement (aide personnelle au logement, ou allocation de logement sociale ou
+    familiale), comme titulaire du droit, ou comme conjoint, concubin ou partenaire lié par
+    un Pacs au titulaire du droit,
+    - ne pas percevoir de revenu d'activité à caractère professionnel propre.
+
+    Choix entre la majoration ou la garantie de ressources
+    La majoration pour la vie autonome n'est pas cumulable avec la garantie de ressources pour les personnes
+    handicapées.
+    La personne qui remplit les conditions d'octroi de ces deux avantages doit choisir de bénéficier de l'un ou de
+    l'autre.
     '''
-# Pour bénéficier du complément de ressources, l’intéressé doit remplir les conditions
-# suivantes :
-# - percevoir l’allocation aux adultes handicapés à taux normal ou en
-#    complément d’une pension d’invalidité, d’une pension de vieillesse ou
-#    d’une rente accident du travail ;
-# - avoir un taux d’incapacité égal ou supérieur à 80 % ;
-# - avoir une capacité de travail, appréciée par la commission des droits et
-#    de l’autonomie (CDAPH) inférieure à 5 % du fait du handicap ;
-# - ne pas avoir perçu de revenu à caractère professionnel depuis un an à la date
-#    du dépôt de la demande de complément ;
-# - disposer d’un logement indépendant.
-# A noter : une personne hébergée par un particulier à son domicile n’est pas
-# considérée disposer d’un logement indépendant, sauf s’il s’agit de son conjoint,
-# de son concubin ou de la personne avec laquelle elle est liée par un pacte civil
-# de solidarité.
 
-#       Complément de ressources Le complément de ressources est
-#       destiné aux personnes handicapées dans l’incapacité de
-#       travailler Il est égal à la différence entre la garantie de
-#       ressources pour les personnes handicapées (GRPH) et l’AAH
+    @dated_function(start=date(2005, 7, 1))
+    def function(self, simulation period):
+        period = period.start.offset('first-of', 'month').period('month')
+        law = simulation.legislation_at(period.start)
 
-    elig_cpl = ((aah > 0) | (asi > 0))  # TODO: éligibilité logement indépendant
-    compl = P.caah.cpltx * P.aah.montant * elig_cpl
-        # En fait perdure jusqu'en 2008
+        law.minim.caah.grph = simulation.calculate(law.minim.caah.cpltx, period)
+        law.minim.aah.montant = simulation.calculate(law.minim.aah.montant, period)
 
+        aah = simulation.calculate(aah, period)
+        asi = simulation.calculate(asi, period)
 
-    # Majoration pour la vie autonome
-    # La majoration pour la vie autonome est destinée à permettre aux personnes, en capacité de travailler et au chômage
-    # en raison de leur handicap, de pourvoir faire face à leur dépense de logement.
+        elig_cpl = ((aah > 0) | (asi > 0))  # TODO: & logement indépendant & inactif 12 derniers mois
+                                            # & capa de travail < 5% & taux d'invalidité >= 80%
+        compl_ress = elig_cpl * max_(law.minim.caah.grph - law.minim.aah.montant / 12, 0)
 
-#        Conditions d'attribution
-# La majoration pour la vie autonome est versée automatiquement aux personnes qui remplissent les conditions suivantes :
-# - percevoir l'AAH à taux normal ou en complément d'un avantage vieillesse ou d'invalidité ou d'une rente accident du travail,
-# - avoir un taux d'incapacité au moins égal à 80 %,
-# - disposer d'un logement indépendant,
-# - bénéficier d'une aide au logement (aide personnelle au logement, ou allocation de logement sociale ou familiale), comme titulaire du droit, ou comme conjoint, concubin ou partenaire lié par un Pacs au titulaire du droit,
-# - ne pas percevoir de revenu d'activité à caractère professionnel propre.
-# Choix entre la majoration ou la garantie de ressources
-# La majoration pour la vie autonome n'est pas cumulable avec la garantie de ressources pour les personnes handicapées.
-# La personne qui remplit les conditions d'octroi de ces deux avantages doit choisir de bénéficier de l'un ou de l'autre.
-    mva = 0
-    caah = max_(compl, mva)
-    return 12 * caah  # annualisé
+        elig_mva = (al > 0) * ((aah > 0) | (asi > 0))  # TODO: & logement indépendant & pas de revenus professionnels
+                                                       # propres & capa de travail < 5% & taux d'invalidité >= 80%
+        mva = 0  # TODO: rentrer mva dans paramètres. mva (mensuelle) = 104,77 en 2015, était de 101,80 en 2006, et de 119,72 en 2007
 
-def _caah_2006_(aah, asi, br_aah, al, P = law.minim):
-    '''
-    Complément d'allocation adulte handicapé
-    '''
-# Pour bénéficier du complément de ressources, l’intéressé doit remplir les conditions
-# suivantes :
-# - percevoir l’allocation aux adultes handicapés à taux normal ou en
-#    complément d’une pension d’invalidité, d’une pension de vieillesse ou
-#    d’une rente accident du travail ;
-# - avoir un taux d’incapacité égal ou supérieur à 80 % ;
-# - avoir une capacité de travail, appréciée par la commission des droits et
-#    de l’autonomie (CDAPH) inférieure à 5 % du fait du handicap ;
-# - ne pas avoir perçu de revenu à caractère professionnel depuis un an à la date
-#    du dépôt de la demande de complément ;
-# - disposer d’un logement indépendant.
-# A noter : une personne hébergée par un particulier à son domicile n’est pas
-# considérée disposer d’un logement indépendant, sauf s’il s’agit de son conjoint,
-# de son concubin ou de la personne avec laquelle elle est liée par un pacte civil
-# de solidarité.
+        return period, max_(compl_ress, mva)
 
-#       Complément de ressources Le complément de ressources est
-#       destiné aux personnes handicapées dans l’incapacité de
-#       travailler Il est égal à la différence entre la garantie de
-#       ressources pour les personnes handicapées (GRPH) et l’AAH
+    @dated_function(stop=date(2005, 6, 30))
+    def function(self, simulation, period):
+        period = period.start.offset('first-of', 'month').period('month')
+        law = simulation.legislation_at(period.start)
 
-    elig_cpl = ((aah > 0) | (asi > 0))  # TODO: éligibilité logement indépendant
-    compl = elig_cpl * max_(P.caah.grph - (aah + br_aah) / 12, 0)
+        law.minim.caah.cpltx = simulation.calculate(law.minim.caah.cpltx, period)
+        law.minim.aah.montant = simulation.calculate(law.minim.aah.montant, period)
 
-        # En fait perdure jusqu'en 2008
+        aah = simulation.calculate(aah, period)
+        asi = simulation.calculate(asi, period)
 
+        elig_ancien_caah = ((aah > 0) | (asi > 0)) * (al > 0)  # TODO: & invalidité >= 80%  & logement indépendant
+        ancien_caah = law.minim.caah.cpltx * law.minim.aah.montant * elig_ancien_caah
+            # En fait le taux cpltx perdure jusqu'en 2008
 
-    # Majoration pour la vie autonome
-    # La majoration pour la vie autonome est destinée à permettre aux personnes, en capacité de travailler et au chômage
-    # en raison de leur handicap, de pourvoir faire face à leur dépense de logement.
-
-#        Conditions d'attribution
-# La majoration pour la vie autonome est versée automatiquement aux personnes qui remplissent les conditions suivantes :
-# - percevoir l'AAH à taux normal ou en complément d'un avantage vieillesse ou d'invalidité ou d'une rente accident du travail,
-# - avoir un taux d'incapacité au moins égal à 80 %,
-# - disposer d'un logement indépendant,
-# - bénéficier d'une aide au logement (aide personnelle au logement, ou allocation de logement sociale ou familiale), comme titulaire du droit, ou comme conjoint, concubin ou partenaire lié par un Pacs au titulaire du droit,
-# - ne pas percevoir de revenu d'activité à caractère professionnel propre.
-# Choix entre la majoration ou la garantie de ressources
-# La majoration pour la vie autonome n'est pas cumulable avec la garantie de ressources pour les personnes handicapées.
-# La personne qui remplit les conditions d'octroi de ces deux avantages doit choisir de bénéficier de l'un ou de l'autre.
-    elig_mva = (al > 0) * ((aah > 0) | (asi > 0))  # TODO: complêter éligibilité
-    mva = P.caah.mva * elig_mva * 0
-    caah = max_(compl, mva)
-    return 12 * caah  # annualisé
+        return period, ancien_caah
