e83b8ce38abdb29d04619250918dd92b695327a7
Christophe Benz <christophe.benz@data.gouv.fr>
2015-09-15 14:52:33 +0200

    Release 0.5.2

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '0.5.2.dev0',
+    version = '0.5.2',
 
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
