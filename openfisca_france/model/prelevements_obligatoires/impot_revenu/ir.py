91ae066c9573937c14c114ced19fef5381015440
Adrien Pacifico <adrienpacifico@gmail.com>
2015-09-16 12:59:07 +0200

    Put reforms/plf2015.py into legislation. WIP still need a proper test on decote

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
@@ -1254,17 +1254,29 @@ class avantage_qf(SimpleFormulaColumn):
 
 
 @reference_formula
-class decote(SimpleFormulaColumn):
+class decote(DatedFormulaColumn):
     column = FloatCol(default = 0)
     entity_class = FoyersFiscaux
     label = u"décote"
-
+    @dated_function(start = date(2001, 1, 1), stop = date(2013, 12, 31))
     def function(self, simulation, period):
         period = period.start.offset('first-of', 'year').period('year')
         ir_plaf_qf = simulation.calculate('ir_plaf_qf', period)
         decote = simulation.legislation_at(period.start).ir.decote
 
         return period, (ir_plaf_qf < decote.seuil) * (decote.seuil - ir_plaf_qf) * 0.5
+        
+    @dated_function(start = date(2001, 1, 1), stop = date(2013, 12, 31))
+    def function(self, simulation, period):
+            period = period.start.offset('first-of', 'year').period('year')
+            ir_plaf_qf = simulation.calculate('ir_plaf_qf', period)
+            nb_adult = simulation.calculate('nb_adult', period)
+            decote_seuil_celib = simulation.legislation_at(period.start).ir.decote_seuil_celib
+            decote_seuil_couple = simulation.legislation_at(period.start).ir.decote_seuil_couple
+            decote_celib = (ir_plaf_qf < decote_seuil_celib) * (decote_seuil_celib - ir_plaf_qf)
+            decote_couple = (ir_plaf_qf < decote_seuil_couple) * (decote_seuil_couple - ir_plaf_qf)
+            
+            return period, (nb_adult == 1) * decote_celib + (nb_adult == 2) * decote_couple
 
 
 @reference_formula
