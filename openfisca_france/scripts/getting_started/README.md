7dc33dfaf2d9f66762a310359b43a7190f640dcf
Christophe Benz <christophe.benz@data.gouv.fr>
2015-10-28 15:43:57 +0100

    Add scripts from doc

---
+++ openfisca_france/scripts/getting_started/README.md
@@ -0,0 +1,6 @@
+# Getting started scripts
+
+These scripts are mentioned in the
+[getting started](http://www.openfisca.fr/documentation/getting-started.html)
+section of the
+[OpenFisca documentation](http://www.openfisca.fr/documentation).
---
+++ openfisca_france/scripts/getting_started/test1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from openfisca_france import init_country
+
+TaxBenefitSystem = init_country()
+tax_benefit_system = TaxBenefitSystem()
+
+year = 2015
+
+scenario = tax_benefit_system.new_scenario()
+scenario.init_single_entity(
+    period = year,
+    parent1 = dict(
+        salaire_de_base = 15000,
+        ),
+    enfants = [
+        dict(birth = datetime.date(year - 10, 1, 1)),
+        dict(birth = datetime.date(year - 12, 1, 1)),
+        dict(birth = datetime.date(year - 18, 1, 1)),
+        ],
+    )
+
+simulation = scenario.new_simulation()
+af = simulation.calculate('af', '2015-01')
+print af
