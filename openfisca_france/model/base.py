7f88d0585f6be725ba238953f585aba6f79e7137
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2015-12-02 19:36:29 +0100

    Clean base.py

--- openfisca_france/model/base.py
+++ openfisca_france/model/base.py
@@ -7,8 +7,7 @@ from openfisca_core.columns import (AgeCol, BoolCol, build_column, DateCol, Enum
     PeriodSizeIndependentIntCol, StrCol)
 from openfisca_core.enumerations import Enum
 from openfisca_core.formulas import (calculate_output_add, calculate_output_add_divide, calculate_output_divide,
-    dated_function, DatedVariable, EntityToPersonColumn, last_duration_last_value,
-    make_formula_decorator, missing_value, PersonToEntityColumn, reference_input_variable,
+    dated_function, DatedVariable, EntityToPersonColumn, last_duration_last_value, missing_value, PersonToEntityColumn, reference_input_variable,
     requested_period_added_value, requested_period_default_value, requested_period_last_value,
     set_input_dispatch_by_period, set_input_divide_by_period, Variable)
 
@@ -53,7 +52,6 @@ __all__ = [
     'QUIFAM',
     'QUIFOY',
     'QUIMEN',
-    'reference_formula',
     'reference_input_variable',
     'requested_period_added_value',
     'requested_period_default_value',
@@ -104,5 +102,3 @@ build_column = functools.partial(
     build_column,
     entity_class_by_symbol = entity_class_by_symbol,
     )
-
-reference_formula = make_formula_decorator(entity_class_by_symbol = entity_class_by_symbol)
