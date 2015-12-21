17d56ccbfa3cd90886450427516163ac7c43eb37
Emmanuel Raviart <emmanuel@raviart.com>
2015-12-21 15:32:22 +0100

    Restore first year to test legislation to 2006.

--- openfisca_france/tests/test_legislations.py
+++ openfisca_france/tests/test_legislations.py
@@ -67,5 +67,5 @@ def check_legislation_xml_file(year):
 
 
 def test_legislation_xml_file():
-    for year in range(2015, datetime.date.today().year + 1):
+    for year in range(2006, datetime.date.today().year + 1):
         yield check_legislation_xml_file, year
