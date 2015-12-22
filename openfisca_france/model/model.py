8231daf184229e8181717bb9c2b1eb6d64e1120e
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2015-12-22 14:49:44 +0100

    Add PCH to prestations

--- openfisca_france/model/model.py
+++ openfisca_france/model/model.py
@@ -45,6 +45,7 @@ from .prelevements_obligatoires.prelevements_sociaux import taxes_salaires_main_
 from .prestations import (  # noqa analysis:ignore
     aides_logement,
     education,
+    handicap,
     )
 
 from prestations.minima_sociaux import (  # noqa analysis:ignore
