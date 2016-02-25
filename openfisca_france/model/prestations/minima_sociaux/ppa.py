2784932a704a7b3db3779ea4c3d3e4e194e8650d
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-02-25 11:54:57 +0100

    Introduce ppa_montant_forfaitaire_familial_non_majore
    Introduce ppa_montant_forfaitaire_familial_majore

--- openfisca_france/model/prestations/minima_sociaux/ppa.py
+++ openfisca_france/model/prestations/minima_sociaux/ppa.py
@@ -21,3 +21,37 @@ class ppa_eligibilite(Variable):
 
         return period, elig
 
+class ppa_montant_forfaitaire_familial_non_majore(Variable):
+    column = FloatCol
+    entity_class = Familles
+    label = u"Montant forfaitaire familial (sans majoration)"
+
+    def function(self, simulation, period):
+        period = period.this_month
+        nb_parents = simulation.calculate('nb_par', period)
+        nb_enfants = simulation.calculate('nb_enfant_rsa', period)
+        ppa_majoree_eligibilite = simulation.calculate('rsa_majore_eligibilite', period)
+        rmi = simulation.legislation_at(period.start).minim.rmi
+        nb_personnes = nb_parents + nb_enfants
+
+        taux_non_majore = (
+            1 +
+            (nb_personnes >= 2) * rmi.txp2 +
+            (nb_personnes >= 3) * rmi.txp3 +
+            (nb_personnes >= 4) * where(nb_parents == 1, rmi.txps, rmi.txp3) + # Si nb_par == 1, pas de conjoint, la 4e personne est un enfant, donc le taux est de 40%.
+            max_(nb_personnes - 4, 0) * rmi.txps
+            )
+
+        return period, rmi.rmi * taux_non_majore
+
+class ppa_montant_forfaitaire_familial_majore(Variable):
+    column = FloatCol
+    entity_class = Familles
+    label = u"Montant forfaitaire familial (avec majoration)"
+
+    def function(self, simulation, period):
+        nb_enfants = simulation.calculate('nb_enfant_rsa', period)
+        rmi = simulation.legislation_at(period.start).minim.rmi
+        taux_majore = rmi.majo_rsa.pac0 + rmi.majo_rsa.pac_enf_sup * nb_enfants
+
+        return period, rmi.rmi * taux_majore
