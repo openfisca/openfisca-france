95b69de7d63c9d55056301d6813c5f7f9df6b162
Mael Thomas <mael.thomas@data.gouv.fr>
2016-03-08 12:05:43 +0100

    Add indemnite_fin_contrat to sum variables

--- openfisca_france/model/revenus/activite/salarie.py
+++ openfisca_france/model/revenus/activite/salarie.py
@@ -781,10 +781,13 @@ class salaire_super_brut_hors_allegements(Variable):
             'depense_cantine_titre_restaurant_employeur', period)
         reintegration_titre_restaurant_employeur = simulation.calculate(
             'reintegration_titre_restaurant_employeur', period)
+        indemnite_fin_contrat = simulation.calculate('indemnite_fin_contrat', period)
+
+
 
         salaire_super_brut_hors_allegements = (
             salaire_de_base + remuneration_principale + remuneration_apprenti +
-            primes_fonction_publique + indemnite_residence + supp_familial_traitement +
+            primes_fonction_publique + indemnite_residence + supp_familial_traitement + indemnite_fin_contrat +
             depense_cantine_titre_restaurant_employeur - reintegration_titre_restaurant_employeur
             - cotisations_employeur
             )
