f21c9f4541ff501124cdbd726148bc1d35c53eca
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-04-27 16:02:36 +0200

    Document travis decryption.
    And make release-tag.sh more robust.

--- release-tag.sh
+++ release-tag.sh
@@ -1,7 +1,11 @@
+# On travis CI, we use the openfisca_bot key. It is locally decrypted on the CI environment from openfisca_bot.enc before deploying.
+
 version=`python setup.py --version`
 eval "$(ssh-agent -s)" #start the ssh agent
-chmod 400 ./openfisca_bot
-ssh-add ./openfisca_bot
-git remote set-url origin git@github.com:openfisca/openfisca-france.git
+if [ -e openfisca_bot ]
+then
+	chmod 400 ./openfisca_bot
+	ssh-add ./openfisca_bot
+fi
 git tag $version
-git push --tags
+git push git@github.com:openfisca/openfisca-france.git --tags
