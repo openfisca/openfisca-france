f459cb370dc88ec670bfa3fdcd1c00661cf95279
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-04-27 14:52:08 +0200

    New version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '1.2.dev1',
+    version = '1.2.dev5',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
