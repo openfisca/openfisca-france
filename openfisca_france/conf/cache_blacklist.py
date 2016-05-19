26b6a71a9e29a871460153aab5cb358c1996970f
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-05-19 16:17:05 +0200

    Blacklist new AL variables

--- openfisca_france/conf/cache_blacklist.py
+++ openfisca_france/conf/cache_blacklist.py
@@ -2,4 +2,10 @@
 # The following variables are intermadiate results and do not need to be cached in those usecases.
 
 cache_blacklist = set([
+	'aide_logement_loyer_retenu',
+	'aide_logement_charges',
+	'aide_logement_R0',
+	'aide_logement_taux_famille',
+	'aide_logement_taux_loyer',
+	'aide_logement_participation_personelle'
 ])
