07855674dac6cbaf5eec052507cc90084dc6e953
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-07-12 20:04:56 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '4.0.4',
+    version = '4.0.5',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
