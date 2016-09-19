4e1dba1168a2b7b3dd6332e64e8d9ae048957ec6
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-09-19 20:36:35 +0200

    Add start and stop instead of period in update_legislation

--- openfisca_france/reforms/plf2015.py
+++ openfisca_france/reforms/plf2015.py
@@ -2,27 +2,26 @@
 
 from __future__ import division
 
-from openfisca_core import columns, periods
+from openfisca_core import periods
 from openfisca_core.reforms import Reform, update_legislation
-from openfisca_core.variables import Variable
 from ..model.base import *
-from ..model.prelevements_obligatoires.impot_revenu import ir
+
 
 def modify_legislation_json(reference_legislation_json_copy):
     reform_legislation_subtree = {
         "@type": "Node",
-        "description": "PLF 2015 sur revenus 2013 (Décote)",
+        "description": u"PLF 2015 sur revenus 2013 (Décote)",
         "children": {
             "seuil_celib": {
                 "@type": "Parameter",
-                "description": "Seuil de la décote pour un célibataire",
+                "description": u"Seuil de la décote pour un célibataire",
                 "format": "integer",
                 "unit": "currency",
                 "values": [{'start': u'2013-01-01', 'stop': u'2013-12-31', 'value': 1135}],
                 },
             "seuil_couple": {
                 "@type": "Parameter",
-                "description": "Seuil de la décote pour un couple",
+                "description": u"Seuil de la décote pour un couple",
                 "format": "integer",
                 "unit": "currency",
                 "values": [{'start': u'2013-01-01', 'stop': u'2013-12-31', 'value': 1870}],
@@ -35,18 +34,21 @@ def modify_legislation_json(reference_legislation_json_copy):
     reference_legislation_json_copy = update_legislation(
         legislation_json = reference_legislation_json_copy,
         path = ('children', 'impot_revenu', 'children', 'bareme', 'brackets', 1, 'rate'),
-        period = reform_period,
+        start = reform_period.start,
+        stop = reform_period.stop,
         value = 0,
         )
     reference_legislation_json_copy = update_legislation(
         legislation_json = reference_legislation_json_copy,
         path = ('children', 'impot_revenu', 'children', 'bareme', 'brackets', 2, 'threshold'),
-        period = reform_period,
+        start = reform_period.start,
+        stop = reform_period.stop,
         value = 9690,
         )
 
     reference_legislation_json_copy['children']['plf2015'] = reform_legislation_subtree
     return reference_legislation_json_copy
+
 
 class decote(DatedVariable):
     label = u"Décote IR 2015 appliquée sur IR 2014 (revenus 2013)"
@@ -62,8 +64,9 @@ class decote(DatedVariable):
         decote_couple = (ir_plaf_qf < plf.seuil_couple) * (plf.seuil_couple - ir_plaf_qf)
         return period, (nb_adult == 1) * decote_celib + (nb_adult == 2) * decote_couple
 
+
 class plf2015(Reform):
-    name = u'Projet de Loi de Finances 2015 appliquée aux revenus 2013'
+    name = u"Projet de Loi de Finances 2015 appliquée aux revenus 2013"
 
     def apply(self):
         self.update_variable(decote)
