2d4f64a3e406d8a403674ce8f35d5007fcead484
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-10-13 15:08:29 +0200

    First working tests for APA

--- openfisca_france/model/prestations/autonomie.py
+++ openfisca_france/model/prestations/autonomie.py
@@ -3,9 +3,23 @@
 
 from __future__ import division
 
-from numpy import maximum as max_, minimum as min_, zeros
+from numpy import maximum as max_, minimum as min_, zeros, ones
 
 from openfisca_france.model.base import *  # noqa analysis:ignore
+
+
+# TODO: fill the parameters file. May be should use the majoration pour tierce personne as parameter
+montant_mensuel_maximum_by_gir = {
+    1: 1312.67,
+    2: 1125.14,
+    3: 843.86,
+    4: 562.57,
+    }
+seuil_non_versement = 28.83
+apa_seuil_1 = .67
+apa_seuil_2 = 2.67
+majoration_tierce_personne = 1103.08
+taux_max_participation = .9
 
 
 class apa_domicile(Variable):
@@ -18,37 +32,29 @@ class apa_domicile(Variable):
         gir = simulation.calculate('gir', period)
         base_ressources_apa = simulation.calculate('base_ressources_apa', period)
         dependance_plan_aide_domicile = simulation.calculate('dependance_plan_aide_domicile', period)
-
-        # TODO: fill the parameters file. May be should use the majoration pour tierce personne as parameter
-        montant_mensuel_maximum_by_gir = dict(
-            gir1 = 1312.67,
-            gir2 = 1125.14,
-            gir3 = 843.86,
-            gir4 = 562.57,
-            )
-        seuil_non_versement = 28.83
-        apa_seuil_1 = 2437.81
-        apa_seuil_2 = 3750.48
-
-        dependance_plan_aide_domicile_maximal = zeros(self.holder.entity.count)
-        girs = ['gir' + i for i in rang(1, 5)]
-        for target_gir in girs:
-            dependance_plan_aide_domicile_maximal = dependance_plan_aide_domicile_maximal + (gir == target_gir) * max_(
-                dependance_plan_aide_domicile,
-                montant_mensuel_maximum_by_gir[target_gir]
+        dependance_plan_aide_domicile_accepte = zeros(self.holder.entity.count)
+        for target_gir in range(1, 5):
+            dependance_plan_aide_domicile_accepte = (
+                dependance_plan_aide_domicile_accepte +
+                (gir == target_gir) * min_(
+                    dependance_plan_aide_domicile,
+                    montant_mensuel_maximum_by_gir[target_gir]
+                    )
                 )
 
-        participation_beneficiaire = dependance_plan_aide_domicile_maximal * .9 * (
-            (base_ressources_apa <= apa_seuil_1) +
-            min_(
-                max_(
-                    (base_ressources_apa - apa_seuil_1) / (apa_seuil_2 - apa_seuil_1),
-                    0,
-                    ),
-                1,
+        participation_beneficiaire = (
+            dependance_plan_aide_domicile_accepte *
+            taux_max_participation * (
+                min_(
+                    max_(
+                        (base_ressources_apa / majoration_tierce_personne - apa_seuil_1) / (apa_seuil_2 - apa_seuil_1),
+                        0,
+                        ),
+                    1,
+                    )
                 )
             )
-        apa = dependance_plan_aide_domicile_maximal - participation_beneficiaire
+        apa = dependance_plan_aide_domicile_accepte - participation_beneficiaire
         return period, apa * (apa >= seuil_non_versement)
 
 
@@ -65,17 +71,6 @@ class apa_etablissement(Variable):
         dependance_tarif_etablissement_gir_dependant = simulation.calculate(
             'dependance_tarif_etablissement_gir_dependant', period)
 
-        # TODO: fill the parameters file. May be should use the majoration pour tierce personne as parameter
-        montant_mensuel_maximum_by_gir = dict(
-            gir1 = 1312.67,
-            gir2 = 1125.14,
-            gir3 = 843.86,
-            gir4 = 562.57,
-            )
-        seuil_non_versement = 28.83
-        apa_seuil_1 = 2437.81
-        apa_seuil_2 = 3750.48
-
         participation_beneficiaire = (
             dependance_tarif_etablissement_gir_5_6 +
             dependance_tarif_etablissement_gir_dependant * (
@@ -90,15 +85,19 @@ class apa_etablissement(Variable):
                 )
             )
         apa = zeros(self.holder.entity.count)
-        girs = ['gir' + i for i in rang(1, 5)]
-        for target_gir in girs:
+        for target_gir in range(1, 5):
             apa = apa + (gir == target_gir) * max_(
-                dependance_tarif_etablissement_gir_5_6 + dependance_tarif_etablissement_gir_dependant
-                - participation_beneficiaire,
+                dependance_tarif_etablissement_gir_5_6 +
+                dependance_tarif_etablissement_gir_dependant - participation_beneficiaire,
                 montant_mensuel_maximum_by_gir[target_gir]
                 )
 
-        return period, apa * (apa >= seuil_non_versement)
+        eligibilite_etablissement = (
+            (dependance_tarif_etablissement_gir_5_6 > 0) *
+            (dependance_tarif_etablissement_gir_dependant > 0)
+            )
+
+        return period, apa * (apa >= seuil_non_versement) * eligibilite_etablissement
 
 
 class base_ressources_apa(Variable):
@@ -107,14 +106,14 @@ class base_ressources_apa(Variable):
     entity_class = Familles
 
     def function(self, simulation, period):
-        return zeros(self.holder.entity.count)
+        return period, zeros(self.holder.entity.count)
 
 
 class gir(Variable):
     column = EnumCol(
         enum = Enum(
             [
-                u"Non pertinent",
+                u"Non défini",
                 u"Gir 1",
                 u"Gir 2",
                 u"Gir 3",
