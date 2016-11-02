2d82511e830b7b3e6c2fe516164c7fcebec475da
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-02 17:10:46 +0100

    Add missing import daetime64 in rsa.py

--- openfisca_france/model/prestations/minima_sociaux/rsa.py
+++ openfisca_france/model/prestations/minima_sociaux/rsa.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 import datetime
 
-from numpy import (floor, logical_and as and_, logical_not as not_, logical_or as or_, maximum as max_, minimum as min_, select, where)
+from numpy import (datetime64, floor, logical_and as and_, logical_not as not_, logical_or as or_, maximum as max_, minimum as min_, select, where)
 
 from openfisca_france.model.base import *  # noqa analysis:ignore
 from openfisca_france.model.prestations.prestations_familiales.base_ressource import nb_enf, age_en_mois_benjamin
