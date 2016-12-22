91b11c8b745c7e6a6e1f7d538b75695058235015
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-12-22 12:14:38 +0100

    Fix end_date apje, ape

--- openfisca_france/model/prestations/prestations_familiales/paje.py
+++ openfisca_france/model/prestations/prestations_familiales/paje.py
@@ -495,13 +495,12 @@ class ape_avant_cumul(Variable):
     column = FloatCol(default = 0)
     entity = Famille
     label = u"Allocation parentale d'éducation, avant prise en compte de la non-cumulabilité avec le CF et l'APJE"
-    stop_date = date(2004, 1, 1)
+    stop_date = date(2003, 12, 31)
     url = "http://fr.wikipedia.org/wiki/Allocation_parentale_d'%C3%A9ducation_en_France"
 
     def function(famille, period, legislation):
         '''
         Allocation parentale d'éducation
-        'fam'
 
         L’allocation parentale d’éducation s’adresse aux parents qui souhaitent arrêter ou
         réduire leur activité pour s’occuper de leurs jeunes enfants, à condition que ceux-ci
@@ -557,7 +556,7 @@ class apje_avant_cumul(Variable):
     column = FloatCol
     entity = Famille
     label = u"Allocation pour le jeune enfant, avant prise en compte de la non-cumulabilité avec le CF et l'APE"
-    stop_date = date(2004, 1, 1)
+    stop_date = date(2003, 12, 31)
     url = "http://vosdroits.service-public.fr/particuliers/F2552.xhtml"
 
     def function(famille, period, legislation):
@@ -565,7 +564,7 @@ class apje_avant_cumul(Variable):
         Allocation pour jeune enfant
         '''
         period = period.this_month
-        base_ressources = famille('prestations_familFiales_base_ressources', period.this_month)
+        base_ressources = famille('prestations_familiales_base_ressources', period.this_month)
         biactivite = famille('biactivite', period, options = [ADD])
         isole = not_(famille('en_couple', period))
         P = legislation(period).prestations.prestations_familiales
@@ -587,7 +586,7 @@ class apje_avant_cumul(Variable):
 
         # Pour bénéficier de cette allocation, il faut que tous les enfants du foyer soient nés, adoptés, ou recueillis
         # en vue d’une adoption avant le 1er janvier 2004, et qu’au moins l’un d’entre eux ait moins de 3 ans.
-        # Cette allocation est verséE du 5��me mois de grossesse jusqu���au mois précédant le 3ème anniversaire de
+        # Cette allocation est versée du 5ème mois de grossesse jusqu'au mois précédant le 3ème anniversaire de
         # l’enfant.
 
         # Non cumul APE APJE CF
@@ -601,7 +600,7 @@ class ape(Variable):
     column = FloatCol(default = 0)
     entity = Famille
     label = u"Allocation parentale d'éducation"
-    stop_date = date(2004, 1, 1)
+    stop_date = date(2003, 12, 31)
     url = "http://fr.wikipedia.org/wiki/Allocation_parentale_d'%C3%A9ducation_en_France"
 
     def function(famille, period):
@@ -621,7 +620,7 @@ class apje(Variable):
     column = FloatCol(default = 0)
     entity = Famille
     label = u"Allocation pour le jeune enfant"
-    stop_date = date(2004, 1, 1)
+    stop_date = date(2003, 12, 31)
     url = "http://vosdroits.service-public.fr/particuliers/F2552.xhtml"
 
     def function(famille, period):
