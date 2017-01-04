a45fbad6dd79ec93be1ba1314ade606805b562de
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-01-04 15:33:59 +0100

    Fixup a2d77c167ea8ae3a62183a56b10cd121dc476481

--- openfisca_france/conf/cache_blacklist.py
+++ openfisca_france/conf/cache_blacklist.py
@@ -2,12 +2,12 @@
 # The following variables are intermadiate results and do not need to be cached in those usecases.
 
 cache_blacklist = set([
-	'aide_logement_loyer_retenu',
-	'aide_logement_charges',
-	'aide_logement_R0',
-	'aide_logement_taux_famille',
-	'aide_logement_taux_loyer',
-	'aide_logement_participation_personelle',
+    'aide_logement_loyer_retenu',
+    'aide_logement_charges',
+    'aide_logement_R0',
+    'aide_logement_taux_famille',
+    'aide_logement_taux_loyer',
+    'aide_logement_participation_personelle',
     'aide_logement_loyer_seuil_degressivite',
     'aide_logement_loyer_seuil_suppression',
     'aide_logement_montant_brut_avant_degressivite',
