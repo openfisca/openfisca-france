c89dee1312115c29960a6fd288bdf7456c4d9a6a
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-02-02 20:37:53 +0100

    Fix typo

--- openfisca_france/model/revenus/capital/foncier.py
+++ openfisca_france/model/revenus/capital/foncier.py
@@ -90,7 +90,7 @@ class f4bl(Variable):
 
 # Variables utilisées par mes aides
 class revenus_locatifs(Variable):
-    base_function = request_period_added_value
+    base_function = requested_period_added_value
     column = FloatCol
     entity = Individu
     label = u"Revenus locatifs"
