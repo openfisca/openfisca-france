525ba73d1886b7293413fa50d0dc4dc68dea10b7
Christophe Benz <christophe.benz@data.gouv.fr>
2017-02-16 19:57:45 +0100

    Update dependency to Core

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'Babel >= 0.9.4',
         'Biryani[datetimeconv] >= 0.10.4',
         'numpy >= 1.11',
-        'OpenFisca-Core >= 4.3.5, < 5.0',
+        'OpenFisca-Core >= 5.0.0, < 6.0',
         'PyYAML >= 3.10',
         'requests >= 2.8',
         ],
