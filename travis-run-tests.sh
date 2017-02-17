6e50ee8dcd89708c4f811bdd08a38f3f559e5c31
Christophe Benz <christophe.benz@data.gouv.fr>
2017-02-17 09:20:39 +0100

    Remove script header to match the same script on Core

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -1,12 +1,5 @@
 #! /usr/bin/env bash
 
-# This script is the entry point for the Travis tests platform.
-# It first checks that the package version has been updated ("bump" in the jargon),
-# then it runs tests via "make test".
-
-
-set -x
-
 current_version=`python setup.py --version`
 
 if ! git diff-index --quiet master openfisca_france
