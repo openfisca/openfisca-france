4e58eb4ed470748dd2bf657c2618444a154ce4c2
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-02-21 20:50:43 +0100

    Improve args checking in draw_bareme

--- openfisca_france/visualizations/charts.py
+++ openfisca_france/visualizations/charts.py
@@ -34,6 +34,7 @@ def draw_waterfall(simulation, axes = None, decomposition_json = None, visible =
 
 def draw_bareme(simulation, axes = None, x_axis = None, reference_simulation = None, decomposition_json = None,
         visible_lines = None, hide_all = False, legend = True, legend_position = None):
+    assert x_axis is not None
     if axes is None:
         fig = plt.figure()
         axes = fig.gca()
