85c0b81e3235e4e8ff4390a3323bc4008d1ae23a
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-03-02 16:07:03 +0100

    Remove hook for taille_entreprise

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/travail_prive.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/travail_prive.py
@@ -758,7 +758,6 @@ class taille_entreprise(Variable):
     label = u"Catégorie de taille d'entreprise"
     url = u"http://www.insee.fr/fr/themes/document.asp?ref_id=ip1321"
     definition_period = MONTH
-    calculate_output = calculate_output_first_month
 
     def function(self, simulation, period):
         effectif_entreprise = simulation.calculate('effectif_entreprise', period)
