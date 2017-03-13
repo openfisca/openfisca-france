3064e2ab431f2d373bc005cfbfd583961b6070c3
Laurent Bossavit <laurent.git@bossavit.com>
2017-03-13 11:39:59 +0100

    Complete test, implement formula, add cases

--- openfisca_france/model/caracteristiques_socio_demographiques/logement.py
+++ openfisca_france/model/caracteristiques_socio_demographiques/logement.py
@@ -34,13 +34,6 @@ class depcom(Variable):
     definition_period = MONTH
     set_input = set_input_dispatch_by_period
 
-class depcom(Variable):
-    column = FixedStrCol(max_length = 5)
-    entity = FoyerFiscal
-    label = u"Code INSEE (depcom) du lieu de résidence"
-    definition_period = MONTH
-    set_input = set_input_dispatch_by_period
-
 class charges_locatives(Variable):
     column = FloatCol
     entity = Menage
--- openfisca_france/model/prestations/anah/eligibilite_anah.py
+++ openfisca_france/model/prestations/anah/eligibilite_anah.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from numpy import (select)
 from openfisca_france.model.base import *  # noqa analysis:ignore
 
 class eligibilite_anah(Variable):
-    column = StrCol
-    entity = FoyerFiscal
+    column = EnumCol(
+        enum = Enum([
+            u"Très modestes",
+            u"Modestes",
+            u"A vérifier"
+        ])
+    )
+    entity = Menage
     label = u"Barème d'éligibilité aux aides ANAH"
     definition_period = YEAR
 
-    def function(foyer, period):
-        rfr = foyer('rfr',period)
-        return select([rfr >= 0],["a_verifier"])
+    def function(menage, period):
+        departement = menage('depcom',period.first_month).astype(int) / 1000
+        in_idf = (departement == 75) + (departement == 77) # TODO: compléter
+
+        rfr = menage.personne_de_reference.foyer_fiscal('rfr', period)
+        nb_members = menage.nb_persons()
+
+        bareme_idf = select(
+            [nb_members==1,nb_members>1],
+            [select([rfr <= 19875,rfr <= 24194, rfr>0],[0,1,2]),
+             select([rfr <= 29171,rfr <= 35510, rfr>0],[0,1,2])])
+        bareme_out = select([rfr <= 14360,rfr <= 18409, rfr>0],[0,1,2])
+
+        return where(in_idf,bareme_idf,bareme_out)
