1eb081c3491340f0c4c8b4fa0a5294ea7baebf5a
Laurent Bossavit <laurent.git@bossavit.com>
2017-03-14 15:31:30 +0100

    Bump minor version and add to CHANGELOG

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '18.4.2',
+    version = '18.5.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
