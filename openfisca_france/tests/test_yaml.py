d8727d3f556e6479a84503021cd4ee49ef2b2645
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-04-14 13:29:05 +0200

    Simplify france test runner

--- openfisca_france/tests/test_yaml.py
+++ openfisca_france/tests/test_yaml.py
@@ -6,32 +6,13 @@ from nose.tools import nottest
 
 from openfisca_core.tools.test_runner import generate_tests
 
-from openfisca_france.tests import base
+from openfisca_france.tests.base import tax_benefit_system
 
 nottest(generate_tests)
 
-options_by_dir = {
-    # 'calculateur_impots': {
-    #     'default_absolute_error_margin': 0.5,
-    #     'reforms' : ['inversion_revenus'],
-    #     },
-    'fiches_de_paie': {},
-    'formulas': {},
-    'mes-aides.gouv.fr': {},
-    'ui.openfisca.fr': {},
-    'scipy': {},
-}
-
-
 def test():
-    for directory, options in options_by_dir.iteritems():
-        path = os.path.abspath(os.path.join(os.path.dirname(__file__), directory))
+    tests_directory = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    test_generator = generate_tests(tax_benefit_system, tests_directory)
 
-        if not options.get('default_relative_error_margin') and not options.get('default_absolute_error_margin'):
-            options['default_absolute_error_margin'] = 0.005
-
-        test_generator = generate_tests(base.tax_benefit_system, path, options)
-
-        for test in test_generator:
-            yield test
-
+    for test in test_generator:
+        yield test
