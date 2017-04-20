eb4ccf52a024285969fb9cbcf765d3bf8bee6ca7
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-04-20 11:18:00 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '17.0.1',
+    version = '17.1.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
