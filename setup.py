62350b7d8c0d53ad211d5afad590f7d867503093
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-05-19 16:38:28 +0200

    [TO REVERT ]Check API deployment

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '18.2.2',
+    version = '18.2.2.dev2',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
