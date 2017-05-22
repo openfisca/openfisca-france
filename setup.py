a5836ff9c4405bf86e070f8cfc59f3db8bc0afab
Anna-Livia <annalivia.cardin@gmail.com>
2017-05-22 13:25:50 +0200

    retirer_i18n#737

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     url = 'https://github.com/openfisca/openfisca-france',
 
     data_files = [
-        ('share/locale/fr/LC_MESSAGES', ['openfisca_france/i18n/fr/LC_MESSAGES/openfisca-france.mo']),
         ('share/openfisca/openfisca-france', ['CHANGELOG.md', 'LICENSE.AGPL.txt', 'README.md']),
         ],
     extras_require = {
