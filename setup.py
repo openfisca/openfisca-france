3d1aa0f01ef35d255fefea1b7861207e4353f3ab
Michel Blancard <michel.blancard@data.gouv.fr>
2017-06-07 14:07:21 +0200

    Bump version + Changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '18.3.1',
+    version = '18.4.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
@@ -51,7 +51,7 @@ setup(
     install_requires = [
         'Biryani[datetimeconv] >= 0.10.4',
         'numpy >= 1.11',
-        'OpenFisca-Core >= 12.2.0, < 13.0',
+        'OpenFisca-Core >= 13.0.0, < 14.0',
         'PyYAML >= 3.10',
         'requests >= 2.8',
         ],
