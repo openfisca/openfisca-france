8783309f6d54a982ff65cb21c4a8c2c2bfc969e7
Michel Blancard <michel.blancard@data.gouv.fr>
2017-06-08 11:54:00 +0200

    Version + Changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '18.5.3',
+    version = '18.6.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
@@ -52,7 +52,7 @@ setup(
     install_requires = [
         'Biryani[datetimeconv] >= 0.10.4',
         'numpy >= 1.11, < 1.13',
-        'OpenFisca-Core >= 14.1.1, < 15.0',
+        'OpenFisca-Core >= 15.0.0, < 16.0',
         'PyYAML >= 3.10',
         'requests >= 2.8',
         ],
