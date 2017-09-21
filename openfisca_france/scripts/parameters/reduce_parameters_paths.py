2624f3e8892e308b3d0442d00bf6c23533b1a803
sandcha <sandcha@users.noreply.github.com>
2017-09-21 07:37:18 +0200

    Append long files contents to index.yaml in _parent_ directory

--- openfisca_france/scripts/parameters/reduce_parameters_paths.py
+++ openfisca_france/scripts/parameters/reduce_parameters_paths.py
@@ -2,8 +2,8 @@
 
 import os
 import shutil
-import sys
 import yaml
+
 
 PARENT_DIRECTORY = os.path.abspath('../..')
 PATH_LENGTH_TO_IGNORE = len(PARENT_DIRECTORY)
@@ -15,6 +15,13 @@ CLEANED_PARAMETERS_DIRECTORY = os.path.join(PARENT_DIRECTORY, 'new_parameters')
 INDEX_FILENAME = 'index.yaml'
 
 
+def clean_from_filename(relative_path):
+    last_separator_index = relative_path.rfind(os.path.sep)
+    if last_separator_index == -1:
+        raise ValueError(u'Directory expected but none found in: ' + relative_path)
+    return relative_path[:last_separator_index]
+
+
 def list_long_paths(absolute_directory_path):
     long_paths = []
 
@@ -22,6 +29,7 @@ def list_long_paths(absolute_directory_path):
         for f in files:
             file_path = os.path.join(directory, f)
             if (len(file_path) - PATH_LENGTH_TO_IGNORE) > PATH_MAX_LENGTH:
+                # print str((len(file_path) - PATH_LENGTH_TO_IGNORE)) + " " + file_path
                 path_extract = clean_from_filename(get_sub_parent_path(file_path, PARAMETERS_DIRECTORY))
                 if path_extract not in long_paths:
                     long_paths.append(path_extract)
@@ -34,61 +42,100 @@ def get_sub_parent_path(directory_path, parent_path):
     return os.path.relpath(absolute_path, parent_path)
 
 
-def clean_from_filename(relative_path):
-    last_separator_index = relative_path.rfind(os.path.sep)
-    if last_separator_index == -1:
-        raise ValueError(u'Directory expected but none found in: ' + relative_path)
-    return relative_path[:last_separator_index]
+def get_raw_content(yaml_filepath):
+    with open(yaml_filepath, 'r') as stream:
+        return yaml.load(stream)
 
 
-def merge(directory, fList, destination_path):
-    print os.linesep
-    print "Merging in: " + destination_path
-    if not fList:
-        return []
+def harvest(item):
+    '''
+    Put into a dictionary the content of the file hierarchy starting at the given item.
 
-    with open(destination_path, 'a') as yaml_file:
-        for f in fList:
-            filepath = os.path.join(directory, f)
-            with open(filepath, 'r') as stream:
-                filename = os.path.splitext(os.path.basename(f))[0]
-                fDict = {filename: None}
-                fDict[filename] = yaml.load(stream)
-                yaml_file.write(yaml.dump(fDict, default_flow_style=False))
+    An 'index.yaml' file content is directly added to the current dictionary key.
+    Any other file or a directory content in stored under a new key (file or directory name).
 
+    @param item: A directory of yaml files or a yaml file.
+    '''
+    content = {}
+    item_name = os.path.basename(item)
 
-def new_parameters_directory(old_directory, new_directory, paths_to_clean):
-    # Clean directory content
-    if os.path.exists(new_directory):
-        shutil.rmtree(new_directory)
-    os.mkdir(new_directory)
+    if os.path.isdir(item):
+        sub_items = os.listdir(item)
+        sub_content = {}
 
+        # Harvest index.yaml first if it exists
+        index_content = None
+        if INDEX_FILENAME in sub_items:
+            index_content = harvest(os.path.join(item, INDEX_FILENAME))
+            sub_items.remove(INDEX_FILENAME)
 
-    # Loop on directories
-    for directory, sub_directories, files in os.walk(old_directory):
-        functional_path = get_sub_parent_path(directory, old_directory)
-        new_path = os.path.join(new_directory, functional_path)
+        for subitem in sub_items:
+            sub_content.update(harvest(os.path.join(item, subitem)))
 
-        if functional_path in paths_to_clean:
-            # If current directory is too long, create directory but merge its content in one file
-            old_index = os.path.join(directory, INDEX_FILENAME)
-            new_index = os.path.join(new_path, INDEX_FILENAME)
-            # Keep index.yaml
-            shutil.copyfile(old_index, new_index)
-            # Merge all in index.yaml
-            files.remove(INDEX_FILENAME)
-            merge(directory, files, new_index)
+        # Add index content on top
+        if index_content:
+            sub_content.update(index_content)
+
+        content[item_name] = sub_content
+
+    else:
+        if item_name == INDEX_FILENAME:
+            content.update(get_raw_content(item))
         else:
-            # If current directory isn't too long, don't change it
-            if not os.path.exists(new_path):
-                shutil.copytree(directory, new_path)
+            # Clean file name from extension
+            item_name = os.path.splitext(item_name)[0]
+            # Store content under cleaned file name section
+            content[item_name] = get_raw_content(item)
+
+    return content
+
+
+def parse_and_clean(directory, paths_to_clean):
+    items = os.listdir(directory)
+
+    # Copy index.yaml first when it exists
+    parent_new_index_path = None
+    if INDEX_FILENAME in items:
+        index_path = os.path.join(directory, INDEX_FILENAME)
+        functional_path = get_sub_parent_path(index_path, PARAMETERS_DIRECTORY)
+
+        parent_new_index_path = os.path.join(CLEANED_PARAMETERS_DIRECTORY, functional_path)
+        shutil.copyfile(index_path, parent_new_index_path)
+        items.remove(INDEX_FILENAME)
+
+    # Parse other items in directory and harvest content for long paths
+    for item in items:
+        item_path = os.path.join(directory, item)
+        functional_path = get_sub_parent_path(item_path, PARAMETERS_DIRECTORY)
+
+        if os.path.isdir(item_path):
+            if functional_path in paths_to_clean:
+                # Check if parent directory has an index.yaml file
+                if parent_new_index_path is None:
+                    # No harvesting when no destination file is identified
+                    print INDEX_FILENAME + "expected. Not found in: " + directory
+
+                else:
+                    print os.linesep + "Cleaning long directory into: " + parent_new_index_path
+                    content = harvest(item_path)
+                    # Append harvested content to parent index.yaml
+                    with open(parent_new_index_path, 'a') as yaml_file:
+                        yaml_file.write(yaml.dump(content, default_flow_style=False))
+            else:
+                # Directory to keep unchanged
+                os.mkdir(os.path.join(CLEANED_PARAMETERS_DIRECTORY, functional_path))
+                parse_and_clean(item_path, paths_to_clean)
+
+        else:
+            shutil.copyfile(item_path, os.path.join(CLEANED_PARAMETERS_DIRECTORY, functional_path))
 
 
 long_parameters_paths = list_long_paths(PARAMETERS_DIRECTORY)
 print str(len(long_parameters_paths)) + " directories have files with more than " + str(PATH_MAX_LENGTH) + \
     " characters in their paths starting from this directory: " + PARENT_DIRECTORY
 
-new_parameters_directory(PARAMETERS_DIRECTORY, CLEANED_PARAMETERS_DIRECTORY, long_parameters_paths)
-# m = merge(["../../parameters/impot_revenu/plus_values/imposition_des_plus_values_mobilieres_levees_d_options_attribuees_depuis_27_4_2000_cessions_realisees_moins_de_deux_ans_apres_la_levee/index.yaml",
-#              "../../parameters/impot_revenu/plus_values/imposition_des_plus_values_mobilieres_levees_d_options_attribuees_depuis_27_4_2000_cessions_realisees_moins_de_deux_ans_apres_la_levee/seuil_tranche_superieure.yaml"
-# ], "./toto.yaml")
+
+if os.path.exists(CLEANED_PARAMETERS_DIRECTORY):
+    shutil.rmtree(CLEANED_PARAMETERS_DIRECTORY)
+os.mkdir(CLEANED_PARAMETERS_DIRECTORY)
+parse_and_clean(PARAMETERS_DIRECTORY, long_parameters_paths)
