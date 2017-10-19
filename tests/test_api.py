b0202e8882f792feb041070baff7370cacf73751
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-10-19 22:59:43 +0200

    Test france compatibility with the new API

--- tests/test_api.py
+++ tests/test_api.py
@@ -19,3 +19,18 @@ class TestOldApi(TestCase):
             subprocess.check_call(['wget', '--quiet',  '--retry-connrefused', '--waitretry=1', '--tries=10', 'http://localhost:2000', '--output-document=/dev/null'])
         except subprocess.CalledProcessError:
             raise subprocess.CalledProcessError("Could not reach OpenFisca Web API at localhost:2000 after 10s")
+
+
+class TestNewApi(TestCase):
+
+    def setUp(self):
+        self.process = subprocess.Popen(['openfisca', 'serve'])
+
+    def tearDown(self):
+        self.process.terminate()
+
+    def test_response(self):
+        try:
+            subprocess.check_call(['wget', '--quiet',  '--retry-connrefused', '--waitretry=1', '--tries=10', 'http://localhost:6000/parameters', '--output-document=/dev/null'])
+        except subprocess.CalledProcessError:
+            raise subprocess.CalledProcessError("Could not reach OpenFisca Web API at localhost:6000 after 10s")
