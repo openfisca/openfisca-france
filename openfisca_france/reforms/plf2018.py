6f5038c6b05abf04af086c02ac46b921fe655ceb
Giuliano de Franchis <giuliano.defranchis@gmail.com>
2017-11-19 13:50:24 +0100

    mise à 0 des paramètres de la paje qui valaient null, et quibloquaient l'exécution du programme en 2018

--- openfisca_france/reforms/plf2018.py
+++ openfisca_france/reforms/plf2018.py
@@ -12,20 +12,17 @@ dir_path = os.path.join(os.path.dirname(__file__), 'parameters')
 
 
 def modify_parameters(parameters):
-    reform_year = 2017
+    reform_year = 2018
     reform_period = period(reform_year)
     print(reform_period)
 
-    # file_path = os.path.join(dir_path, 'plf2018.yaml')
-    # reform_parameters_subtree = load_parameter_file(name='plf2018', file_path=file_path)
-    # parameters.add_child('plf2018', reform_parameters_subtree)
+    parameters.prelevements_sociaux.cotisations_sociales.chomage.salarie[0].rate.update(period=reform_period, value=0.0)
     parameters.prelevements_sociaux.contributions.csg.activite.deductible.taux.update(period=reform_period, value=0.068)
     parameters.prelevements_sociaux.contributions.csg.retraite.deductible.taux_plein.update(period=reform_period, value=0.068)
     parameters.prestations.minima_sociaux.aspa.montant_annuel_seul.update(period=reform_period, value=(9638.42+100*12))
     parameters.prestations.minima_sociaux.aspa.montant_annuel_couple.update(period=reform_period, value=(14963.65+100*12))
     parameters.prestations.minima_sociaux.rsa.montant_de_base_du_rsa.update(period=reform_period, value=(545.48+20))
-    # parameters.cotsoc.sal.commun.maladie[0].rate.update(period=reform_period, value=0.0)
-    # parameters.prelevements_sociaux.cotisations_sociales.chomage.salarie[0].rate.update(period=reform_period, value=0.0)
+    parameters.cotsoc.sal.commun.maladie[0].rate.update(period=reform_period, value=0.10)
 
     return parameters
 
