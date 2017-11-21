9be23a04535e7cb513ca8b24730e1a05e7642b92
Claire Leroy <cleroy@pse.local>
2017-11-21 15:19:45 +0100

    Update json_to_yaml()

--- openfisca_france/scripts/calculateur_impots/step2_convert_json_to_yaml.py
+++ openfisca_france/scripts/calculateur_impots/step2_convert_json_to_yaml.py
@@ -5,7 +5,6 @@ import collections
 import logging
 import os
 
-from lxml import etree
 from openfisca_core import conv
 import yaml
 
@@ -44,17 +43,11 @@ def json_to_yaml(json_dir, json_filename, var, output_dir):
         data = conv.check(input_to_json_data)(json_file.read()) 
 
     scenario = data['scenario']
-    tax_calculator_inputs = base.transform_scenario_to_tax_calculator_inputs(scenario)
-    tax_calculator_outputs = collections.OrderedDict() # float ou int à voir    
-    for variable in data['resultat_officiel'].iteritems():
-        code = variable[0]
-        if code != "Annee" and code != "Type":
-            tax_calculator_outputs[code] = variable[1]['value'] 
 
     log.info(u"Creating the YAML test from file {}...".format(json_filename))
     test = collections.OrderedDict((
             ('name', var),
-            ))
+        ))
     test.update(scenario.to_json())
     test['period'] = scenario.period.start.year
     test_case = test.pop('test_case', None)
@@ -65,16 +58,7 @@ def json_to_yaml(json_dir, json_filename, var, output_dir):
             for code, value_by_key in data['resultat_officiel'].iteritems()
             if value_by_key['openfisca_name'] is not None
         ))
-    '''
-    test['output_variables'] = collections.OrderedDict(sorted(
-        (variable_name, variable_value)
-        for variable_name, variable_value in (
-            (base.openfisca_variable_name_by_tax_calculator_code[code], value)
-            for code, value in tax_calculator_outputs.iteritems() if code != "ANNEE"
-            )
-        if variable_name is not None
-        ))
-    '''
+
     tests_file_path = os.path.join(output_dir, '{}.yaml'.format(var))
     if os.path.exists(tests_file_path):
         with open(tests_file_path) as tests_file:
