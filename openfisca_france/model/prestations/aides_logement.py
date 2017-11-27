6f38a40e9ab44f83e3d7bb8d5c198f9989c979bc
Thomas Guillet <guillet.thomas@gmail.com>
2017-11-27 16:58:04 +0100

    Refactor TypesAideLogementNonCalculable Enum

--- openfisca_france/model/prestations/aides_logement.py
+++ openfisca_france/model/prestations/aides_logement.py
@@ -786,7 +786,6 @@ class apl(Variable):
 class aide_logement_non_calculable(Variable):
     value_type = Enum
     possible_values = TypesAideLogementNonCalculable
-    default_value = TypesAideLogementNonCalculable.non_renseigne
     entity = Famille
     label = u"Aide au logement non calculable"
     definition_period = MONTH
@@ -801,7 +800,7 @@ class aide_logement_non_calculable(Variable):
             ],
             [
                 TypesAideLogementNonCalculable.locataire_foyer,
-                TypesAideLogementNonCalculable.non_renseigne
+                TypesAideLogementNonCalculable.calculable
             ]
         )
 
