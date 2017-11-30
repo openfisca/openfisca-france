fe96b14d6f4769fc7568eab624bbcdb2ce753a24
Claire Leroy <cleroy@pse.local>
2017-11-30 12:08:10 +0100

    Update formula for tax reduction 'resimm' for year 2016

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot.py
@@ -2488,7 +2488,7 @@ class resimm(Variable):
     label = u"Réduction d'impôt au titre des dépenses de restauration immobilière - Dispositif Malraux"
     reference = "http://bofip.impots.gouv.fr/bofip/1372-PGP"
     definition_period = YEAR
-    end = '2013-12-31'
+    end = '2016-12-31'
 
 
     def formula_2009_01_01(foyer_fiscal, period, parameters):
@@ -2546,7 +2546,7 @@ class resimm(Variable):
     def formula_2013_01_01(foyer_fiscal, period, parameters):
         '''
         Travaux de restauration immobilière (cases 7RA, 7RB, 7RC, 7RD, 7RE, 7RF, 7SX, 7SY)
-        2012
+        2013-2015
         '''
         f7ra = foyer_fiscal('f7ra', period)
         f7rb = foyer_fiscal('f7rb', period)
@@ -2565,6 +2565,24 @@ class resimm(Variable):
         max5 = max_(max4 - f7ra, 0)
         return (P.taux_rd * min_(f7rd, max1) + P.taux_rb * min_(f7rb, max2) + P.taux_rc * min_(f7sy + f7rf + f7rc, max3) +
                 P.taux_ra * min_(f7ra, max4) + P.taux_re * min_(f7re + f7sx, max5))
+
+    def formula_2016_01_01(self, simulation, period):
+        '''
+        Travaux de restauration immobilière
+        2016
+        '''
+        f7nx = simulation.calculate('f7nx', period)
+        f7ny = simulation.calculate('f7ny', period)
+        f7re = simulation.calculate('f7re', period)
+        f7rf = simulation.calculate('f7rf', period)
+        f7sx = simulation.calculate('f7sx', period)
+        f7sy = simulation.calculate('f7sy', period)
+        P = simulation.parameters_at(period.start).impot_revenu.reductions_impots.resimm
+
+        max1 = P.max
+        max2 = max_(max1 - f7nx - f7sy - f7rf, 0)
+        return (P.taux_rc * min_(f7sy + f7rf + f7nx, max1) +
+                 P.taux_re * min_(f7re + f7sx + f7ny, max2))
 
 
 class rsceha(Variable):
