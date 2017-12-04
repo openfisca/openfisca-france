aab13a8410e73d7b5398106471911a6be4c53106
Claire Leroy <cleroy@pse.local>
2017-12-04 10:17:26 +0100

    Correct end date of tax reduction 'garext' (tax credit 'ci_garext' after 2004)

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot.py
@@ -1755,7 +1755,7 @@ class garext(Variable):
     entity = FoyerFiscal
     label = u"garext"
     definition_period = YEAR
-    end = '2016-12-31'
+    end = '2004-12-31'
 
 
     def formula_2002_01_01(foyer_fiscal, period, parameters):
@@ -1776,7 +1776,7 @@ class garext(Variable):
         '''
         Frais de garde des enfants à l’extérieur du domicile (cases GA, GB, GC de la 2042)
         et GE, GF, GG
-        2003-
+        2003-2004
         '''
         f7ga = foyer_fiscal('f7ga', period)
         f7gb = foyer_fiscal('f7gb', period)
