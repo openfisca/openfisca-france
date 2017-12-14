1a6d5bd5507e11fcc47951d00c4f649ddaf585fc
Laurent Bossavit <laurent.git@bossavit.com>
2017-12-14 21:56:18 +0100

    Clarify test

--- tests/test_coefficient.py
+++ tests/test_coefficient.py
@@ -17,7 +17,7 @@ def test_coefficient_proratisation_only_contract_periods():
     contrat_de_travail_debut='2017-11-1',
     allegement_fillon_mode_recouvrement=u'progressif'))
   simulation = scenario.new_simulation()
-  assert_equal(simulation.calculate_add('coefficient_proratisation','2017-11'),1)
-  assert_equal(simulation.calculate_add('coefficient_proratisation','2017-12'),1)
-  assert_equal(simulation.calculate_add('coefficient_proratisation','2017-10'),0)
+  assert_equal(simulation.calculate('coefficient_proratisation','2017-11'),1)
+  assert_equal(simulation.calculate('coefficient_proratisation','2017-12'),1)
+  assert_equal(simulation.calculate('coefficient_proratisation','2017-10'),0)
   assert_equal(simulation.calculate_add('coefficient_proratisation','2017'),2)
