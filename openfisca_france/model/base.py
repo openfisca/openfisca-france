9b65b740e16dc8ac637d8ac3937ef8fb90512503
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-01-11 14:41:05 -0500

    Add __order__ to enums

--- openfisca_france/model/base.py
+++ openfisca_france/model/base.py
@@ -3,13 +3,17 @@
 from openfisca_core.model_api import *
 from openfisca_france.entities import Famille, FoyerFiscal, Individu, Menage
 
+# Enums used through the legislation
+# The __order__ is only necessary because of Python 2
 
 class TypesAAHNonCalculable(Enum):
+    __order__ = 'calculable intervention_CDAPH_necessaire'
     calculable = u"Calculable"
     intervention_CDAPH_necessaire = u"intervention_CDAPH_necessaire"
 
 
 class TypesActivite(Enum):
+    __order__ = 'actif chomeur etudiant retraite inactif'
     actif = u'Actif occupé'
     chomeur = u'Chômeur'
     etudiant = u'Étudiant, élève'
@@ -18,23 +22,26 @@ class TypesActivite(Enum):
 
 
 class TypesAideLogementNonCalculable(Enum):
+    __order__ = 'calculable locataire_foyer'
     calculable = u"Calculable"
     locataire_foyer = u"Non calculable (Locataire foyer)"
 
-
 class TypesAllegementCotisationAllocationsFamilialesModeRecouvrement(Enum):
+    __order__ = 'fin_d_annee anticipe progressif'
     fin_d_annee = u"fin_d_annee"
     anticipe = u"anticipe_regularisation_fin_de_periode"
     progressif = u"progressif"
 
-
+# TODO: Merge with the previous one ?
 class TypesAllegementFillonModeRecouvrement(Enum):
+    __order__ = 'fin_d_annee anticipe progressif'
     fin_d_annee = u"fin_d_annee"
     anticipe = u"anticipe_regularisation_fin_de_periode"
     progressif = u"progressif"
 
 
 class TypesCategorieSalarie(Enum):
+    __order__ = 'prive_non_cadre prive_cadre public_titulaire_etat public_titulaire_militaire public_titulaire_territoriale public_titulaire_hospitaliere public_non_titulaire non_pertinent'
     prive_non_cadre = u'prive_non_cadre'
     prive_cadre = u'prive_cadre'
     public_titulaire_etat = u'public_titulaire_etat'
@@ -46,6 +53,7 @@ class TypesCategorieSalarie(Enum):
 
 
 class TypesContratDeTravail(Enum):
+    __order__ = 'temps_plein temps_partiel forfait_heures_semaines forfait_heures_mois forfait_heures_annee forfait_jours_annee sans_objet'
     temps_plein = u"temps_plein"
     temps_partiel = u"temps_partiel"
     forfait_heures_semaines = u"forfait_heures_semaines"
@@ -56,11 +64,13 @@ class TypesContratDeTravail(Enum):
 
 
 class TypesContratDeTravailDuree(Enum):
+    __order__ = 'cdi cdd'
     cdi = u"CDI"
     cdd = u"CDD"
 
 
 class TypesTauxCSGRemplacement(Enum):
+    __order__ = 'non_renseigne exonere taux_reduit taux_plein'
     non_renseigne = u"Non renseigné/non pertinent"
     exonere = u"Exonéré"
     taux_reduit = u"Taux réduit"
@@ -68,18 +78,21 @@ class TypesTauxCSGRemplacement(Enum):
 
 
 class TypesCotisationSocialeModeRecouvrement(Enum):
+    __order__ = 'mensuel annuel mensuel_strict'
     mensuel = u"Mensuel avec régularisation en fin d'année"
     annuel = u"Annuel"
     mensuel_strict = u"Mensuel strict"
 
 
 class TypesEligibiliteANAH(Enum):
+    __order__ = 'a_verifier modestes tres_modeste'
     a_verifier = u"A vérifier"
     modestes = u"Modestes"
     tres_modeste = u"Très modestes"
 
 
 class TypesExpositionAccident(Enum):
+    __order__ = 'faible moyen eleve tres_eleve'
     faible = u"Faible"
     moyen = u"Moyen"
     eleve = u"Élevé"
@@ -87,12 +100,14 @@ class TypesExpositionAccident(Enum):
 
 
 class TypesExpositionPenibilite(Enum):
+    __order__ = 'nulle simple multiple'
     nulle = u"Nulle, pas d'exposition de l'employé à un facteur de pénibilité"
     simple = u"Simple, exposition à un seul facteur de pénibilité"
     multiple = u"Multiple, exposition à plusieurs facteurs de pénibilité"
 
 
 class TypesGir(Enum):
+    __order__ = 'gir_1 gir_2 gir_3 gir_4 gir_5 gir_6'
     non_defini = u"Non défini"
     gir_1 = u"Gir 1"
     gir_2 = u"Gir 2"
@@ -103,18 +118,21 @@ class TypesGir(Enum):
 
 
 class TypesRSANonCalculable(Enum):
+    __order__ = 'calculable tns conjoint_tns'
     calculable = u"Calculable"
     tns = u"tns"
     conjoint_tns = u"conjoint_tns"
 
 
 class TypesScolarite(Enum):
+    __order__ = 'inconnue college lycee'
     inconnue = u"Inconnue"
     college = u"Collège"
     lycee = u"Lycée"
 
 
 class TypesStatutMarital(Enum):
+    __order__ = 'non_renseigne marie celibataire divorce veuf pacse jeune_veuf'
     non_renseigne = u'Non renseigné'
     marie = u'Marié'
     celibataire = u'Celibataire'
@@ -125,6 +143,7 @@ class TypesStatutMarital(Enum):
 
 
 class TypesStatutOccupationLogement(Enum):
+    __order__ = 'non_renseigne primo_accedant proprietaire locataire_hlm locataire_vide locataire_meuble loge_gratuitement locataire_foyer sans_domicile'
     non_renseigne = u"Non renseigné"
     primo_accedant = u"Accédant à la propriété"
     proprietaire = u"Propriétaire (non accédant) du logement"
@@ -137,6 +156,7 @@ class TypesStatutOccupationLogement(Enum):
 
 
 class TypesTailleEntreprise(Enum):
+    __order__ = 'non_pertinent moins_de_10 de_10_a_19 de_20_a_249 plus_de_250'
     non_pertinent = u"Non pertinent"
     moins_de_10 = u"Moins de 10 salariés"
     de_10_a_19 = u"De 10 à 19 salariés"
@@ -145,12 +165,14 @@ class TypesTailleEntreprise(Enum):
 
 
 class TypesTnsTypeActivite(Enum):
+    __order__ = 'achat_revente bic bnc'
     achat_revente = u'achat_revente'
     bic = u'bic'
     bnc = u'bnc'
 
 
-class TypesZoneApl(Enum) :
+class TypesZoneApl(Enum):
+    __order__ = 'non_renseigne zone_1 zone_2 zone_3'
     non_renseigne = u"Non renseigné"
     zone_1 = u"Zone 1"
     zone_2 = u"Zone 2"
@@ -160,6 +182,7 @@ TAUX_DE_PRIME = 1 / 4  # primes_fonction_publique (hors suppl. familial et indem
 
 # Legacy roles. To be removed when they are not used by formulas anymore.
 class QUIFAM(Enum):
+    __order__ = 'chef part enf1 enf2 enf3 enf4 enf5 enf6 enf7 enf8 enf9'
     chef = 0
     part = 1
     enf1 = 2
@@ -173,6 +196,7 @@ class QUIFAM(Enum):
     enf9 = 10
 
 class QUIFOY(Enum):
+    __order__ = 'vous conj pac1 pac2 pac3 pac4 pac5 pac6 pac7 pac8 pac9'
     vous = 0
     conj = 1
     pac1 = 2
@@ -186,6 +210,7 @@ class QUIFOY(Enum):
     pac9 = 10
 
 class QUIMEN(Enum):
+    __order__ = 'pref cref enf1 enf2 enf3 enf4 enf5 enf6 enf7 enf8 enf9'
     pref = 0
     cref = 1
     enf1 = 2
