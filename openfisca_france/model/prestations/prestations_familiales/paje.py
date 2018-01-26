e62e309bed0a6c118b5e3d4c4d0e9fab81ccb53b
Anna-Livia <annalivia.gomart@gmail.com>
2018-01-26 16:08:07 +0100

    [TO CHECK] Fait pointer d'anciennes formules vers PREPARE plutot que CLCA

--- openfisca_france/model/prestations/prestations_familiales/paje.py
+++ openfisca_france/model/prestations/prestations_familiales/paje.py
@@ -343,6 +343,12 @@ class paje_clca_taux_plein(Variable):
     reference = "http://vosdroits.service-public.fr/particuliers/F313.xhtml"
     definition_period = MONTH
 
+    def formula_2017_04_01(famille, period):
+        paje_clca = famille('paje_prepare', period)
+        partiel1 = famille('inactif', period)
+
+        return (paje_clca > 0) * partiel1
+
     def formula_2004_01_01(famille, period):
         paje_clca = famille('paje_clca', period)
         inactif = famille('inactif', period)
@@ -356,6 +362,12 @@ class paje_clca_taux_partiel(Variable):
     label = u"Indicatrice Clca taux partiel"
     reference = "http://vosdroits.service-public.fr/particuliers/F313.xhtml"
     definition_period = MONTH
+
+    def formula_2017_04_01(famille, period):
+        paje_clca = famille('paje_prepare', period)
+        partiel1 = famille('partiel1', period)
+
+        return (paje_clca > 0) * partiel1
 
     def formula_2004_01_01(famille, period):
         paje_clca = famille('paje_clca', period)
