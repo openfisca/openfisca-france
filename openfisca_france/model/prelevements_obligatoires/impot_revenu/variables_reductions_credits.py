2ad134affb8d2f4ae136b2e62e4151679b129ab4
Claire Leroy <cleroy@pse.local>
2018-02-19 19:03:31 +0100

    Rebase + Variable with change of meaning

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/variables_reductions_credits.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/variables_reductions_credits.py
@@ -1336,7 +1336,7 @@ class f7qe(Variable):
     unit = 'currency'
     entity = FoyerFiscal
     label = u"Investissements outre-mer dans le cadre de l'entreprise : Autres investissements ayant fait l'objet avant 1.1.2009 d'une demande d'agrément, d'une déclaration d'ouverture de chantier ou d'un accompte d'au moins 50%"
-    end = '2012-12-31'
+    # end = '2012-12-31' changes meaning in 2014
     definition_period = YEAR
 
 
