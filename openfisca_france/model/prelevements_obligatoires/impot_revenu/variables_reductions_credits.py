2141e4eb39ed513879a7e1ef458207a9e16560a4
Claire Leroy <cleroy@pse.local>
2018-02-21 13:39:45 +0100

    Add end dates

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/variables_reductions_credits.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/variables_reductions_credits.py
@@ -171,6 +171,7 @@ class f7vy(Variable):
     entity = FoyerFiscal
     label = u"Intérêt des emprunts contractés pour l'acquisition ou la construction de l'habitation principale: logements anciens (acquis entre le 06/05/2007 et le 30/09/2011) ou neufs (acquis entre le 06/05/2007 et le 31/12/2009): Première annuité"
 #    start_date = date(2008, 1, 1)
+    end = '2013-12-31'
     definition_period = YEAR
 
 
@@ -200,6 +201,7 @@ class f7vw(Variable):
     entity = FoyerFiscal
     label = u"Intérêt des emprunts contractés pour l'acquisition ou la construction de l'habitation principale: logements neufs non-BBC acquis ou construits du 01/01/2010 au 31/12/2010: première annuité"
 #    start_date = date(2010, 1, 1)
+    end = '2013-12-31'
     definition_period = YEAR
 
 
@@ -210,6 +212,7 @@ class f7vv(Variable):
     entity = FoyerFiscal
     label = u"Intérêt des emprunts contractés pour l'acquisition ou la construction de l'habitation principale: logements neufs non-BBC acquis ou construits du 01/01/2010 au 31/12/2010: annuités suivantes"
 #    start_date = date(2011, 1, 1)
+    end = '2015-12-31'
     definition_period = YEAR
 
   # TODO: variable non présente dans OF, à intégrer partout où c'est nécessaire
@@ -221,6 +224,7 @@ class f7vu(Variable):
     entity = FoyerFiscal
     label = u"Intérêt des emprunts contractés pour l'acquisition ou la construction de l'habitation principale: logements neufs non-BBC acquis ou construits du 01/01/2011 au 30/09/2011: première annuité"
 #    start_date = date(2011, 1, 1)
+    end = '2014-12-31'
     definition_period = YEAR
 
   # TODO: variable non présente dans OF, à intégrer partout où c'est nécessaire
