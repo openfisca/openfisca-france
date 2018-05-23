ac1a1dcea2ba7499f3303d55b0e78dc8cb44252c
TOMAS Frederic <frederic.tomas@soprasteria.com>
2018-05-23 14:53:37 +0200

    Met à jour CHANGELOG et setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '21.10.3',
+    version = '21.10.4',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
