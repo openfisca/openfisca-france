c977059af926334021c3d7b3ac4796f818e540b4
Anna-Livia <annalivia.gomart@gmail.com>
2018-06-08 16:32:20 +0200

    Met à jour le CHANGELOG

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '21.10.7',
+    version = '21.10.8',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
