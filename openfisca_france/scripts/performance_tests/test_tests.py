f75d12c3ff5e61761ee6d88e0548457f6fe98763
sandcha <sandcha@users.noreply.github.com>
2018-06-12 17:45:25 +0200

    Compare le temps d'exécution de tests yaml à une baseline

--- openfisca_france/scripts/performance_tests/test_tests.py
+++ openfisca_france/scripts/performance_tests/test_tests.py
@@ -7,9 +7,17 @@ It is placed in openfisca-france because it is the largest set we currently have
 
 import os
 import time
+import logging
 import pkg_resources
+
 from openfisca_core.tools.test_runner import run_tests
 from openfisca_france import CountryTaxBenefitSystem
+
+
+# Baselines for comparision - unit : seconds
+BASELINE_TBS_LOAD_TIME = 9.10831403732
+BASELINE_YAML_TESTS_TIME = 271.448431969
+
 
 # Time tax benefit system loading
 start_time_tbs = time.time()
@@ -26,5 +34,20 @@ start_time_tests = time.time()
 run_tests(tbs, yaml_tests_dir)
 time_spent_tests = time.time() - start_time_tests
 
+
+def compare_performance(baseline, test_result):
+    delta = (test_result - baseline) * 100 / baseline
+    
+    if test_result > baseline * 1.2:
+        logging.warning("The perfomance seems to have worsen by {} %.".format(delta))
+    elif test_result < baseline * 0.8:
+        logging.info("The performance seems to have been improved by {} %.".format(delta))
+    else:
+        logging.info("The performance seems steady ({} %).".format(delta))
+
+
 print("Generate Tax Benefit System: --- {}s seconds ---".format(time_spent_tbs))
+compare_performance(BASELINE_TBS_LOAD_TIME, time_spent_tbs)
+
 print("Pass Mes-aides tests: --- {}s seconds ---".format(time_spent_tests))
+compare_performance(BASELINE_YAML_TESTS_TIME, time_spent_tests)
