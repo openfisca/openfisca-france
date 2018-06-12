38c4f8a198736a427d80741245d9c092c415a4ce
sandcha <sandcha@users.noreply.github.com>
2018-06-12 15:43:10 +0200

    Spécifie l'encodage et commente le test de performances

--- openfisca_france/scripts/performance_tests/test_perf_tests.py
+++ openfisca_france/scripts/performance_tests/test_perf_tests.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 """
-This files tests the performance of the test runner (openfisca-run-test).
+This files tests the performance of the YAML test runner (openfisca-run-test).
 It is placed in openfisca-france because it is the largest set we currently have.
 """
 
@@ -9,7 +11,7 @@ import pkg_resources
 from openfisca_core.tools.test_runner import run_tests
 from openfisca_france import CountryTaxBenefitSystem
 
-
+# Time tax benefit system loading
 start_time_tbs = time.time()
 tbs = CountryTaxBenefitSystem()
 time_spent_tbs = time.time() - start_time_tbs
@@ -18,6 +20,8 @@ time_spent_tbs = time.time() - start_time_tbs
 openfisca_france_dir = pkg_resources.get_distribution('OpenFisca-France').location
 yaml_tests_dir = os.path.join(openfisca_france_dir, 'tests', 'mes-aides.gouv.fr')
 
+
+# Time openfisca-run-test runner
 start_time_tests = time.time()
 run_tests(tbs, yaml_tests_dir)
 time_spent_tests = time.time() - start_time_tests
