e53a7abd6c9c787d874cb7b32781361bcba915e5
Claire Leroy <cleroy@pse.local>
2018-06-29 16:08:39 +0200

    Ajoute le scénario de test du crédit d'impôt "ci_garext"

--- openfisca_france/scripts/calculateur_impots/input_scenario_builder/build_scenarios_to_test.py
+++ openfisca_france/scripts/calculateur_impots/input_scenario_builder/build_scenarios_to_test.py
@@ -87,6 +87,7 @@ def create_all_scenarios_to_test(directory, years):
             #'charges_deduc_non_plafonnees_eparet': TODO
             'charges_deduc_non_plafonnees_reparations': define_single_worker_scenario(year, {'salaire_imposable': fixed_wage_amount, 'f6cb': 25000, 'f6hj': 10000 , 'f6hk': 10000}),
             'charges_deduc_non_plafonnees_pens_alim': define_single_worker_scenario(year, {'salaire_imposable': fixed_wage_amount, 'f6gi': tested_reduction_amount, 'f6gj': tested_reduction_amount, 'f6el': tested_reduction_amount, 'f6em': tested_reduction_amount, 'f6gp': tested_reduction_amount, 'f6gu': tested_reduction_amount}),
+            'credit_garext': define_family_scenario(year, value_by_variable = {'f7ga': 3000, 'f7ge': 2000}, nb_enfants = 2, nbF = 1, nbH = 1),
             'credit_preetu': define_single_worker_scenario(year, {'salaire_imposable': fixed_wage_amount, 'f7uk': 1000, 'f7vo': 2, 'f7td': 3000}, date_naissance = year - 25),
             'credit_saldom': define_single_worker_scenario(year, {'salaire_imposable': fixed_wage_amount, 'f7db': tested_income_amount, 'f7dq': 1, 'f7dg': 1}),
             'decote': define_family_scenario(year, income_amount1 = 25000, income_amount2 = 20000),
