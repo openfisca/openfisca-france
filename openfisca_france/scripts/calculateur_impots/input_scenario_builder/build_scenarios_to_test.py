913bcffd618558a8eddf786b63a56011d271c79a
Claire Leroy <cleroy@pse.local>
2018-06-29 16:07:46 +0200

    Ajoute une fonctionalité dans le scenario builder

--- openfisca_france/scripts/calculateur_impots/input_scenario_builder/build_scenarios_to_test.py
+++ openfisca_france/scripts/calculateur_impots/input_scenario_builder/build_scenarios_to_test.py
@@ -204,7 +204,8 @@ def define_single_worker_scenario(year, value_by_variable,
     return scenario
 
 
-def define_family_scenario(year, date_naissance1 = 1970, date_naissance2 = 1970, income_amount1 = 50000, income_amount2 = 50000, nb_enfants = 3,
+def define_family_scenario(year, value_by_variable = {}, 
+    date_naissance1 = 1970, date_naissance2 = 1970, income_amount1 = 50000, income_amount2 = 50000, nb_enfants = 3,
     nbF = 3, nbG = 0, nbR = 0, nbH = 0, nbI = 0, nbJ = 0, nbN = 0, caseP = 0, caseF = 0, caseW = 0, caseS = 0, caseG = 0):
     """
         Function that creates a scenario from the base tax & benefits system for one entity (a family with 3 children)
@@ -269,6 +270,23 @@ def define_family_scenario(year, date_naissance1 = 1970, date_naissance2 = 1970,
         nbN = nbN,
         nbR = nbR,
     )
+
+    for variable, value in value_by_variable.iteritems():
+        column = base.tax_benefit_system.variables[variable]
+        entity = column.entity.key
+
+        start = 1990
+        end = 2050 if column.end is None else column.end.year
+        value = value if conv.test_between(start, end)(year)[1] is None else 0
+
+        if entity == 'individu':
+            parent1[variable] = value
+        elif entity == 'foyer_fiscal':
+            foyer_fiscal[variable] = value
+        elif entity == 'famille':
+            famille[variable] = value
+        elif entity == 'menage':
+            menage[variable] = value
 
     scenario.init_single_entity(
         period = year,
