5d48157c701326495805504c79d8eb3e123f9cee
Thomas Guillet <guillet.thomas@gmail.com>
2018-07-13 14:03:13 +0200

    Update version and CHANGELOG

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '22.2.1',
+    version = '22.2.2',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
