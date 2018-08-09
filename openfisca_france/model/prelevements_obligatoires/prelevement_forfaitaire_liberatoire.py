03ec17b9bb09f6f96d011fa18c0e5e6ab86db42f
Claire Leroy <cleroy@pse.local>
2018-08-09 10:04:03 +0200

    Rajoute l'ancienne formule du PFL pour avant 2013

--- openfisca_france/model/prelevements_obligatoires/prelevement_forfaitaire_liberatoire.py
+++ openfisca_france/model/prelevements_obligatoires/prelevement_forfaitaire_liberatoire.py
@@ -49,6 +49,7 @@ class produit_etats_non_cooperatif(Variable):
     label = u"Produits de placement à revenus fixe ou de contrats de capitalisation et d'assurance-vie versés à un bénéficiaire résidant dans un état non-coopératif"
     definition_period = YEAR
 
+
 class prelevement_forfaitaire_liberatoire(Variable):
     value_type = float
     entity = FoyerFiscal
@@ -56,18 +57,47 @@ class prelevement_forfaitaire_liberatoire(Variable):
     reference = "art. 125-0 A du Code Général des Impôts"
     definition_period = YEAR
 
+    def formula_2002_01_01(foyer_fiscal, period, parameters):
+        '''
+        Prelèvement libératoire sur les revenus du capital
+        '''
+        f2dh = foyer_fiscal('f2dh', period)
+        f2ee = foyer_fiscal('f2ee', period)
+        param_pfl_av = parameters(period).taxation_capital.pfl_av.bons_ou_contrats_de_capitalisation_et_placements_de_meme_nature_assurance_vie_lors_du_denouement_du_contrat
+        param_pfl = parameters(period).taxation_capital.pfl
+
+        return (
+            f2dh * param_pfl_av.souscrits_apres_le_1_1_90_et_le_pour_une_duree_de.duree_8_ans_et_plus_pour_les_produits_acquis_apres_le_01_01_1998_avec_abattement_sur_l_ir_5
+            + f2ee * param_pfl.autres_produits_de_placement_a_revenu_fixe_comptes_sur_livrets_creances_depots_cautionnement_et_comptes_courants.courus_entre_le_1_1_95_et_apres
+            )
+    
+    def formula_2008_01_01(foyer_fiscal, period, parameters):
+        '''
+        Prelèvement libératoire sur les revenus du capital
+        '''
+        f2da = foyer_fiscal('f2da', period)
+        f2dh = foyer_fiscal('f2dh', period)
+        f2ee = foyer_fiscal('f2ee', period)
+        param_pfl_av = parameters(period).taxation_capital.pfl_av.bons_ou_contrats_de_capitalisation_et_placements_de_meme_nature_assurance_vie_lors_du_denouement_du_contrat
+        param_pfl = parameters(period).taxation_capital.pfl
+
+        return (
+            f2da * param_pfl.dividendes
+            + f2ee * param_pfl.autres_produits_de_placement_a_revenu_fixe_comptes_sur_livrets_creances_depots_cautionnement_et_comptes_courants.courus_entre_le_1_1_95_et_apres
+            + f2dh * param_pfl_av.souscrits_apres_le_1_1_90_et_le_pour_une_duree_de.duree_8_ans_et_plus_pour_les_produits_acquis_apres_le_01_01_1998_avec_abattement_sur_l_ir_5
+            )
+
     def formula_2013_01_01(foyer_fiscal, period, parameters):
         '''
         Prelèvement forfaitaire libératoire (PFL) sur les revenus du capital
         Notes :
-          (1) On ne prend en compte le PFL qu'à partir de 2013. Or, il existait avant.
-              Il était codé sous la variable imp_lib, mais avec des erreurs
-              (application d'un unique taux à la case 2EE). On ne rperend pas tout.
+          (1) La formule avant 2013 provient de l'ancienne variable 'imp_lib'.
+              A priori, cette ancienne formule n'est pas entièrement correcte (application d'un taux unique à la case 2EE par exemple) et mériterait d'être corrigée.
           (2) Cette variable ne comprend pas la taxation forfaitaire à 24% associée
               à la base f2fa, qui est prise en compte via la variable tax_rvcm_forfaitaire,
               qui est incluse dans irpp. Cette taxaiton à 24% est certes forfaitaire,
               mais apparemment pas à la source. Elle serait calculée au moment de l'irpp,
-              d'où mle fait de l'inclure dans la variable IRPP.
+              d'où le fait de l'inclure dans la variable IRPP.
         '''
 
         assurance_vie_pl_non_anonyme_plus8ans_depuis1990 = foyer_fiscal('assurance_vie_pl_non_anonyme_plus8ans_depuis1990', period)
