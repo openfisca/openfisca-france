6c339edee6627dbe21166f9d4a535441dc85efcc
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-08-10 18:09:30 +0200

    Major update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-France',
-    version = '22.9.0',
+    version = '23.0.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
