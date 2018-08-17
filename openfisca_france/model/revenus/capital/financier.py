239578fbe374a3236bb181c9f5a9859aee54bf9a
Brice Fabre <b.fabre@pse.local>
2018-08-17 17:41:07 +0200

    Actualise revenus_capitaux suite au rebase avec version 23.1.1 de master)

--- openfisca_france/model/revenus/capital/financier.py
+++ openfisca_france/model/revenus/capital/financier.py
@@ -491,6 +491,7 @@ class revenus_capital(Variable):
         revenus_capitaux = (
             max_(0, individu.foyer_fiscal('revenus_capitaux_prelevement_bareme', period))
             + max_(0, individu.foyer_fiscal('revenus_capitaux_prelevement_liberatoire', period))
+            + max_(0, individu.foyer_fiscal('revenus_capitaux_prelevement_forfaitaire_unique_ir', period))
             )
 
         return revenus_capitaux
