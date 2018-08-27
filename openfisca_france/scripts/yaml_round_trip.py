00626ea338c50dcad03afcdb25d65720fe9e7bed
Thomas Guillet <guillet.thomas@gmail.com>
2018-08-27 09:53:10 +0200

    fixup! Add logic to run round trip validation on YAML test files

--- openfisca_france/scripts/yaml_round_trip.py
+++ openfisca_france/scripts/yaml_round_trip.py
@@ -17,13 +17,9 @@ yaml.width = 1000
 
 def roundTrip(path):
     with io.open(path, 'r', encoding='utf8') as f:
-        try:
-            data = yaml.load(f)
-            with io.open(path, 'w', encoding='utf8') as f:
-                yaml.dump(data, f)
-        except Exception as e:
-            raise e
-
+        data = yaml.load(f)
+        with io.open(path, 'w', encoding='utf8') as f:
+            yaml.dump(data, f)
 
 if __name__ == "__main__":
     for p in sys.argv[1:]:
