0800037dfe917f4a09d07ab1a774c0b6b71d4832
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-08-27 22:44:02 +0200

    Comment out taux_csg_remplacement for too early year

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/contributions_sociales/remplacement.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/contributions_sociales/remplacement.py
@@ -44,18 +44,18 @@ class taux_csg_remplacement(Variable):
             )
         return taux_csg_remplacement
 
-    def formula_1991_02_01(individu, period, parameters):
-        # TODO: Temporary hack to avoid too much recursion
-        if period.start >= 2010:
-            irpp = individu.foyer_fiscal('irpp', period = period.n_2)
-            taux_csg_remplacement = where(
-                irpp <= parameters(period.start).prelevements_sociaux.contributions.csg.remplacement.pensions_de_retraite_et_d_invalidite.seuil_d_ir,
-                TypesTauxCSGRemplacement.exonere,
-                TypesTauxCSGRemplacement.taux_plein,
-                )
-        else:
-            taux_csg_remplacement = TypesTauxCSGRemplacement.taux_plein
-        return taux_csg_remplacement
+    # def formula_1991_02_01(individu, period, parameters):
+    #     # TODO: Temporary hack to avoid too much recursion
+    #     if period.start >= 2010:
+    #         irpp = individu.foyer_fiscal('irpp', period = period.n_2)
+    #         taux_csg_remplacement = where(
+    #             irpp <= parameters(period.start).prelevements_sociaux.contributions.csg.remplacement.pensions_de_retraite_et_d_invalidite.seuil_d_ir,
+    #             TypesTauxCSGRemplacement.exonere,
+    #             TypesTauxCSGRemplacement.taux_plein,
+    #             )
+    #     else:
+    #         taux_csg_remplacement = TypesTauxCSGRemplacement.taux_plein
+    #     return taux_csg_remplacement
 
 
 # Allocations chômage
