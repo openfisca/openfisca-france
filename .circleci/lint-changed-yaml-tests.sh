13fc6035170b8302fe39f44ddbbfb3ea1f407cef
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2018-09-25 14:23:46 +0200

    Ne lint pas les fichiers supprimés

--- .circleci/lint-changed-yaml-tests.sh
+++ .circleci/lint-changed-yaml-tests.sh
@@ -2,7 +2,7 @@
 
 last_tagged_commit=`git describe --tags --abbrev=0 --first-parent`  # --first-parent ensures we don't follow tags not published in master through an unlikely intermediary merge commit
 
-if ! changes=$(git diff-index --name-only --exit-code $last_tagged_commit -- "tests/*.yaml")
+if ! changes=$(git diff-index --name-only --diff-filter=ACMR --exit-code $last_tagged_commit -- "tests/*.yaml")
 then
   echo "Hello boss, I'm linting the following changed files:"
   echo $changes
