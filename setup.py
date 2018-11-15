9c5f3f7e74283c3fc28b834421752a4727f400aa
Laurent Bossavit <laurent.git@bossavit.com>
2018-11-15 21:02:20 +0100

    Evite l'erreur PyTest liée à la version 4.0

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
             "flake8 >= 3.5.0, < 3.6.0",
             "flake8-print",
             "pycodestyle >= 2.3.0, < 2.4.0",  # To avoid incompatibility with flake
-            "pytest",
+            "pytest < 4.0",
             "scipy >= 0.17",  # Only used to test de_net_a_brut reform
             "requests >= 2.8",
             "yamllint >= 1.11.1, < 1.12",
