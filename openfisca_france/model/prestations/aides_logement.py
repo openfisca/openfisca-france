80c721fb8cd54801e4d517cb26f207327a3a11f3
Thomas Guillet <guillet.thomas@gmail.com>
2018-12-11 09:03:27 +0100

    Update aides_logement.py

--- openfisca_france/model/prestations/aides_logement.py
+++ openfisca_france/model/prestations/aides_logement.py
@@ -1111,7 +1111,7 @@ class aides_logement_accedant_et_foyer(Variable):
 
         loyer = famille.demandeur.menage('loyer', period)
         plafond_mensualite = famille('aides_logement_plafond_mensualite', period)
-        L = where(logement_conventionne, min_(plafond_mensualite, loyer), plafond_mensualite)
+        L = min_(plafond_mensualite, loyer)
         C = forfait_charges * famille('aide_logement_charges', period)
         K = famille('aides_logement_k', period)
         Lo = famille('aides_logement_loyer_minimal', period)
