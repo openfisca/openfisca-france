d1cafedfa3fa9741a37ce01c1e6c75a6058e0b79
Laurent Bossavit <laurent.git@bossavit.com>
2018-12-13 11:04:12 +0100

    Modifie CHANGELOG.md et setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "32.2.1",
+    version = "32.3.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
