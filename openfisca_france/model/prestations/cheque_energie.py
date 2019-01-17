1685120466134daf2d5db0281387f56b8288066d
Laurent Bossavit <laurent.git@bossavit.com>
2019-01-17 13:55:43 +0100

    Factorise les formules datées et transforme le paramètre en barème

--- openfisca_france/model/prestations/cheque_energie.py
+++ openfisca_france/model/prestations/cheque_energie.py
@@ -5,13 +5,6 @@ from __future__ import division
 from openfisca_france.model.base import *
 
 
-class UCChequeEnergie(Enum):
-    __order__ = 'une_uc une_uc_a_deux_uc plus_de_deux_uc'  # Needed to preserve the enum order in Python 2
-    une_uc = u'Une unité de consommation (UC)'
-    une_uc_a_deux_uc = u'Une UC à deux UC'
-    plus_de_deux_uc = u'Plus de deux UC'
-
-
 class cheque_energie_unites_consommation(Variable):
     entity = Menage
     value_type = float
@@ -19,7 +12,7 @@ class cheque_energie_unites_consommation(Variable):
     label = u"Unités de consommation du ménage pour le calcul du chèque Énergie"
     definition_period = YEAR
 
-    def formula_2018(menage, period, parameters):
+    def formula_2017(menage, period, parameters):
         uc = parameters(period).cheque_energie.unites_consommation
         nb_personnes = menage.nb_persons()
         gardes_alternees = menage.sum(menage.members('garde_alternee', period.first_month))
@@ -44,7 +37,7 @@ class cheque_energie_eligibilite_logement(Variable):
     label = u"Éligibilité du logement occupé au chèque énergie"
     definition_period = MONTH
 
-    def formula_2018(menage, period, parameters):
+    def formula_2017(menage, period, parameters):
         statut_occupation_logement = menage('statut_occupation_logement', period)
         residence_saint_martin = menage('residence_saint_martin', period)
 
@@ -69,48 +62,20 @@ class cheque_energie_montant(Variable):
     label = u"Montant du chèque énergie"
     definition_period = YEAR
 
-    def formula_2018(menage, period, parameters):
+    def formula_2017(menage, period, parameters):
         baremes = parameters(period).cheque_energie.baremes
-        seuils = baremes.thresholds
-        montants = baremes.montants
 
         uc_menage = menage('cheque_energie_unites_consommation', period)
         rfr = menage.personne_de_reference.foyer_fiscal('rfr', period.n_2)
 
         ressources_par_uc = rfr / uc_menage
 
-        uc = select(
-            [uc_menage <= 1, uc_menage < 2, 2 <= uc_menage],
-            [UCChequeEnergie.une_uc, UCChequeEnergie.une_uc_a_deux_uc, UCChequeEnergie.plus_de_deux_uc]
-            )
-
         return (
-            + (ressources_par_uc < seuils.tranche_une) * montants[uc].tranche_une
-            + (seuils.tranche_une <= ressources_par_uc) * (ressources_par_uc < seuils.tranche_deux) * montants[uc].tranche_deux
-            + (seuils.tranche_deux <= ressources_par_uc) * (ressources_par_uc < seuils.tranche_trois) * montants[uc].tranche_trois
+            (uc_menage <= 1) * baremes.une_uc.calc(ressources_par_uc)
+            + ((uc_menage > 1) * (uc_menage < 2)) * baremes.une_uc_a_deux_uc.calc(ressources_par_uc)
+            + (uc_menage >= 2) * baremes.plus_de_deux_ucs.calc(ressources_par_uc)
             )
 
-    def formula_2019(menage, period, parameters):
-        baremes = parameters(period).cheque_energie.baremes
-        seuils = baremes.thresholds
-        montants = baremes.montants
-
-        uc_menage = menage('cheque_energie_unites_consommation', period)
-        rfr = menage.personne_de_reference.foyer_fiscal('rfr', period.n_2)
-
-        ressources_par_uc = rfr / uc_menage
-
-        uc = select(
-            [uc_menage <= 1, uc_menage < 2, 2 <= uc_menage],
-            [UCChequeEnergie.une_uc, UCChequeEnergie.une_uc_a_deux_uc, UCChequeEnergie.plus_de_deux_uc]
-            )
-
-        return (
-            + (ressources_par_uc < seuils.tranche_une) * montants[uc].tranche_une
-            + (seuils.tranche_une <= ressources_par_uc) * (ressources_par_uc < seuils.tranche_deux) * montants[uc].tranche_deux
-            + (seuils.tranche_deux <= ressources_par_uc) * (ressources_par_uc < seuils.tranche_trois) * montants[uc].tranche_trois
-            + (seuils.tranche_trois <= ressources_par_uc) * (ressources_par_uc < seuils.tranche_quatre) * montants[uc].tranche_quatre
-            )
 
 class cheque_energie(Variable):
     entity = Menage
@@ -119,5 +84,7 @@ class cheque_energie(Variable):
     label = u"Montant auquel le ménage peut prétendre au titre du chèque energie"
     definition_period = MONTH
 
-    def formula_2018(menage, period, parameters):
-        return menage('cheque_energie_montant', period.this_year) * menage('cheque_energie_eligibilite_logement', period)
+    def formula_2017(menage, period, parameters):
+        eligible = menage('cheque_energie_eligibilite_logement', period)
+        montant = menage('cheque_energie_montant', period.this_year)
+        return eligible * montant
