73f1a43185739c2e97511d7d889b27de3e45c867
Laurent Bossavit <laurent.git@bossavit.com>
2019-01-20 16:38:52 +0100

    Supprime requested_period_last_or_next_value pour rsa_isolement_recent

--- openfisca_france/model/prestations/minima_sociaux/rsa.py
+++ openfisca_france/model/prestations/minima_sociaux/rsa.py
@@ -834,7 +834,6 @@ class rsa_isolement_recent(Variable):
     value_type = bool
     entity = Famille
     label = u"Situation d'isolement depuis moins de 18 mois"
-    base_function = requested_period_last_or_next_value
     definition_period = MONTH
 
 
