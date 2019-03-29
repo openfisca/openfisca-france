7661662f5bce16b9de4b0f18b0b8595fd5ec4cb8
Laurent Bossavit <laurent.git@bossavit.com>
2019-03-29 10:40:12 +0100

    Met à jour Core à v.30

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         },
     include_package_data = True,  # Will read MANIFEST.in
     install_requires = [
-        "OpenFisca-Core >=29.0, <30.0",
+        "OpenFisca-Core >=30.0, <31.0",
         ],
     message_extractors = {"openfisca_france": [
         ("**.py", "python", None),
