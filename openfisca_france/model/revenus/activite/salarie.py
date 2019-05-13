03faf4e0944f5b59d779eee2b37b1659952af07f
TOMAS Frederic <frederic.tomas@soprasteria.com>
2019-05-13 09:28:11 +0200

    Ajoute les types de congés manquants

--- openfisca_france/model/revenus/activite/salarie.py
+++ openfisca_france/model/revenus/activite/salarie.py
@@ -1117,6 +1117,7 @@ class TypesConges(Enum):
     conge_parental = "Congé parental"
     conge_maternite_paternite = "Congé maternité ou paternité"
     conge_presence_parentale = "Congé de présence parentale"
+    conge_presence_parental = "Congé de présence parentale"
     conge_conventionnel = "Congé conventionnel"
     conge_sans_solde = "Congé sans solde"
     disponibilite = "Mise en disponibilité (fonction publique)"
