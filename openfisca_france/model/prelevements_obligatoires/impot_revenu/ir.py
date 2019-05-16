0c7f478997c75e0c4d77e5fcfdfd82dcf4c82538
TOMAS Frederic <frederic.tomas@soprasteria.com>
2019-05-16 14:58:25 +0200

    Retire la compatibilite Python 2 superflue, corrections mineurs (orthographe de noms de variable, description de tests)

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
@@ -3514,5 +3514,5 @@ class plus_values_gains_divers(Variable):
 class deficit_exercice(Variable):
     value_type = float
     entity = FoyerFiscal
-    label = u"Déficit sur l'exercice"
+    label = "Déficit sur l'exercice"
     definition_period = YEAR
