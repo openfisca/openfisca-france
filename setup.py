d0ad3fc89da311b09acac8cf0213a5ce53276806
Laurent Bossavit <laurent.git@bossavit.com>
2019-05-20 10:31:09 +0200

    Met à jour la version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "42.2.0",
+    version = "42.3.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
