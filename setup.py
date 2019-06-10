5555a23c1d70ad55b6e3f7c58575b82204a63464
Claire Leroy <cleroy@pse.local>
2019-06-10 11:02:13 +0200

    Mets à jour le CHANGELOG et le setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "42.5.1",
+    version = "43.0.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
