e374fb910cbc610d672d009dbe300f68cb0cc643
Claire Leroy <cleroy@pse.local>
2019-06-13 18:11:02 +0200

    Renomme `assurance_vie_pfu_ir_moins8ans_19970926_primes_apres_20170927` en `f2zz`

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/prelevements_forfaitaires/ir_prelevement_forfaitaire_unique.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/prelevements_forfaitaires/ir_prelevement_forfaitaire_unique.py
@@ -58,7 +58,8 @@ class assurance_vie_pfu_ir_moins4ans_19970926_primes_avant_20170927(Variable):
     definition_period = YEAR
 
 
-class assurance_vie_pfu_ir_moins8ans_19970926_primes_apres_20170927(Variable):
+class f2zz(Variable):
+    cerfa_field = u"2ZZ"
     value_type = float
     entity = FoyerFiscal
     label = u"Produits des bons ou contrats de capitalisation et d'assurance vie de moins de 8 ans pour les contrats souscrits après le 26 septembre 1997, dont le produits sont associés aux primes versées après le 27 septembre 2017, et que le bénéficiaire décide de soumettre au prélèvement forfaitaire unique au titre de l'impôt sur le revenu"
@@ -79,11 +80,11 @@ class assurance_vie_pfu_ir(Variable):
     definition_period = YEAR
 
     def formula_2018_01_01(foyer_fiscal, period):
-        assurance_vie_pfu_ir_moins8ans_19970926_primes_apres_20170927 = foyer_fiscal('assurance_vie_pfu_ir_moins8ans_19970926_primes_apres_20170927', period)
+        f2zz = foyer_fiscal('f2zz', period)
         assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927 = foyer_fiscal('assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927', period)
 
         return (
-            assurance_vie_pfu_ir_moins8ans_19970926_primes_apres_20170927
+            f2zz
             + assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927
             )
 
@@ -183,14 +184,14 @@ class prelevement_forfaitaire_unique_ir_sur_assurance_vie(Variable):
 
         maries_ou_pacses = foyer_fiscal('maries_ou_pacses', period)
 
-        assurance_vie_pfu_ir_moins8ans_19970926_primes_apres_20170927 = foyer_fiscal('assurance_vie_pfu_ir_moins8ans_19970926_primes_apres_20170927', period)
+        f2zz = foyer_fiscal('f2zz', period)
         assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927 = foyer_fiscal('assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927', period)
 
         # Nouveau régime de taxation (produits au titre des primes versées à compter du 26 septembre 1997, pour les contrats souscrits à partir du 26 septembre 1997)
         assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927_apres_abt = max_(assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927 - rvcm.abat_assvie * (1 + maries_ou_pacses), 0)
         reliquat_abt = max_(rvcm.abat_assvie * (1 + maries_ou_pacses) - assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927, 0)
         pfu_ir_av_nouveau_regime = -(
-            (assurance_vie_pfu_ir_moins8ans_19970926_primes_apres_20170927 * P1.taux)
+            (f2zz * P1.taux)
             + (min_(assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927_apres_abt, P1.seuil_taux_reduit_av) * P1.taux_reduit_av)
             + (max_(assurance_vie_pfu_ir_plus8ans_19970926_primes_apres_20170927_apres_abt - P1.seuil_taux_reduit_av, 0) * P1.taux)
             )
