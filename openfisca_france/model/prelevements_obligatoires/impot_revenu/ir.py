714f0e8d4e807b95a759941475ed84aabf6da053
Laurent Bossavit <laurent.git@bossavit.com>
2019-06-14 17:21:49 +0200

    Modifie les modalités de calcul de l'âge

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
@@ -62,36 +62,15 @@ class age(Variable):
     label = "Âge (en années) au premier jour du mois"
     definition_period = MONTH
     is_period_size_independent = True
-    set_input = set_input_dispatch_by_period
+
+    # Convertit un age en date de naissance, de sorte que l'âge équivalent se propage à toutes les périodes
+    def set_input(holder, period, array):
+        array = holder._to_array(array)
+        naissance = (datetime64(period.first_month.start).astype('datetime64[M]') - (array * 12).astype('timedelta64[M]')).astype('datetime64[D]')
+        holder.simulation.set_input('date_naissance', period, naissance)
 
     def formula(individu, period, parameters):
-        has_birth = individu.get_holder('date_naissance').get_known_periods()
-        if not has_birth:
-            has_age_en_mois = bool(individu.get_holder('age_en_mois').get_known_periods())
-            if has_age_en_mois:
-                return individu('age_en_mois', period) // 12
-
-            # If age is known at the same day of another year, compute the new age from it.
-            holder = individu.get_holder('age')
-            start = period.start
-            known_periods = holder.get_known_periods()
-            if known_periods:
-                for last_period in sorted(known_periods, reverse = True):
-                    last_start = last_period.start
-                    if last_start.day == start.day:
-                        last_array = holder.get_array(last_period)
-                        return (
-                            last_array
-                            + int(
-                                start.year
-                                - last_start.year
-                                + (start.month - last_start.month) / 12
-                                )
-                            )
-
-        date_naissance = individu('date_naissance', period)
-        epsilon = timedelta64(1)
-        return (datetime64(period.start) - date_naissance + epsilon).astype('timedelta64[Y]')
+        return individu('age_en_mois', period) / 12
 
 
 class age_en_mois(Variable):
@@ -103,23 +82,13 @@ class age_en_mois(Variable):
     is_period_size_independent = True
     definition_period = MONTH
 
+    # Convertit un âge en mois en date de naissance, de sorte que l'âge équivalent se propage à toutes les périodes
+    def set_input(holder, period, array):
+        array = holder._to_array(array)
+        naissance = (datetime64(period.first_month.start).astype('datetime64[M]') - array.astype('timedelta64[M]')).astype('datetime64[D]')
+        holder.simulation.set_input('date_naissance', period, naissance)
+
     def formula(individu, period, parameters):
-        # If age_en_mois is known at the same day of another month, compute the new age_en_mois from it.
-        holder = individu.get_holder('age_en_mois')
-        start = period.start
-        known_periods = holder.get_known_periods()
-
-        for last_period in sorted(known_periods, reverse = True):
-            last_start = last_period.start
-            if last_start.day == start.day:
-                last_array = holder.get_array(last_period)
-                return last_array + ((start.year - last_start.year) * 12 + (start.month - last_start.month))
-
-        has_birth = individu.get_holder('date_naissance').get_known_periods()
-        if not has_birth:
-            has_age = bool(individu.get_holder('age').get_known_periods())
-            if has_age:
-                return individu('age', period) * 12
         date_naissance = individu('date_naissance', period)
         epsilon = timedelta64(1)
         return (datetime64(period.start) - date_naissance + epsilon).astype('timedelta64[M]')
