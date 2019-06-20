27bf42e602d524ead35d6319b7f2f62ac5a020cb
Claire Leroy <cleroy@pse.local>
2019-06-20 14:51:56 +0200

    Update le Changelog et le setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "46.0.0",
+    version = "47.0.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
