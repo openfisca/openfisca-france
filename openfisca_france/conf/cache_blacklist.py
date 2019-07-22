675d2e752e3074a61b743406472e5c558fc43af0
Thomas Guillet <guillet.thomas@gmail.com>
2019-07-22 12:25:40 +0200

    Ajoute ppa_plancher_revenu_activite_etudiant dans la blacklist

--- openfisca_france/conf/cache_blacklist.py
+++ openfisca_france/conf/cache_blacklist.py
@@ -20,5 +20,6 @@ cache_blacklist = set([
     'garantie_jeunes_eligibilite_age',
     'montant_cheque_energie',
     'ppa_forfait_logement',
+    'ppa_plancher_revenu_activite_etudiant',
     'unites_consommation_cheque_energie',
     ])
