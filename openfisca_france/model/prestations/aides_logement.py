f36e4569ea59d866bb68c05912985e95a2ea2fda
TOMAS Frederic <frederic.tomas@soprasteria.com>
2019-08-20 11:34:43 +0200

    Corrige l'utilisation de pensions alimentaires versees (à prendre niveau individu)

--- openfisca_france/model/prestations/aides_logement.py
+++ openfisca_france/model/prestations/aides_logement.py
@@ -959,7 +959,8 @@ class aide_logement_base_ressources(Variable):
         ressources_annee_glissante += paje
 
         # Ressources N-1
-        pensions_alimentaires_versees = famille.demandeur.foyer_fiscal('pensions_alimentaires_versees', period.last_year)
+        pensions_alimentaires_versees_i = famille.members('pensions_alimentaires_versees_individu', period.last_year)
+        pensions_alimentaires_versees = famille.sum(pensions_alimentaires_versees_i, role=Famille.PARENT)
         #Montant à soustraire
         ressources_n_1 = -pensions_alimentaires_versees
 
