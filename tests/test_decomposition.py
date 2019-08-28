8f049cdc2ff380ac7d1bed8f5d9a708647ce9b27
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-08-28 16:52:28 +0200

    Failing test_decomposition

--- tests/test_decomposition.py
+++ tests/test_decomposition.py
@@ -1,64 +1,72 @@
+import datetime
+import logging
 import os
 import yaml
-from yaml import SafeLoader
 
 
 from openfisca_france import CountryTaxBenefitSystem as FranceTaxBenefitSystem
 from openfisca_france.france_taxbenefitsystem import COUNTRY_DIR
+from openfisca_france.scenarios import init_single_entity
 
+
+log = logging.getLogger(__name__)
+
+
+def visit(node, decomposition_simulation, simulation, period):
+    variable_name = node["variable_name"]
+    children = node.get("children")
+    log.info("{}: decomposition = {}, direct {}".format(
+        variable_name,
+        decomposition_simulation.calculate(variable_name, period = period),
+        simulation.calculate(variable_name, period = period) if variable_name in simulation.tax_benefit_system.variables else "Not available",
+        ))
+    if children:
+        for child_node in children:
+            visit(child_node, decomposition_simulation, simulation, period)
+    else:
+        return
 
 def test_revenu_disponible_decomposition():
     # Just ensure that disposable_income can be calculated with and without decomposition.
     # Without decomposition
-    tax_benefit_system = FranceTaxBenefitSystem()
 
+    tax_benefit_system = FranceTaxBenefitSystem()
     # With decomposition
     decomposition_path = os.path.join(COUNTRY_DIR, 'model', 'decomposition.yaml')
     with open(decomposition_path, 'r') as yaml_file:
-        decomposition_tree = yaml.load(yaml_file, Loader = SafeLoader)
+        decomposition_tree = yaml.load(yaml_file, Loader = yaml.SafeLoader)
 
     decomposition_tax_benefit_system = FranceTaxBenefitSystem()
     decomposition_tax_benefit_system.add_variables_from_decomposition_tree(decomposition_tree, update = True)
 
-    scenarios_arguments = [
-        dict(
-            period = year,
-            parent1 = dict(
-                date_naissance = datetime.date(1972, 1, 1),
-                salaire_de_base = 2000,
-                effectif_entreprise = 25,
-                categorie_salarie = "prive_non_cadre",
-                ),
-            parent2 = dict(
-                date_naissance = datetime.date(1972, 1, 1),
-                ),
-            menage = dict(
-                zone_apl = "zone_1",
-                ),
-            )
-        for year  in [2018] # in range(2006, 2020)
-        ]
+    year = 2018
+    scenario_arguments = dict(
+        period = year,
+        parent1 = dict(
+            date_naissance = datetime.date(1972, 1, 1),
+            salaire_de_base = 2000,
+            effectif_entreprise = 25,
+            categorie_salarie = "prive_non_cadre",
+            ),
+        parent2 = dict(
+            date_naissance = datetime.date(1972, 1, 1),
+            ),
+        menage = dict(
+            zone_apl = "zone_1",
+            ),
+        )
 
-@pytest.mark.parametrize("scenario_arguments", scenarios_arguments)
-def test_basics(scenario_arguments):
-    scenario = tax_benefit_system.new_scenario()
-    init_single_entity(scenario, **scenario_arguments)
-    simulation = scenario.new_simulation(debug = False)
-    period = scenario_arguments['period']
-    assert simulation.calculate('revenu_disponible', period = period) is not None, "Can't compute revenu_disponible on period {}".format(period)
-    assert simulation.calculate_add('salaire_super_brut', period = period) is not None, \
-        "Can't compute salaire_super_brut on period {}".format(period)
+    for tbs in [tax_benefit_system, decomposition_tax_benefit_system]:
+        scenario = tbs.new_scenario()
+        init_single_entity(scenario, **scenario_arguments)
+        period = scenario_arguments['period']
+        if tbs == decomposition_tax_benefit_system:
+            decomposition_simulation = scenario.new_simulation(debug = False)
+        else:
+            simulation = scenario.new_simulation(debug = False)
 
-
-    simulation
+    visit(decomposition_tree, decomposition_simulation, simulation, period)
 
 
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    test_revenu_disponible_decomposition()
