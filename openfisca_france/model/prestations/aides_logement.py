9f1adb7e6f9803d4d22d069ea32e69f25963ab90
Your Name <you@example.com>
2019-09-10 14:07:08 +0200

    Ajoute la gestion manquante des tns_autres_revenus

--- openfisca_france/model/prestations/aides_logement.py
+++ openfisca_france/model/prestations/aides_logement.py
@@ -970,17 +970,21 @@ class aide_logement_base_ressources(Variable):
         benefice_agricole_i_n_2 = famille.members('tns_benefice_exploitant_agricole', period.n_2)
         benefice_micro_entreprise_i_n_2 = famille.members('tns_micro_entreprise_benefice', period.n_2)
         benefice_auto_entrepreneur_i_n_2 = famille.members('tns_auto_entrepreneur_benefice', period.n_2, options=[ADD])
+        tns_autres_revenus_i_n_2 = famille.members('tns_autres_revenus', period.n_2)
         # En l'absence de benefices TNS en N-2, on recupère les bénéfices de l'année glissante à compter de M-1
         annee_glissante_m_1 = annee_glissante.offset(-1, 'month')
         benefice_agricole_i_m_12 = famille.members('tns_benefice_exploitant_agricole', annee_glissante_m_1)
         benefice_micro_entreprise_i_m_12 = famille.members('tns_micro_entreprise_benefice', annee_glissante_m_1)
         benefice_auto_entrepreneur_i_m_12 = famille.members('tns_auto_entrepreneur_benefice', annee_glissante_m_1,
                                                             options=[ADD])
+        tns_autres_revenus_i_m_12 = famille.members('tns_autres_revenus', annee_glissante_m_1)
         benefice_agricole_i = where(benefice_agricole_i_n_2 > 0, benefice_agricole_i_n_2, benefice_agricole_i_m_12)
         benefice_micro_entreprise_i = where(benefice_micro_entreprise_i_n_2 > 0, benefice_micro_entreprise_i_n_2,
                                             benefice_micro_entreprise_i_m_12)
         benefice_auto_entrepreneur_i = where(benefice_auto_entrepreneur_i_n_2 > 0, benefice_auto_entrepreneur_i_n_2,
                                              benefice_auto_entrepreneur_i_m_12)
+        tns_autres_revenus_i = where(tns_autres_revenus_i_n_2 > 0, tns_autres_revenus_i_n_2,
+                                     tns_autres_revenus_i_m_12)
 
         ressources_n_2_i = (
                 indemnites_journalieres_atexa_i
@@ -988,6 +992,7 @@ class aide_logement_base_ressources(Variable):
                 + benefice_agricole_i
                 + benefice_micro_entreprise_i
                 + benefice_auto_entrepreneur_i
+                + tns_autres_revenus_i
         )
         ressources_n_2 = famille.sum(ressources_n_2_i, role=Famille.PARENT)
         f4ba = famille.demandeur.foyer_fiscal('f4ba', period.n_2)
