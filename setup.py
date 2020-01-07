240af57fb56efc03b225145739ae28fc3274be81
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2020-01-07 10:14:27 +0100

    Incrémente en minor la version à 48.10.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "48.9.9",
+    version = "48.10.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
