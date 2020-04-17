d31129c70741ae6e934d3ea45541abf6d7df83f0
sandcha <sandcha@users.noreply.github.com>
2020-04-17 19:09:34 +0200

    Incrémente en patch la version à 48.10.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "48.10.6",
+    version = "48.10.7",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
