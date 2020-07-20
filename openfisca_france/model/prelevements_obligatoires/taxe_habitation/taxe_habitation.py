da8c76419e78e04f10d9b1cf85a69981f3fb9209
Brice Fabre <b.fabre@pse.local>
2020-07-20 10:40:39 +0200

    Remplace la variable code_INSEE_commune par depcom (cela faisait doublon)

--- openfisca_france/model/prelevements_obligatoires/taxe_habitation/taxe_habitation.py
+++ openfisca_france/model/prelevements_obligatoires/taxe_habitation/taxe_habitation.py
@@ -249,7 +249,7 @@ class taxe_habitation_commune_epci_avant_degrevement(Variable):
         taux_th_commune = menage('taux_th_commune', period)
         taux_th_epci = menage('taux_th_epci', period)
         # Attention, si (taux_th_commune + taux_th_epci == 0), alors
-        # il n'y a pas de taux de taxe d'habitation défini pour le code_INSEE_commune indiqué.
+        # il n'y a pas de taux de taxe d'habitation défini pour le depcom indiqué.
 
         base_nette_th_commune = menage('base_nette_th_commune', period)
         base_nette_th_epci = menage('base_nette_th_epci', period)
