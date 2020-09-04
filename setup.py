31c65957fc49deebf8b5fde17fb9e0358bd239d4
Brice Fabre <b.fabre@pse.local>
2020-09-04 15:50:30 +0200

    Actualise le CHANGELOG et le setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "48.14.0",
+    version = "48.15.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
