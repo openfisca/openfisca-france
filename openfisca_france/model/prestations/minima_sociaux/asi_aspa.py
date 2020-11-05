ff04484f5358d5ba4d5b22588f24971e3e1923dc
sandcha <sandcha@users.noreply.github.com>
2020-11-05 21:01:36 +0100

    Versionne les màj de références en 54.0.1

--- openfisca_france/model/prestations/minima_sociaux/asi_aspa.py
+++ openfisca_france/model/prestations/minima_sociaux/asi_aspa.py
@@ -189,13 +189,13 @@ class asi_aspa_nb_alloc(Variable):
 
 
 class asi(Variable):
-    calculate_output = calculate_output_add
     value_type = float
     label = "Allocation supplémentaire d'invalidité (ASI)"
-    reference = "https://www.legifrance.gouv.fr/codes/id/LEGISCTA000006156277/"
     entity = Individu
     definition_period = MONTH
+    reference = "https://www.legifrance.gouv.fr/codes/id/LEGISCTA000006156277/"
     set_input = set_input_divide_by_period
+    calculate_output = calculate_output_add
 
     def formula_2007(individu, period, parameters):
         maries = individu.famille('maries', period)
