921f5a3a4d785316870c988e64d90575189e729e
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2021-01-11 12:27:57 +0100

    test init_single_entity with parallel axes

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -38,3 +38,32 @@ def test_basics(scenario_arguments):
     assert simulation.calculate('revenu_disponible', period = period) is not None, "Can't compute revenu_disponible on period {}".format(period)
     assert simulation.calculate_add('salaire_super_brut', period = period) is not None, \
         "Can't compute salaire_super_brut on period {}".format(period)
+
+
+def test_init_single_entity_parallel_axes():
+    year = 2019
+    count = 3
+    indexes = [0, 1]
+    salaire_de_base_axes = [
+        dict(
+            count = count,
+            index = index,
+            min = 0,
+            max = 15000,
+            name = 'salaire_de_base',
+            period = year,
+            )
+        for index in indexes
+        ]
+
+    axes = [salaire_de_base_axes]
+
+    scenario_kwargs = dict(
+        parent1 = dict(age = 40),
+        parent2 = dict(age = 40),
+        axes = axes,
+        period = year
+        )
+
+    simulation = init_single_entity(tax_benefit_system.new_scenario(), **scenario_kwargs).new_simulation()
+    assert simulation.calculate_add('salaire_de_base', year) == pytest.approx([0, 0, 7500, 7500, 15000, 15000])
