5473179f9323abc9adfe20d8cfcf27ab6944046a
Brice Fabre <b.fabre@pse.local>
2021-01-22 18:55:09 +0100

    Ajoute le dispositif pour l'aide exceptionnelle de mai

--- openfisca_france/model/covid19.py
+++ openfisca_france/model/covid19.py
@@ -53,8 +53,16 @@ class covid_aide_exceptionnelle_famille_montant(Variable):
         af_nbenf = famille('af_nbenf', period)
         aer = famille.sum(famille.members('aer', period)) > 0
         prim_forf = famille.sum(famille.members('prime_forfaitaire_mensuelle_reprise_activite', period)) > 0
-        base = rsa + ass + aer + prim_forf
-        montant = base * (montants.base + montants.par_enfant * af_nbenf) + not_(base) * al * af_nbenf * montants.par_enfant
+        age_i = famille.members('age', period)
+        etudiant_i = famille.members('etudiant', period)
+        moins_de_25_ans_non_etudiant = famille.any((age_i<=24) * (etudiant_i == 0), role = Famille.PARENT)
+        base_jeune = moins_de_25_ans_non_etudiant * al
+        base = (rsa + ass + aer + prim_forf)
+        montant = (
+            base_jeune * (montants.base_jeune + montants.par_enfant * af_nbenf)
+            + base * not_(base_jeune) * (montants.base + montants.par_enfant * af_nbenf)
+            + not_(base) * not_(base_jeune) * al * af_nbenf * montants.par_enfant
+            )
 
         period_1 = period.offset(-1, 'month')
         rsa_n_1 = famille('rsa', period_1) > 0
@@ -63,10 +71,18 @@ class covid_aide_exceptionnelle_famille_montant(Variable):
         af_nbenf_n_1 = famille('af_nbenf', period_1) > 0
         aer_n_1 = famille.sum(famille.members('aer', period_1)) > 0
         prim_forf_n_1 = famille.sum(famille.members('prime_forfaitaire_mensuelle_reprise_activite', period_1)) > 0
-        base_n_1 = rsa_n_1 + ass_n_1 + aer_n_1 + prim_forf_n_1
-        montant_n_1 = base_n_1 * (montants.base + montants.par_enfant * af_nbenf_n_1) + not_(base_n_1) * al_n_1 * af_nbenf_n_1 * montants.par_enfant
+        age_i_n_1 = famille.members('age', period_1)
+        etudiant_i_n_1 = famille.members('etudiant', period_1)
+        moins_de_25_ans_non_etudiant_n_1 = famille.any((age_i_n_1<=24) * (etudiant_i_n_1 == 0), role = Famille.PARENT)
+        base_jeune_n_1 = moins_de_25_ans_non_etudiant_n_1 * al_n_1
+        base_n_1 = (rsa_n_1 + ass_n_1 + aer_n_1 + prim_forf_n_1)
+        montant_n_1 = (
+            base_jeune_n_1 * (montants.base_jeune + montants.par_enfant * af_nbenf_n_1)
+            + base_n_1 * not_(base_jeune_n_1) * (montants.base + montants.par_enfant * af_nbenf_n_1)
+            + not_(base_n_1) * not_(base_jeune_n_1) * al_n_1 * af_nbenf_n_1 * montants.par_enfant
+            )
         
-        return (montant > 0) * montant + (montant == 0) * (montant_n_1 > 0) * montant_n_1
+        return max_(montant, montant_n_1)
 
 class covid_activite_partielle_eligible(Variable):
     entity = Individu
