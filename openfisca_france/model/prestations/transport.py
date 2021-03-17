d59ce03b67aaca5b3264a44d5785aded2e8b3b12
sandcha <sandcha@users.noreply.github.com>
2021-03-17 11:03:57 +0100

    Retire la condition de non cumul du prêt pour un même bénéficiaire

--- openfisca_france/model/prestations/transport.py
+++ openfisca_france/model/prestations/transport.py
@@ -1,20 +1,4 @@
-from openfisca_france.model.base import Variable, Individu, MONTH, not_
-
-
-class pret_formation_permis(Variable):
-    value_type = bool
-    label = "Bénéficiaire du prêt à la formation au permis de conduire à 1 euro par jour"
-    entity = Individu
-    definition_period = MONTH
-    reference = [
-        "https://www.legifrance.gouv.fr/loda/id/LEGITEXT000006052491/",
-        "https://www.securite-routiere.gouv.fr/passer-son-permis-de-conduire/financement-du-permis-de-conduire/permis-1-eu-par-jour"
-        ]
-    documentation = '''
-    Le prêt « permis à un euro par jour » est exclusivement destiné au financement
-    d’une formation initiale ou, dans le cas d’un échec à l’épreuve pratique
-    de l’examen du permis de conduire, d’une formation complémentaire.
-    '''
+from openfisca_france.model.base import Variable, Individu, MONTH
 
 
 class pret_formation_permis_eligibilite(Variable):
@@ -26,10 +10,17 @@ class pret_formation_permis_eligibilite(Variable):
         "https://www.legifrance.gouv.fr/loda/id/LEGITEXT000006052491/",
         "https://www.securite-routiere.gouv.fr/passer-son-permis-de-conduire/financement-du-permis-de-conduire/permis-1-eu-par-jour"
         ]
+    documentation = '''
+    Le prêt « permis à un euro par jour » est exclusivement destiné au financement
+    d’une formation initiale ou, dans le cas d’un échec à l’épreuve pratique
+    de l’examen du permis de conduire, d’une formation complémentaire.
+    Conditions non modélisées :
+    Il ne peut être attribué qu’une seule fois à un même bénéficiaire et pour une même catégorie de permis.
+    Ces formations doivent viser l’obtention du permis de conduire soit de la catégorie A1,
+    soit de la catégorie A2, soit de la catégorie B.
+    '''
 
     def formula_2005_09_30(individu, period, parameters):
-        pret_formation_permis = individu('pret_formation_permis', period)
         age = individu('age', period)
         criteres_age = parameters(period).prestations.transport.pret_formation_permis.age
-        condition_age = (criteres_age.minimum <= age) * (age <= criteres_age.maximum)
-        return not_(pret_formation_permis) * condition_age
+        return (criteres_age.minimum <= age) * (age <= criteres_age.maximum)
