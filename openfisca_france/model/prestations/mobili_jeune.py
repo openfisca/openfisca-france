e17d8d81a48ab348ed04d4d19d2b50f7cf33d7de
sandcha <sandcha@users.noreply.github.com>
2021-03-17 12:18:29 +0100

    Initie la formule d'éligibilité à mobili-jeune

--- openfisca_france/model/prestations/mobili_jeune.py
+++ openfisca_france/model/prestations/mobili_jeune.py
@@ -7,3 +7,11 @@ class mobili_jeune_eligibilite(Variable):
     entity = Individu
     definition_period = MONTH
     reference = "https://www.actionlogement.fr/l-aide-mobili-jeune"
+
+    def formula(individu, period, parameters):
+        condition_age = individu("age", period) >= 30
+        condition_contrat = individu("alternant", period) * (
+            individu("apprenti", period) 
+            + individu("professionnalisation", period)
+            )
+        return condition_age * condition_contrat
