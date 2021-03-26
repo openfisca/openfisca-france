290380f880ec3f4e61308fc3b699e4940db264ef
Thomas Guillet <thomas+github@palya.fr>
2021-03-26 10:02:30 +0100

    Ajoute une première version de l'éligibilité aux logements du CROUS

--- openfisca_france/model/crous.py
+++ openfisca_france/model/crous.py
@@ -3,7 +3,7 @@ from openfisca_france.model.base import Individu, Variable, MONTH
 
 class crous_logement_eligibilite(Variable):
     entity = Individu
-    value_type = int
+    value_type = bool
     label = "Indicatrice indiquant une forte probabilité à être éligible aux logements proposés par le CROUS"
     definition_period = MONTH
 
