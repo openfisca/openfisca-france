3826c756cb2436866a0bfd4d68e8c5ce0cebcc45
Thomas Guillet <thomas+github@palya.fr>
2021-03-31 14:54:37 +0200

    Montée de version mineure et mise à jour du CHANGELOG

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "51.10.0",
+    version = "51.11.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
