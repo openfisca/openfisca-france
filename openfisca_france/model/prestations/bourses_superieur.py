1ea1b92778e3bccf6b2af7f2f3c851269ac897bb
sandcha <sandcha@users.noreply.github.com>
2021-04-01 19:43:38 +0200

    Corrige les périodes de diplôme et bourse

--- openfisca_france/model/prestations/bourses_superieur.py
+++ openfisca_france/model/prestations/bourses_superieur.py
@@ -1,3 +1,4 @@
+from numpy import datetime64
 from openfisca_france.model.base import *
 
 
@@ -45,29 +46,35 @@ class aide_jeunes_diplomes_anciens_boursiers_eligibilite(Variable):
 
         diplome = individu("diplome", period)
         date_diplome = individu("date_diplome", period)
+        annee_validation_diplome = date_diplome.astype('datetime64[Y]')
+
         condition_diplome = (
             ( diplome == TypesDiplome.niveau_5 )
             + ( diplome == TypesDiplome.niveau_6 )
             + ( diplome == TypesDiplome.niveau_7 )
             + ( diplome == TypesDiplome.niveau_8 )
             ) * (
-               date_diplome.this_year == '2020'
-               + date_diplome.this_year == '2021'
-               )  # à améliorer
+               ( annee_validation_diplome == datetime64('2020') )
+               + ( annee_validation_diplome == datetime64('2021') ) 
+               )
 
-        bourse_criteres_sociaux = individu("bourse_criteres_sociaux", date_diplome.last_year) > 0  # à améliorer
+        # bourse au cours de la dernière année de préparation du diplôme 
+        condition_periode_bourse = (
+            ( individu("bourse_criteres_sociaux", 2020, options = [ADD]) > 0 )
+            + (individu("bourse_criteres_sociaux", 2021, options = [ADD]) > 0 )
+        )
 
         # Être inscrit sur la liste des demandeurs d’emploi
         demandeur_emploi = individu("activite", period) == TypesActivite.chomeur
         pas_en_formation = not_(individu("formation", period))
         condition_activite = demandeur_emploi * pas_en_formation
 
-        condition_non_cumul = (
+        condition_non_cumul = not_(
             individu("are", period)
             * individu("ass", period)
             * individu("allocation_travailleur_independant", period)
             * individu("rsa", period)
             * individu("garantie_jeunes", period)
-            ) <= 0.
+            )
 
-        return condition_age * condition_diplome * bourse_criteres_sociaux * condition_activite * condition_non_cumul
+        return condition_age * condition_diplome * condition_periode_bourse * condition_activite * condition_non_cumul
