750b9293c2654ddee3837a59ded535c4ceec7d51
sandcha <sandcha@users.noreply.github.com>
2021-04-02 18:03:37 +0200

    Ajoute la condition d'étudiant du supérieur

--- openfisca_france/model/prestations/enseignement_superieur/aide_merite.py
+++ openfisca_france/model/prestations/enseignement_superieur/aide_merite.py
@@ -8,6 +8,7 @@ class aide_merite_eligibilite(Variable):
     definition_period = MONTH
     reference = [
         "https://www.etudiant.gouv.fr/fr/aide-au-merite-1291",
+        "https://www.service-public.fr/particuliers/vosdroits/F1010",
         "https://www.legifrance.gouv.fr/codes/id/LEGISCTA000030722211/"
         ]
     documentation = '''
@@ -17,9 +18,15 @@ class aide_merite_eligibilite(Variable):
     Non modélisé : 
     L'étudiant respecte les conditions d'inscription pédagogique, d'assiduité
     et de présentation aux examens (non applicable si nouveau bachelier).
+    L'étudiant éligible à la bourse sur critères sociaux et éligible à une aide au mérite
+    en année universitaire N-1 et ayant réalisé un Service Civique
+    au titre de cette même année, peut percevoir son aide au mérite en N.
     '''
 
     def formula(individu, period, parameters):
+        # l'individu intègre un établissement supérieur à la rentrée
+        etudiant = individu('etudiant', period)
+
         bourse_criteres_sociaux = individu("bourse_criteres_sociaux", period)
         allocation_annuelle = individu("allocation_annuelle", period)
         condition_ressources = bourse_criteres_sociaux + allocation_annuelle
@@ -27,4 +34,4 @@ class aide_merite_eligibilite(Variable):
         mention_baccalaureat = individu("mention_baccalaureat", period)
         condition_mention = mention_baccalaureat == TypesMention.mention_tres_bien
 
-        return condition_ressources * condition_mention
+        return etudiant * condition_ressources * condition_mention
