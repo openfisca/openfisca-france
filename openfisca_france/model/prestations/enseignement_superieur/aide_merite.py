9ae61de3bc4510289a1c16d78349cffbe2dda406
sandcha <sandcha@users.noreply.github.com>
2021-04-02 17:00:46 +0200

    Initie un calcul d'éligibilité à l'aide au mérite

--- openfisca_france/model/prestations/enseignement_superieur/aide_merite.py
+++ openfisca_france/model/prestations/enseignement_superieur/aide_merite.py
@@ -11,5 +11,20 @@ class aide_merite_eligibilite(Variable):
         "https://www.legifrance.gouv.fr/codes/id/LEGISCTA000030722211/"
         ]
     documentation = '''
-    Complémentaire à la bourse sur critères sociaux, pour les étudiants les plus méritants.
+    Complémentaire à la bourse sur critères sociaux, pour les étudiants
+    ayant eu mention très bien au baccalauréat.
+
+    Non modélisé : 
+    L'étudiant respecte les conditions d'inscription pédagogique, d'assiduité
+    et de présentation aux examens (non applicable si nouveau bachelier).
     '''
+
+    def formula(individu, period, parameters):
+        bourse_criteres_sociaux = individu("bourse_criteres_sociaux", period)
+        allocation_annuelle = individu("allocation_annuelle", period)
+        condition_ressources = bourse_criteres_sociaux + allocation_annuelle
+
+        mention_baccalaureat = individu("mention_baccalaureat", period)
+        condition_mention = mention_baccalaureat == TypesMention.mention_tres_bien
+
+        return condition_ressources * condition_mention
