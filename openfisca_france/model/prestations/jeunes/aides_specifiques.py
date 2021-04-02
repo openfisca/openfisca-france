c8ee532af1ffa1c5b819c51880c771b5a6c04c1d
sandcha <sandcha@users.noreply.github.com>
2021-04-02 16:31:21 +0200

    Initie l'allocation annuelle

---
+++ openfisca_france/model/prestations/jeunes/aides_specifiques.py
@@ -0,0 +1,18 @@
+from openfisca_france.model.base import Variable, Individu, MONTH
+
+
+class allocation_annuelle(Variable):
+    value_type = float
+    entity = Individu
+    label = "Allocation annuelle (type d'aide spécifique)"
+    definition_period = MONTH
+    reference = [
+        "https://www.education.gouv.fr/bo/14/Hebdo40/MENS1420893C.htm",
+        "https://www.etudiant.gouv.fr/fr/aides-specifiques-1306"
+        ]
+    documentation = '''
+    Accordée à l'étudiant qui rencontre des difficultés pérennes.
+    Cette aide est l'équivalent d'une bourse sur critères sociaux. 
+    Elle permet ainsi d'être exonéré·e des droits d'inscription universitaires 
+    et de la contribution vie étudiante et de campus (CVEC).
+    '''
