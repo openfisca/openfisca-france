084dffd5bd7d8878e5373d5ae4acf22dcc289e0c
Matti Schneider <matti@mes-aides.org>
2021-04-07 18:03:54 +0200

    Bump version to 51.16.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "51.15.0",
+    version = "51.16.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
