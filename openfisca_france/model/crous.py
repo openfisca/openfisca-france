82a27a3fcf9f011de0ecc801aecd2e406fbf0fc3
Thomas Guillet <thomas+github@palya.fr>
2021-04-13 11:49:33 +0200

    Explicite les choix concernant l'éligibilité au logement CROUS

--- openfisca_france/model/crous.py
+++ openfisca_france/model/crous.py
@@ -4,8 +4,17 @@ from openfisca_france.model.base import Individu, Variable, MONTH
 class crous_logement_eligibilite(Variable):
     entity = Individu
     value_type = bool
-    label = "Indicatrice indiquant une forte probabilité à être éligible aux logements proposés par le CROUS"
+    label = "Éligibilité aux logements proposés par le CROUS"
     definition_period = MONTH
+    reference = "https://www.etudiant.gouv.fr/fr/comment-se-passe-l-attribution-des-logements-en-residence-73"
 
     def formula(individu, period):
+        """
+        Tous les étudiants sont éligibles aux logements proposés par le CROUs mais
+        les non boursiers ont une probabilité très faible d'en obtenir.
+        Pour une résidence donnée, les dossiers sont classés par « indice social » défini par :
+        - revenu brut global (+ 1) divisé par le nombre total de points de charge (+ 1)
+        Pour informer de façon pertinente c'est à dire sans trop de faux positif ni trop de faux négatifs,
+        les étudiants boursiers sont considérés éligibles et les autre non
+        """
         return individu('bourse_criteres_sociaux_echelon', period) >= 0
