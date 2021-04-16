2968ffdead82656b8e2a8ca956aa9862ec3040ce
sandcha <sandcha@users.noreply.github.com>
2021-04-16 17:39:11 +0100

    Ajoute le calcul du montant d'aide au mérite annuel

--- openfisca_france/model/prestations/enseignement_superieur/aide_merite.py
+++ openfisca_france/model/prestations/enseignement_superieur/aide_merite.py
@@ -12,19 +12,22 @@ class aide_merite_eligibilite(Variable):
         "https://www.service-public.fr/particuliers/vosdroits/F1010",
         "https://www.legifrance.gouv.fr/codes/id/LEGISCTA000030722211/"
         ]
+    set_input = set_input_dispatch_by_period
     documentation = '''
     Complémentaire à la bourse sur critères sociaux, pour les étudiants
     ayant eu mention très bien au baccalauréat.
 
-    Non modélisé : 
+    Non modélisé :
     L'étudiant respecte les conditions d'inscription pédagogique, d'assiduité
-    et de présentation aux examens (non applicable si nouveau bachelier).
+    et de présentation aux examens (non applicable si nouveau bachelier)
+    ou redouble pour raisons médicales.
     L'étudiant éligible à la bourse sur critères sociaux et éligible à une aide au mérite
     en année universitaire N-1 et ayant réalisé un Service Civique
     au titre de cette même année, peut percevoir son aide au mérite en N.
+    Un étudiant ne peut bénéficier de plus de 3 fois de l'aide au mérite.
     '''
 
-    def formula(individu, period, parameters):
+    def formula(individu, period):
 
         def periode_universitaire_precedente(mois_calcul):
             nb_mois_annee_courante = mois_calcul.date.month - mois_calcul.this_year.date.month + 1
@@ -58,3 +61,25 @@ class aide_merite_eligibilite(Variable):
             )
 
         return etudiant * condition_ressources * ( condition_mention + aide_merite_eligibilite_an_dernier )
+
+
+class aide_merite_montant(Variable):
+    value_type = float
+    entity = Individu
+    label = "Montant de l'aide au mérite pour l'année universitaire"
+    definition_period = YEAR
+    reference = [
+        "https://www.etudiant.gouv.fr/fr/aide-au-merite-1291",
+        "https://www.service-public.fr/particuliers/vosdroits/F1010",
+        "https://www.legifrance.gouv.fr/codes/id/LEGISCTA000030722211/"
+        ]
+    documentation = '''
+    Aide versée en 9 mensualités.
+
+    Non modélisé :
+    Pour un baccalauréat obtenu avant 2015, le montant de l'aide est de 1800€/an.
+    '''
+    
+    def formula(individu, period, parameters):
+        aide_merite_eligibilite = individu("aide_merite_eligibilite", period.first_month)
+        return aide_merite_eligibilite * parameters(period).bourses_enseignement_superieur.aide_merite.montant_annuel
