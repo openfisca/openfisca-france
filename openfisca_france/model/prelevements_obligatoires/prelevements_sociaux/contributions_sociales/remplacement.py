e5071f5a8d35f59506b7db94efdf219bd4940461
Sasha-Laniece <sasha.laniece@gmail.com>
2021-05-24 17:07:16 +0200

    Fix flake8
    (cherry picked from commit 645d4b04b70e716ae44b6c9d270494561df1d0cb)

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/contributions_sociales/remplacement.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/contributions_sociales/remplacement.py
@@ -1,14 +1,13 @@
 import logging
 
 from openfisca_france.model.base import *
-from openfisca_france.model.prelevements_obligatoires.prelevements_sociaux.contributions_sociales.base import (
-    montant_csg_crds
-    )
+from openfisca_france.model.prelevements_obligatoires.prelevements_sociaux.contributions_sociales.base import (montant_csg_crds)
+
 log = logging.getLogger(__name__)
 
 
 class TypesTauxCSGRemplacement(Enum):
-    __order__ = 'non_renseigne exonere taux_reduit taux_plein'  # Needed to preserve the enum order in Python 2
+    __order__ = "non_renseigne exonere taux_reduit taux_plein"  # Needed to preserve the enum order in Python 2
     non_renseigne = "Non renseigné/non pertinent"
     exonere = "Exonéré"
     taux_reduit = "Taux réduit"
@@ -25,24 +24,18 @@ class taux_csg_remplacement(Variable):
     set_input = set_input_dispatch_by_period
 
     def formula_2015(individu, period, parameters):
-        rfr = individu.foyer_fiscal('rfr', period = period.n_2)
-        nbptr = individu.foyer_fiscal('nbptr', period = period.n_2)
+        rfr = individu.foyer_fiscal("rfr", period=period.n_2)
+        nbptr = individu.foyer_fiscal("nbptr", period=period.n_2)
         seuils = parameters(period).prelevements_sociaux.contributions_sociales.csg.retraite_invalidite.seuils
-        seuil_exoneration = seuils.seuil_rfr1 + (nbptr - 1) * seuils.rfr2_demi_part_suppl 
+        seuil_exoneration = (seuils.seuil_rfr1 + (nbptr - 1) * seuils.rfr2_demi_part_suppl)
         seuil_reduction = seuils.seuil_rfr2 + (nbptr - 1) * seuils.rfr2_demi_part_suppl
-        taux_csg_remplacement = where(
-            rfr <= seuil_exoneration,
-            TypesTauxCSGRemplacement.exonere,
-            where(
-                rfr <= seuil_reduction,
-                TypesTauxCSGRemplacement.taux_reduit,
-                TypesTauxCSGRemplacement.taux_plein,
-                )
-            )
+        taux_csg_remplacement = where(rfr <= seuil_exoneration, TypesTauxCSGRemplacement.exonere,
+            where(rfr <= seuil_reduction, TypesTauxCSGRemplacement.taux_reduit, TypesTauxCSGRemplacement.taux_plein))
         return taux_csg_remplacement
 
 
 # Allocations chômage
+
 
 class csg_deductible_chomage(Variable):
     calculate_output = calculate_output_add
@@ -53,36 +46,20 @@ class csg_deductible_chomage(Variable):
     definition_period = MONTH
 
     def formula(individu, period, parameters):
-        chomage_brut = individu('chomage_brut', period)
-        csg_imposable_chomage = individu('csg_imposable_chomage', period)
-        taux_csg_remplacement = individu('taux_csg_remplacement', period)
+        chomage_brut = individu("chomage_brut", period)
+        csg_imposable_chomage = individu("csg_imposable_chomage", period)
+        taux_csg_remplacement = individu("taux_csg_remplacement", period)
         parameters = parameters(period)
         montant_csg = montant_csg_crds(
             base_avec_abattement = chomage_brut,
             indicatrice_taux_plein = (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_plein),
             indicatrice_taux_reduit = (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_reduit),
             law_node = parameters.prelevements_sociaux.contributions_sociales.csg.chomage.deductible,
-            plafond_securite_sociale = parameters.cotsoc.gen.plafond_securite_sociale,
-            )
+            plafond_securite_sociale = parameters.cotsoc.gen.plafond_securite_sociale)
         nbh_travail = 35 * 52 / 12  # = 151.67  # TODO: depuis 2001 mais avant ?
-
-        cho_seuil_exo = (
-            parameters.prelevements_sociaux.contributions_sociales.csg.chomage.min_exo
-            * nbh_travail
-            * parameters.cotsoc.gen.smic_h_b
-            )
-
-        csg_deductible_chomage = max_(
-            - montant_csg
-            - max_(cho_seuil_exo - (
-                chomage_brut
-                + csg_imposable_chomage
-                + montant_csg
-                ), 0),
-            0,
-            )
-
-        return - csg_deductible_chomage
+        cho_seuil_exo = (parameters.prelevements_sociaux.contributions_sociales.csg.chomage.min_exo * nbh_travail * parameters.cotsoc.gen.smic_h_b)
+        csg_deductible_chomage = max_(-montant_csg - max_(cho_seuil_exo - (chomage_brut + csg_imposable_chomage + montant_csg), 0), 0)
+        return -csg_deductible_chomage
 
 
 class csg_imposable_chomage(Variable):
@@ -94,22 +71,17 @@ class csg_imposable_chomage(Variable):
     definition_period = MONTH
 
     def formula(individu, period, parameters):
-        chomage_brut = individu('chomage_brut', period)
+        chomage_brut = individu("chomage_brut", period)
         parameters = parameters(period)
 
         montant_csg = montant_csg_crds(
-            base_avec_abattement = chomage_brut,
-            law_node = parameters.prelevements_sociaux.contributions_sociales.csg.chomage.imposable,
-            plafond_securite_sociale = parameters.cotsoc.gen.plafond_securite_sociale,
-            )
+            base_avec_abattement=chomage_brut,
+            law_node=parameters.prelevements_sociaux.contributions_sociales.csg.chomage.imposable,
+            plafond_securite_sociale=parameters.cotsoc.gen.plafond_securite_sociale)
         nbh_travail = 35 * 52 / 12  # = 151.67  # TODO: depuis 2001 mais avant ?
-        cho_seuil_exo = (
-            parameters.prelevements_sociaux.contributions_sociales.csg.chomage.min_exo
-            * nbh_travail
-            * parameters.cotsoc.gen.smic_h_b
-            )
-        csg_imposable_chomage = max_(- montant_csg - max_(cho_seuil_exo - (chomage_brut + montant_csg), 0), 0)
-        return - csg_imposable_chomage
+        cho_seuil_exo = (parameters.prelevements_sociaux.contributions_sociales.csg.chomage.min_exo * nbh_travail * parameters.cotsoc.gen.smic_h_b)
+        csg_imposable_chomage = max_(-montant_csg - max_(cho_seuil_exo - (chomage_brut + montant_csg), 0), 0)
+        return -csg_imposable_chomage
 
 
 class crds_chomage(Variable):
@@ -121,53 +93,44 @@ class crds_chomage(Variable):
     definition_period = MONTH
 
     def formula(individu, period, parameters):
-        chomage_brut = individu('chomage_brut', period)
-        csg_deductible_chomage = individu('csg_deductible_chomage', period)
-        csg_imposable_chomage = individu('csg_imposable_chomage', period)
-        taux_csg_remplacement = individu('taux_csg_remplacement', period)
+        chomage_brut = individu("chomage_brut", period)
+        csg_deductible_chomage = individu("csg_deductible_chomage", period)
+        csg_imposable_chomage = individu("csg_imposable_chomage", period)
+        taux_csg_remplacement = individu("taux_csg_remplacement", period)
         law = parameters(period)
         smic_h_b = law.cotsoc.gen.smic_h_b
         # salaire_mensuel_reference = chomage_brut / .7
         # heures_mensuelles = min_(salaire_mensuel_reference / smic_h_b, 35 * 52 / 12)  # TODO: depuis 2001 mais avant ?
         heures_mensuelles = 35 * 52 / 12
-        cho_seuil_exo = law.prelevements_sociaux.contributions_sociales.csg.chomage.min_exo * heures_mensuelles * smic_h_b
-        eligible = (
-            (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_reduit)
-            + (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_plein)
-            )
-        montant_crds = montant_csg_crds(
-            base_avec_abattement = chomage_brut,
-            law_node = law.prelevements_sociaux.contributions_sociales.crds.activite,
-            plafond_securite_sociale = law.cotsoc.gen.plafond_securite_sociale,
-            ) * eligible
-
-        crds_chomage = max_(
-            -montant_crds - max_(
-                cho_seuil_exo - (chomage_brut + csg_imposable_chomage + csg_deductible_chomage + montant_crds), 0
-                ), 0
-            )
+        cho_seuil_exo = (law.prelevements_sociaux.contributions_sociales.csg.chomage.min_exo * heures_mensuelles * smic_h_b)
+        eligible = (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_reduit) + (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_plein)
+        montant_crds = (montant_csg_crds(
+            base_avec_abattement=chomage_brut,
+            law_node=law.prelevements_sociaux.contributions_sociales.crds.activite,
+            plafond_securite_sociale=law.cotsoc.gen.plafond_securite_sociale)
+            * eligible)
+        crds_chomage = max_(-montant_crds - max_(cho_seuil_exo - (chomage_brut + csg_imposable_chomage + csg_deductible_chomage + montant_crds), 0), 0)
         return -crds_chomage
 
 
 class chomage_imposable(Variable):
     value_type = float
-    unit = 'currency'
+    unit = "currency"
     cerfa_field = {
         0: "1AP",
         1: "1BP",
         2: "1CP",
         3: "1DP",
-        4: "1EP",
-        }
+        4: "1EP"}
     entity = Individu
     label = "Allocations chômage imposables"
     set_input = set_input_divide_by_period
-    reference = "http://www.insee.fr/fr/methodes/default.asp?page=definitions/chomage.htm"
+    reference = ("http://www.insee.fr/fr/methodes/default.asp?page=definitions/chomage.htm")
     definition_period = MONTH
 
     def formula(individu, period):
-        chomage_brut = individu('chomage_brut', period)
-        csg_deductible_chomage = individu('csg_deductible_chomage', period)
+        chomage_brut = individu("chomage_brut", period)
+        csg_deductible_chomage = individu("csg_deductible_chomage", period)
 
         return chomage_brut + csg_deductible_chomage
 
@@ -181,14 +144,15 @@ class chomage_net(Variable):
     definition_period = MONTH
 
     def formula(individu, period):
-        chomage_imposable = individu('chomage_imposable', period)
-        csg_imposable_chomage = individu('csg_imposable_chomage', period)
-        crds_chomage = individu('crds_chomage', period)
+        chomage_imposable = individu("chomage_imposable", period)
+        csg_imposable_chomage = individu("csg_imposable_chomage", period)
+        crds_chomage = individu("crds_chomage", period)
 
         return chomage_imposable + csg_imposable_chomage + crds_chomage
 
 
 # Pensions
+
 
 class csg_deductible_retraite(Variable):
     calculate_output = calculate_output_add
@@ -199,17 +163,16 @@ class csg_deductible_retraite(Variable):
     definition_period = MONTH
 
     def formula(individu, period, parameters):
-        retraite_brute = individu('retraite_brute', period)
-        taux_csg_remplacement = individu('taux_csg_remplacement', period)
+        retraite_brute = individu("retraite_brute", period)
+        taux_csg_remplacement = individu("taux_csg_remplacement", period)
         law = parameters(period)
 
         montant_csg = montant_csg_crds(
-            base_sans_abattement = retraite_brute,
-            indicatrice_taux_plein = (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_plein),
-            indicatrice_taux_reduit = (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_reduit),
-            law_node = law.prelevements_sociaux.contributions_sociales.csg.retraite_invalidite.deductible,
-            plafond_securite_sociale = law.cotsoc.gen.plafond_securite_sociale,
-            )
+            base_sans_abattement=retraite_brute,
+            indicatrice_taux_plein=(taux_csg_remplacement == TypesTauxCSGRemplacement.taux_plein),
+            indicatrice_taux_reduit=(taux_csg_remplacement == TypesTauxCSGRemplacement.taux_reduit),
+            law_node=law.prelevements_sociaux.contributions_sociales.csg.retraite_invalidite.deductible,
+            plafond_securite_sociale=law.cotsoc.gen.plafond_securite_sociale)
         return montant_csg
 
 
@@ -222,14 +185,13 @@ class csg_imposable_retraite(Variable):
     definition_period = MONTH
 
     def formula(individu, period, parameters):
-        retraite_brute = individu('retraite_brute', period)
+        retraite_brute = individu("retraite_brute", period)
         law = parameters(period)
 
         montant_csg = montant_csg_crds(
-            base_sans_abattement = retraite_brute,
-            law_node = law.prelevements_sociaux.contributions_sociales.csg.retraite_invalidite.imposable,
-            plafond_securite_sociale = law.cotsoc.gen.plafond_securite_sociale,
-            )
+            base_sans_abattement=retraite_brute,
+            law_node=law.prelevements_sociaux.contributions_sociales.csg.retraite_invalidite.imposable,
+            plafond_securite_sociale=law.cotsoc.gen.plafond_securite_sociale)
         return montant_csg
 
 
@@ -242,15 +204,16 @@ class crds_retraite(Variable):
     definition_period = MONTH
 
     def formula(individu, period, parameters):
-        retraite_brute = individu('retraite_brute', period)
-        taux_csg_remplacement = individu('taux_csg_remplacement', period)
+        retraite_brute = individu("retraite_brute", period)
+        taux_csg_remplacement = individu("taux_csg_remplacement", period)
         law = parameters(period)
 
-        montant_crds = montant_csg_crds(
-            base_sans_abattement = retraite_brute,
-            law_node = law.prelevements_sociaux.contributions_sociales.crds.retraite,
-            plafond_securite_sociale = law.cotsoc.gen.plafond_securite_sociale,
-            ) * (taux_csg_remplacement == TypesTauxCSGRemplacement.exonere)
+        montant_crds = (
+            montant_csg_crds(
+                base_sans_abattement=retraite_brute,
+                law_node=law.prelevements_sociaux.contributions_sociales.crds.retraite,
+                plafond_securite_sociale=law.cotsoc.gen.plafond_securite_sociale,) * (taux_csg_remplacement == TypesTauxCSGRemplacement.exonere)
+            )
         return montant_crds
 
 
@@ -262,26 +225,17 @@ class casa(Variable):
     definition_period = MONTH
 
     def formula_2013_04_01(individu, period, parameters):
-        retraite_brute = individu('retraite_brute', period = period)
-        taux_csg_remplacement = individu('taux_csg_remplacement', period)
+        retraite_brute = individu("retraite_brute", period=period)
+        taux_csg_remplacement = individu("taux_csg_remplacement", period)
         contributions = parameters(period).prelevements_sociaux.cotisations_securite_sociale_regime_general
-        casa = (
-            (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_plein)
-            * contributions.casa.calc(retraite_brute)
-            )
-        return - casa
+        casa = (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_plein) * contributions.casa.calc(retraite_brute)
+        return -casa
 
 
 class retraite_imposable(Variable):
-    unit = 'currency'
+    unit = "currency"
     value_type = float
-    cerfa_field = {
-        0: "1AS",
-        1: "1BS",
-        2: "1CS",
-        3: "1DS",
-        4: "1ES",
-        }
+    cerfa_field = {0: "1AS", 1: "1BS", 2: "1CS", 3: "1DS", 4: "1ES"}
     entity = Individu
     label = "Retraites au sens strict imposables (rentes à titre onéreux exclues)"
     set_input = set_input_divide_by_period
@@ -289,8 +243,8 @@ class retraite_imposable(Variable):
     definition_period = MONTH
 
     def formula(individu, period):
-        retraite_brute = individu('retraite_brute', period)
-        csg_deductible_retraite = individu('csg_deductible_retraite', period)
+        retraite_brute = individu("retraite_brute", period)
+        csg_deductible_retraite = individu("csg_deductible_retraite", period)
 
         return retraite_brute + csg_deductible_retraite
 
@@ -304,10 +258,10 @@ class retraite_nette(Variable):
     definition_period = MONTH
 
     def formula(individu, period):
-        retraite_imposable = individu('retraite_imposable', period)
-        casa = individu('casa', period)
-        csg_imposable_retraite = individu('csg_imposable_retraite', period)
-        crds_retraite = individu('crds_retraite', period)
+        retraite_imposable = individu("retraite_imposable", period)
+        casa = individu("casa", period)
+        csg_imposable_retraite = individu("csg_imposable_retraite", period)
+        crds_retraite = individu("crds_retraite", period)
 
         return retraite_imposable + csg_imposable_retraite + crds_retraite + casa
 
@@ -320,13 +274,13 @@ class crds_pfam(Variable):
     definition_period = YEAR
 
     def formula(famille, period, parameters):
-        af = famille('af', period, options = [ADD])
-        cf = famille('cf', period, options = [ADD])
-        asf = famille('asf', period, options = [ADD])
-        ars = famille('ars', period)
-        paje = famille('paje', period, options = [ADD])
-        ape = famille('ape', period, options = [ADD])
-        apje = famille('apje', period, options = [ADD])
+        af = famille("af", period, options=[ADD])
+        cf = famille("cf", period, options=[ADD])
+        asf = famille("asf", period, options=[ADD])
+        ars = famille("ars", period)
+        paje = famille("paje", period, options=[ADD])
+        ape = famille("ape", period, options=[ADD])
+        apje = famille("apje", period, options=[ADD])
         taux_crds = parameters(period).prelevements_sociaux.contributions_sociales.crds.taux_global
 
         return -(af + cf + asf + ars + paje + ape + apje) * taux_crds
