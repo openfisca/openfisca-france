b5d2c75ca79fdd2506dc1808c1656eb09edd2e7e
Sasha-Laniece <sasha.laniece@gmail.com>
2021-05-25 11:39:00 +0200

    Harmonise le fichier parameters.prelevements_sociaux.cotisations_securite_sociale_regime_general.casa.yaml vers le dossier parameters.prelevements_sociaux.cotisations_securite_sociale_regime_general.casa
    Attention: pour remplacement.py (l269), la formule est updatée mais ne prend pas en compte les seuils

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/contributions_sociales/remplacement.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/contributions_sociales/remplacement.py
@@ -225,12 +225,15 @@ class casa(Variable):
     definition_period = MONTH
 
     def formula_2013_04_01(individu, period, parameters):
-        retraite_brute = individu("retraite_brute", period=period)
-        taux_csg_remplacement = individu("taux_csg_remplacement", period)
-        contributions = parameters(period).prelevements_sociaux.cotisations_securite_sociale_regime_general
-        casa = (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_plein) * contributions.casa.calc(retraite_brute)
-        return -casa
-
+        retraite_brute = individu('retraite_brute', period = period)
+        taux_csg_remplacement = individu('taux_csg_remplacement', period)
+        cotisations = parameters(period).prelevements_sociaux.cotisations_securite_sociale_regime_general
+        casa = (
+            (taux_csg_remplacement == TypesTauxCSGRemplacement.taux_plein)
+            * cotisations.casa.pensions_retraite_preretraite_invalidite.calc(retraite_brute)
+            )
+        return - casa
+# TODO Mettre à jour la formule pour prendre en compte les seuils d'exonération
 
 class retraite_imposable(Variable):
     unit = "currency"
