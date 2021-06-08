4d722f8c863dc5fe28f38c040272380e29ec9cda
Sasha-Laniece <sasha.laniece@gmail.com>
2021-06-08 15:43:36 +0200

    En cours

--- openfisca_france/print_pat_and_sal.py
+++ openfisca_france/print_pat_and_sal.py
@@ -4,7 +4,7 @@ Ce script sert à enregistrer les variables pat & sal avant modification
 import os
 import functools
 import re
-import json
+import yaml # import json
 from pprint import pprint
 from openfisca_core.parameters import ParameterNode
 
@@ -29,24 +29,31 @@ def get_directory_structure(rootdir):
     return dir
 
 
-# PAT 
+# Create PAT dict 
 startpath = "openfisca_france/parameters/cotsoc/pat"
 pat_structure = get_directory_structure(startpath)
-print(type(pat_structure))
-# Dir    
-json_file = json.dumps(pat_structure)
-with open("pat_structure.json", "w") as outfile:
-    outfile.write(json_file)
+# print(type(pat_structure))
+# Dir Yaml
+f = open('pat_structure.yaml', 'w+')
+yaml.dump(pat_structure, f, allow_unicode=True)
 # Tree    
 # pprint(pat_structure)
 
-# SAL 
+# Create SAL dict 
 startpath = "openfisca_france/parameters/cotsoc/sal"
 sal_structure = get_directory_structure(startpath)
 # Dir
-json_file = json.dumps(sal_structure)
-with open("sal_structure.json", "w") as outfile:
-    outfile.write(json_file)
+f = open('sal_structure.yaml', 'w+')
+yaml.dump(sal_structure, f, allow_unicode=True)
+# yaml.dump(sal_structure, f, allow_unicode=True, default_flow_style=False)
 # Tree    
 # pprint(sal_structure)
 
+
+# Load pat & sal as a: <class 'openfisca_core.parameters.parameter_node.ParameterNode'>
+print(type(pat_structure))
+cwd = os.getcwd()
+print(cwd)
+
+pat = ParameterNode(name="pat", directory_path = "/", file_path = "pat_structure.yaml")
+print(type(pat))
