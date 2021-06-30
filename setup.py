22e0192a53bd653592c2d47a1e17e6c2dc160dc7
@ChloeLallemand <chloe.lallemand@ipp.eu>
2021-06-30 14:35:36 +0200

    Passe à la version 55.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "54.0.3",
+    version = "55.0.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
