90e8f815c785cea8c3ae75aa64201b2fa171a70d
Thomas Guillet <thomas@palya.fr>
2021-07-05 14:34:55 +0200

    fixup! Ajoute les années habituelles de doctorat

--- openfisca_france/model/prestations/education.py
+++ openfisca_france/model/prestations/education.py
@@ -336,7 +336,7 @@ class statuts_etablissement_scolaire(Variable):
 
 
 class TypesClasse(Enum):
-    __order__ = 'seconde premiere terminale licence_1 licence_2 licence_3 master_1 master_2 doctorat_1 autre'
+    __order__ = 'seconde premiere terminale licence_1 licence_2 licence_3 master_1 master_2 doctorat_1 doctorat_2 doctorat_3 autre'
     seconde = 'Seconde'
     premiere = 'Première'
     terminale = 'Terminale'
