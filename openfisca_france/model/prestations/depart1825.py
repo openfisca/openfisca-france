df79d20ce4aa0cf22245015898c756aaa163b790
Quentin Madura <quentin.madura@gmail.com>
2021-07-07 14:17:48 +0200

    Adapte la formule d'éligibilité suite à la revue de code

--- openfisca_france/model/prestations/depart1825.py
+++ openfisca_france/model/prestations/depart1825.py
@@ -1,26 +1,7 @@
 from openfisca_france.model.base import Variable, Individu, MONTH, TypesActivite, max_
 
 
-class depart1825_eligibilite_financiere(Variable):
-    value_type = bool
-    label = "Éligibilité au dispositif Départ 18-25 en raison des ressources du foyer"
-    entity = Individu
-    definition_period = MONTH
-    reference = [
-        "https://programme-depart-1825.com/eligibilite/",
-        "https://www.ancv.com/actualites/le-magazine/depart-1825-un-nouveau-programme-pour-les-jeunes-de-18-25-ans"
-        ]
-
-    def formula(individu, period, parameters):
-        nbptr = individu.foyer_fiscal('nbptr', period.n_2)
-        plafond_ressources = parameters(period).prestations.depart1825.plafond_ressources
-        plafond_ressources = plafond_ressources.base + 2 * max_(0, nbptr - 1) * plafond_ressources.par_demi_part_supplementaire
-
-        ressources = individu.foyer_fiscal('rfr', period.n_2)
-        return ressources <= plafond_ressources
-
-
-class depart1825_eligibilite_statut(Variable):
+class depart1825_eligibilite(Variable):
     """
     Situations non prises en compte
     - en contrat aidé,
@@ -29,41 +10,29 @@ class depart1825_eligibilite_statut(Variable):
     - jeunes suivis par l'Aide Sociale à l'Enfance.​
     """
     value_type = bool
-    label = "Éligibilité au dispositif Départ 18-25 en raison de la situation du jeune"
-    entity = Individu
-    definition_period = MONTH
-    reference = [
-        "https://programme-depart-1825.com/eligibilite/"
-        ]
-
-    def formula(individu, period):
-        etudiant_boursier = (individu('activite', period) == TypesActivite.etudiant) * individu('boursier', period)
-        alternant = individu('alternant', period)
-        garantie_jeunes = individu('garantie_jeunes', period) > 0
-
-        return etudiant_boursier + alternant + garantie_jeunes
-
-
-class depart1825_eligibilite(Variable):
-    value_type = bool
     label = "Éligibilité au dispositif départ 18-25"
     entity = Individu
     definition_period = MONTH
     reference = [
         "https://programme-depart-1825.com/eligibilite/",
         "https://www.ancv.com/actualites/le-magazine/depart-1825-un-nouveau-programme-pour-les-jeunes-de-18-25-ans"
-        ]
+    ]
 
     def formula(individu, period, parameters):
         criteres_age = parameters(period).prestations.depart1825.age
         age = individu('age', period)
+        eligibilite_age = (criteres_age.minimum <= age) * (age <= criteres_age.maximum)
+        etudiant_boursier = (individu('activite', period) == TypesActivite.etudiant) * individu('boursier', period)
+        alternant = individu('alternant', period)
+        garantie_jeunes = individu('garantie_jeunes', period) > 0
+        eligibilite_statut = etudiant_boursier + alternant + garantie_jeunes
+        nbptr = individu.foyer_fiscal('nbptr', period.n_2)
+        plafond_ressources = parameters(period).prestations.depart1825.plafond_ressources
+        plafond_ressources = plafond_ressources.base + 2 * max_(0, nbptr - 1) * plafond_ressources.par_demi_part_supplementaire
 
-        eligibilite_statut = individu('depart1825_eligibilite_statut', period)
-        eligibilite_financiere = individu('depart1825_eligibilite_financiere', period)
-        eligible = eligibilite_statut + eligibilite_financiere
-
-        return (criteres_age.minimum <= age) * (age <= criteres_age.maximum) * eligible
-
+        ressources = individu.foyer_fiscal('rfr', period.n_2)
+        eligibilite_financiere = ressources <= plafond_ressources
+        return eligibilite_age * (eligibilite_statut + eligibilite_financiere)
 
 class depart1825_montant_maximum(Variable):
     value_type = float
