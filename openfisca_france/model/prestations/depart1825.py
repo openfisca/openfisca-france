e41cb95363cc74ded76bd8cb4f8ecf364dfa37fa
Quentin Madura <quentin.madura@gmail.com>
2021-07-09 12:00:42 +0200

    Aère le fichier de départ 18-25

--- openfisca_france/model/prestations/depart1825.py
+++ openfisca_france/model/prestations/depart1825.py
@@ -21,16 +21,29 @@ class depart1825_eligibilite(Variable):
     def formula(individu, period, parameters):
         criteres_age = parameters(period).prestations.depart1825.age
         age = individu('age', period)
+
+
         eligibilite_age = (criteres_age.minimum <= age) * (age <= criteres_age.maximum)
+
+
         etudiant_boursier = (individu('activite', period) == TypesActivite.etudiant) * individu('boursier', period)
         alternant = individu('alternant', period)
         garantie_jeunes = individu('garantie_jeunes', period) > 0
+
+
         eligibilite_statut = etudiant_boursier + alternant + garantie_jeunes
+
+
         nbptr = individu.foyer_fiscal('nbptr', period.n_2)
         plafond_ressources = parameters(period).prestations.depart1825.plafond_ressources
         plafond_ressources = plafond_ressources.base + 2 * max_(0, nbptr - 1) * plafond_ressources.par_demi_part_supplementaire
+
         ressources = individu.foyer_fiscal('rfr', period.n_2)
+
+
         eligibilite_financiere = ressources <= plafond_ressources
+
+
         return eligibilite_age * (eligibilite_statut + eligibilite_financiere)
 
 
@@ -45,6 +58,7 @@ class depart1825_montant_maximum(Variable):
         ]
 
     def formula(individu, period, parameters):
+
         montant = parameters(period).prestations.depart1825.montant_maximum
         eligibilite = individu('depart1825_eligibilite', period)
 
