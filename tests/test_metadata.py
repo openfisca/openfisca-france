ea41d653008d8f476beaedb153eb5bd28c5db168
@ChloeLallemand <chloe.lallemand@ipp.eu>
2021-07-09 13:53:58 +0200

    Corrige diverses erreurs

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,7 +1,7 @@
-# from .cache import tax_benefit_system
+from .cache import tax_benefit_system
 
 
-# def test_metadata():
-#     metadata = tax_benefit_system.get_package_metadata()
-#     assert metadata['name'] == 'openfisca-france'
-#     assert metadata['repository_url'] == 'https://github.com/openfisca/openfisca-france'
+def test_metadata():
+    metadata = tax_benefit_system.get_package_metadata()
+    assert metadata['name'] == 'openfisca-france'
+    assert metadata['repository_url'] == 'https://github.com/openfisca/openfisca-france'
