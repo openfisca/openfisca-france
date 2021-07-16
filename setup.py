acd692c6ec0e3024ec55904e26833157d0f8edbe
DorineLam <46896006+DorineLam@users.noreply.github.com>
2021-07-16 13:45:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "56.0.0",
+    version = "56.0.2",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
