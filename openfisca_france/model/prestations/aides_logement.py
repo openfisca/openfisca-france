e876d1a98965b5af61c66edba7a254344cea26ac
chloelallemand <55277136+clallemand@users.noreply.github.com>
2021-07-21 15:47:23 +0200

    Update openfisca_france/model/prestations/aides_logement.py
    Co-authored-by: pdp <7412441+pzuldp@users.noreply.github.com>

--- openfisca_france/model/prestations/aides_logement.py
+++ openfisca_france/model/prestations/aides_logement.py
@@ -520,7 +520,7 @@ class aide_logement_abattement_revenus_activite_professionnelle(Variable):
 class aide_logement_abattement_indemnites_chomage(Variable):
     value_type = float
     entity = Individu
-    label = "Conditon de l'abattement sur les indemnités chômages pour les personnes partant à la retraite (R351-13 du CCH)"
+    label = "Condition de l'abattement sur les indemnités chômages pour les personnes partant à la retraite (R351-13 du CCH)"
     definition_period = MONTH
     reference = [
         "Article 822-13 du code de la construction et de l'habitation",
