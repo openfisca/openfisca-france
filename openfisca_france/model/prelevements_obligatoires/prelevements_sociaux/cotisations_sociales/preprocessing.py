f72272bbbfd38d08d50be94ae7db2e5813b98975
@ChloeLallemand <chloe.lallemand@ipp.eu>
2021-08-02 10:06:22 +0200

    Supprime des anciens chemins du preprocessing

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
@@ -49,7 +49,6 @@ def build_pat(node_json):  # Ici node_json c'est le dossier 'parameters'
     # Initialisation
     noncadre = ParameterNode("noncadre", data={})
     pat.add_child('noncadre', noncadre)
-    pat.children['noncadre'].children.update(retraites.employeur.noncadre.children) # a supprimer
     pat.children['noncadre'].children.update(retraites.agff.employeur.noncadre.children)
     pat.children['noncadre'].children.update(retraites.arrco.employeur.noncadre.children)
     pat.children['noncadre'].children.update(commun.children)
@@ -58,7 +57,6 @@ def build_pat(node_json):  # Ici node_json c'est le dossier 'parameters'
     # Initialisation
     cadre = ParameterNode("cadre", data={})
     pat.add_child('cadre', cadre)
-    pat.children['cadre'].children.update(retraites.employeur.cadre.children) # a supprimer
     pat.children['cadre'].children.update(retraites.agff.employeur.cadre.children)
     pat.children['cadre'].children.update(retraites.arrco.employeur.cadre.children)
     pat.children['cadre'].children.update(retraites.agirc.employeur.children)
@@ -157,7 +155,6 @@ def build_sal(node_json):
     # Initialisation
     noncadre = ParameterNode("noncadre", data={})
     sal.add_child('noncadre', noncadre)
-    sal.children['noncadre'].children.update(retraites.salarie.noncadre.children) # a supprimer
     sal.children['noncadre'].children.update(retraites.agff.salarie.noncadre.children)
     sal.children['noncadre'].children.update(retraites.arrco.salarie.noncadre.children)
     sal.children['noncadre'].children.update(commun.children)
@@ -165,7 +162,6 @@ def build_sal(node_json):
     # Cadre
     cadre = ParameterNode("cadre", data={})
     sal.add_child('cadre', cadre)
-    sal.children['cadre'].children.update(retraites.salarie.cadre.children) # a supprimer
     sal.children['cadre'].children.update(retraites.agff.salarie.cadre.children)
     sal.children['cadre'].children.update(retraites.arrco.salarie.cadre.children)
     sal.children['cadre'].children.update(retraites.agirc.salarie.children)
