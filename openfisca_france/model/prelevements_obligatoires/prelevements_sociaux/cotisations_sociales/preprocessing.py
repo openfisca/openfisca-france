b3e3fcc39f66a4d3710958f7dd436e65b33241fc
@ChloeLallemand <chloe.lallemand@ipp.eu>
2021-08-02 10:31:16 +0200

    Corrige le preprocessing

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
@@ -43,7 +43,11 @@ def build_pat(node_json):  # Ici node_json c'est le dossier 'parameters'
     commun.children.update(regime_general.mmid.bareme.employeur.children)  # À harmoniser ! + Créer params depuis IPP
     commun.children.update(autres.fnal.children)  # À harmoniser !
     commun.children.update(autres.fin_syndic.children)  # À harmoniser !
-    commun.children.update(retraites.employeur.children)
+    # commun.children.update(retraites.employeur.children)
+    commun.children.update(retraites.ceg.employeur.children)
+    commun.children.update(retraites.cet2019.employeur.children)
+    commun.children.update(retraites.agirc_arcco.employeur.children)
+
 
     # Réindexation NonCadre
     # Initialisation
@@ -148,7 +152,10 @@ def build_sal(node_json):
     commun.children.update(regime_general.mmid.bareme.salarie.children)  # harmoniser !
     commun.children.update(regime_general.mmid_am.bareme.children)  # À harmoniser ! + Créer params depuis IPP
     commun.children.update(regime_general.cnav.bareme.salarie.children)  # À harmoniser !
-    commun.children.update(retraites.salarie.children)
+    # commun.children.update(retraites.salarie.children)
+    commun.children.update(retraites.ceg.salarie.children)
+    commun.children.update(retraites.cet2019.salarie.children)
+    commun.children.update(retraites.agirc_arcco.salarie.children)
 
 
     # Non Cadre
