0ba1733bb2ae3d440133298d64612488c1ef73a2
DorineLam <46896006+DorineLam@users.noreply.github.com>
2021-08-04 10:41:16 +0200

    Ajoute ligne de code pour importer les variables set_input dans le fichier

--- openfisca_france/model/prestations/jeunes/mobili_jeune.py
+++ openfisca_france/model/prestations/jeunes/mobili_jeune.py
@@ -1,7 +1,7 @@
 from openfisca_france.model.base import (
     Variable, Individu, MONTH,
-    TypesCategorieSalarie, TypesSecteurActivite, TypesStatutOccupationLogement,
-    min_, where,
+    TypesCategorieSalarie, TypesSecteurActivite, TypesStatutOccupationLogement, 
+    min_, where, set_input_dispatch_by_period, set_input_divide_by_period
     )
 
 
