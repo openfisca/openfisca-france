338f4b8afeea1f3f6639cd9a8e9a5d8bad0a7bcb
DorineLam <46896006+DorineLam@users.noreply.github.com>
2021-08-04 10:35:09 +0200

    Ajoute ligne de code pour importer les variables set_input dans le fichier

--- openfisca_france/model/prestations/enseignement_superieur/bourse_criteres_sociaux.py
+++ openfisca_france/model/prestations/enseignement_superieur/bourse_criteres_sociaux.py
@@ -1,4 +1,4 @@
-from openfisca_core.model_api import not_, select, where, Variable, MONTH, set_input_divide_by_period
+from openfisca_core.model_api import not_, select, where, Variable, MONTH, set_input_divide_by_period, set_input_dispatch_by_period
 from openfisca_france.model.base import Famille, Individu, TypesStatutMarital
 from openfisca_france.model.prestations.education import TypesScolarite, StatutsEtablissementScolaire
 
