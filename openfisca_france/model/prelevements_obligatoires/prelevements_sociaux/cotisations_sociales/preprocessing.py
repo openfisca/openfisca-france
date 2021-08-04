d49ea25912a9452b2acfcc431dd51bf83c8efb75
Sasha-Laniece <sasha.laniece@gmail.com>
2021-08-04 10:33:45 +0200

    Harmonisation de cotisations_securite_sociale_regime_general.assiette.cnav

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
@@ -44,7 +44,7 @@ def build_pat(node_json):  # Ici node_json c'est le dossier 'parameters'
     commun.children.update(regime_general.csa.bareme.children)  # À harmoniser !
     commun.children.update(regime_general.famille.bareme.children)
     commun.children.update(regime_general.penibilite.bareme.children)  # À harmoniser !
-    commun.children.update(regime_general.cnav.bareme.employeur.children)  # À harmoniser !
+    commun.children.update(regime_general.cnav.employeur.children)
     commun.children.update(regime_general.mmid.bareme.employeur.children)  # À harmoniser ! + Créer params depuis IPP
     commun.children.update(autres.fnal.children)  # À harmoniser !
     commun.children['fnal_cont_moins_de_20_salaries'] = autres.fnal.children['contribution_moins_de_20_salaries']
@@ -174,7 +174,8 @@ def build_sal(node_json):
     commun.children.update(chomage.asf.salarie.children)
     commun.children.update(regime_general.mmid.bareme.salarie.children)  # harmoniser !
     commun.children.update(regime_general.mmid_am.bareme.children)  # À harmoniser ! + Créer params depuis IPP
-    commun.children.update(regime_general.cnav.bareme.salarie.children)  # À harmoniser !
+    commun.children.update(regime_general.cnav.salarie.children)
+    #commun.children.update(regime_general.cnav.bareme.salarie.children)  # À harmoniser !
     commun.children.update(retraites.ceg.salarie.children)
     commun.children.update(retraites.cet2019.salarie.children)
     commun.children.update(retraites.agirc_arrco.salarie.children)
