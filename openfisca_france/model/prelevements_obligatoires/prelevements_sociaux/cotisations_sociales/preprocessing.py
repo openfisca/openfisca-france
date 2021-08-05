d90cbd23c6954238e65aca30b49146181006671f
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2021-08-05 17:28:24 +0200

    First test of preprocessing

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
@@ -281,8 +281,8 @@ def preprocess_parameters(parameters):
     cotsoc.add_child('sal', sal)
 
     # Modifs
-    cotsoc.children["cotisations_employeur"] = ParameterNode('cotisations_employeur_after_preprocessing', data = {})
-    cotsoc.children["cotisations_salarie"] = ParameterNode('cotisations_salarie_after_preprocessing', data = {})
+    cotsoc.add_child("cotisations_employeur", ParameterNode('cotisations_employeur_after_preprocessing', data = {}))
+    cotsoc.add_child("cotisations_salarie", ParameterNode('cotisations_salarie_after_preprocessing', data = {}))
 
     for cotisation_name, baremes in (
             ('cotisations_employeur', pat.children),
