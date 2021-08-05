8d28afb9b73dd5baa0faa7e1d7657b59a8438fe8
Sophie Cottet <Chtoune@sophies-macbook-air.home>
2021-08-05 11:58:38 +0200

    Update setup.py # Conflicts: #	setup.py
    # Conflicts:
    #	setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "66.0.2",
+    version = "65.0.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
