af9e0601e57df05118b94d0af917d27ea16f4c09
HAEKADI <hajar.aitelkadi.o@gmail.com>
2021-08-09 23:16:56 +0200

    Add test para

---
+++ .github/split-tests.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+set -m
+
+dir=($(ls -d tests/**/*.{yaml,yml}))
+final_list=()
+len_dir=${#dir[@]}
+
+for i in $(seq 1 $len_dir); do
+  index_file=$(($i % ${CI_NODE_TOTAL}))
+  if [[ $index_file -eq ${CI_NODE_INDEX} ]]
+  then
+    final_list+=" ${dir[$i]}"
+  fi
+done
+
+openfisca test $final_list
+
+
+
+
+
+
+
+
+
+
