3cad4ad4ad017e878b2ed13a8c0ecde1c8d9bb47
bchoho <bilal.choho@gmail.com>
2021-08-10 14:25:46 +0200

    corrected mistakes, added links, generalized test

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -7,7 +7,8 @@ from .cache import tax_benefit_system
 cotisations_salarie_by_name = {
     "agff": {
         "start_non_null_date": "2001-04-01",
-        "final_null_date": "2019-01-01",
+        #"final_null_date": "2019-01-01", #TODO: update parameter from bareme IPP
+        #https://framagit.org/french-tax-and-benefit-tables/baremes-ipp-yaml/-/blob/master/parameters/prelevements_sociaux/regimes_complementaires_retraite_secteur_prive/agff.yaml
     },
     "agirc_arrco": {
         "start_non_null_date": "2019-01-01",
@@ -21,7 +22,8 @@ cotisations_salarie_by_name = {
     },
     "arrco": {
         "start_non_null_date": "1962-01-01",
-        "final_null_date": "2019-01-01",
+        #"final_null_date": "2019-01-01", #TODO: update parameter from bareme IPP
+        #https://framagit.org/french-tax-and-benefit-tables/baremes-ipp-yaml/-/blob/master/parameters/prelevements_sociaux/regimes_complementaires_retraite_secteur_prive/arrco.yaml
     },
     "assedic": {}, #TODO: add dates
     "ceg": {
@@ -35,13 +37,17 @@ cotisations_salarie_by_name = {
         "start_non_null_date": "2019-01-01",
     },
     "cnracl1": {
-        "start_non_null_date": "1947-09-19",
+        "start_non_null_date": "1984-01-01",
+        #"start_non_null_date": "1947-09-19", #TODO: update parameter from bareme IPP
+        #https://framagit.org/french-tax-and-benefit-tables/baremes-ipp-yaml/-/blob/master/parameters/prelevements_sociaux/cotisations_secteur_public/cnracl.yaml
     },
     "cnracl2": {
-        "start_non_null_date": "1947-09-19",
+        "start_non_null_date": "1984-01-01",
+        #"start_non_null_date": "1947-09-19", #TODO: update parameter from bareme IPP
+        #https://framagit.org/french-tax-and-benefit-tables/baremes-ipp-yaml/-/blob/master/parameters/prelevements_sociaux/cotisations_secteur_public/cnracl.yaml
     },
     "excep_solidarite": {}, #TODO: add dates
-    "forfait_annuel": {}, #TODO: vérifier barème IPP: 2011 0 ou null? Start date?
+    "forfait_annuel": {}, #TODO: add dates; 2011 0 or null?
     "ircantec": {
         "start_non_null_date": "1971-01-01",
     },
@@ -49,7 +55,9 @@ cotisations_salarie_by_name = {
         "start_non_null_date": "1989-09-01",
     },
     "maladie": {
-        "start_non_null_date": "1993-07-01", #TODO: barème IPP commence en 1967
+        "start_non_null_date": "1993-07-01",
+        #"start_non_null_date": "1967-10-01", #TODO: update parameter from bareme IPP
+        #https://framagit.org/french-tax-and-benefit-tables/baremes-ipp-yaml/-/blob/master/parameters/prelevements_sociaux/cotisations_securite_sociale_ss_regime_general/mmid.yaml
         "final_null_date": "2018-01-01",
     },
     "pension": {}, #TODO: add dates
@@ -60,7 +68,9 @@ cotisations_salarie_by_name = {
         "start_non_null_date": "2004-07-01"
     },
     "vieillesse": {
-        "start_non_null_date": "1993-07-01", #TODO: barème IPP commence en 1967
+        "start_non_null_date": "1993-07-01",
+        #"start_non_null_date": "1967-10-01", #TODO: update parameter from bareme IPP
+        #https://framagit.org/french-tax-and-benefit-tables/baremes-ipp-yaml/-/blob/master/parameters/prelevements_sociaux/cotisations_securite_sociale_ss_regime_general/cnav.yaml
     },
 }
 
@@ -124,7 +134,6 @@ cotisations_salarie_by_categorie_salarie = {
         ],
     }
 
-
 def test_preprocessing():
     """Tests the result of parameters preprocessing."""
     parameters = tax_benefit_system.parameters
@@ -151,55 +160,57 @@ def test_preprocessing():
     categorie_salaries = [
         "prive_cadre",
         "prive_non_cadre",
+        "public_non_titulaire",
+        "public_titulaire_etat",
+        "public_titulaire_hospitaliere",
+        "public_titulaire_territoriale",
         ]
+
     for categorie_salarie in categorie_salaries:
         test = parameters.cotsoc.cotisations_salarie.children[categorie_salarie].children.keys()
         target = cotisations_salarie_by_categorie_salarie[categorie_salarie]
         assert set(test) == set(target), "Les barèmes de cotisations salarié {} ne sont pas les bons".format(
             categorie_salarie)
 
-    cotisations_salaries = set(sum(
-        (cotisations_salarie_by_categorie_salarie[categorie_salarie] for categorie_salarie in categorie_salaries),
-        [],
-        ))
-
-    for cotisation_salarie in sorted(cotisations_salaries):
-        bareme = parameters.cotsoc.cotisations_salarie.children["prive_cadre"].children[cotisation_salarie]
-        
-        final_null_date = cotisations_salarie_by_name[cotisation_salarie].get("final_null_date")
-        if final_null_date:
-            thresholds = [
-                dict(
-                    (parameter_at_instant.instant_str, parameter_at_instant.value)
-                    for parameter_at_instant in bracket.threshold.values_list
+    for categorie_salarie in categorie_salaries:
+        cotisations_salaries = set(cotisations_salarie_by_categorie_salarie[categorie_salarie])
+        for cotisation_salarie in sorted(cotisations_salaries):
+            bareme = parameters.cotsoc.cotisations_salarie.children[categorie_salarie].children[cotisation_salarie]
+            
+            final_null_date = cotisations_salarie_by_name[cotisation_salarie].get("final_null_date")
+            if final_null_date:
+                thresholds = [
+                    dict(
+                        (parameter_at_instant.instant_str, parameter_at_instant.value)
+                        for parameter_at_instant in bracket.threshold.values_list
+                        )
+                    for bracket in bareme.brackets
+                    ]
+                final_thresholds_by_instant_str = OrderedDict(sorted(threshold.items(), reverse = True)[0] for threshold in thresholds)
+                assert all([final_threshold is None for final_threshold in final_thresholds_by_instant_str.values()]), "Barème salarié {} ne s'éteint pas (il devrait en {})".format(
+                    cotisation_salarie,
+                    final_null_date,
                     )
-                for bracket in bareme.brackets
-                ]
-            final_thresholds_by_instant_str = OrderedDict(sorted(threshold.items(), reverse = True)[0] for threshold in thresholds)
-            assert all([final_threshold is None for final_threshold in final_thresholds_by_instant_str.values()]), "Barème salarié {} ne s'éteint pas (il devrait en {})".format(
-                cotisation_salarie,
-                final_null_date,
-                )
-            assert max(final_thresholds_by_instant_str.keys()) == final_null_date, "Barème salarié {} ne s'éteint pas en {}".format(
-                cotisation_salarie,
-                final_null_date,
-                )
-
-        start_non_null_date = cotisations_salarie_by_name[cotisation_salarie].get("start_non_null_date")
-        if start_non_null_date:
-            thresholds = [
-                dict(
-                    (parameter_at_instant.instant_str, parameter_at_instant.value)
-                    for parameter_at_instant in bracket.threshold.values_list
+                assert max(final_thresholds_by_instant_str.keys()) == final_null_date, "Barème salarié {} ne s'éteint pas en {}".format(
+                    cotisation_salarie,
+                    final_null_date,
                     )
-                for bracket in bareme.brackets
-                ]
-            start_thresholds_by_instant_str = OrderedDict(sorted(threshold.items())[0] for threshold in thresholds)
-            assert all([start_threshold is not None for start_threshold in start_thresholds_by_instant_str.values()]), "Barème salarié {} ne commence pas à la bonne date (il devrait en {})".format(
-                cotisation_salarie,
-                start_non_null_date,
-                )
-            assert min(start_thresholds_by_instant_str.keys()) == start_non_null_date, "Barème salarié {} ne commence pas en {}".format(
-                cotisation_salarie,
-                start_non_null_date,
-                )
+
+            start_non_null_date = cotisations_salarie_by_name[cotisation_salarie].get("start_non_null_date")
+            if start_non_null_date:
+                thresholds = [
+                    dict(
+                        (parameter_at_instant.instant_str, parameter_at_instant.value)
+                        for parameter_at_instant in bracket.threshold.values_list
+                        )
+                    for bracket in bareme.brackets
+                    ]
+                start_thresholds_by_instant_str = OrderedDict(sorted(threshold.items())[0] for threshold in thresholds)
+                assert all([start_threshold is not None for start_threshold in start_thresholds_by_instant_str.values()]), "Barème salarié {} ne commence pas à la bonne date (il devrait en {})".format(
+                    cotisation_salarie,
+                    start_non_null_date,
+                    )
+                assert min(start_thresholds_by_instant_str.keys()) == start_non_null_date, "Barème salarié {} ne commence pas en {}".format(
+                    cotisation_salarie,
+                    start_non_null_date,
+                    )
