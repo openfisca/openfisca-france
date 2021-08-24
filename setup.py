3e939a53cc1cff92d7dac2db5ac79e007c55996c
kout95 <antoine.bernier@outlook.fr>
2021-08-24 12:23:50 +0200

    Monte la version mineur

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "71.1.0",
+    version = "71.15247425.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
