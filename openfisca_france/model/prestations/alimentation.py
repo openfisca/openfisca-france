2ece5153a01eefb6ec6197f4ed0ceb0f67b36486
Thomas Guillet <thomas@palya.fr>
2021-09-06 17:26:29 +0200

    Met à jour la référence

--- openfisca_france/model/prestations/alimentation.py
+++ openfisca_france/model/prestations/alimentation.py
@@ -8,7 +8,7 @@ class crous_repas_un_euro_eligibilite(Variable):
     entity = Individu
     definition_period = MONTH
     set_input = set_input_dispatch_by_period
-    reference = "https://www.etudiant.gouv.fr/fr/le-repas-au-crous-passe-1-euro-pour-tous-les-etudiants-2314"
+    reference = "https://www.etudiant.gouv.fr/sites/default/files/2021-01/le-repas-un-euro-4977.pdf"
     documentation = '''
     Suite à la crise Covid-19, tous les étudiants, boursiers ou non, peuvent bénéficier
     de deux repas par jour au tarif de 1 euro.
