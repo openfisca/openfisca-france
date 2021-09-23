607cbe044bf7a1e99119a5faf3776d0bb5da65e6
HAEKADI <hajar.aitelkadi.o@gmail.com>
2021-09-23 12:37:24 +0200

    Traite les exit codes dans le script python

--- .github/check-api-version.py
+++ .github/check-api-version.py
@@ -10,4 +10,7 @@ response_API = requests.get('https://fr.openfisca.org/api/latest/spec')
 data = json.loads(response_API.text)
 api_package_version = data['info']['version']
 
-sys.stdout.write(str(api_package_version == package_version))
+sys.stdout.write("API package version: {}\nLocal package version: {}\n".format(api_package_version, package_version))
+
+if api_package_version != package_version:
+    sys.exit("The version of the API deployed on fr.openfisca.org/api/latest/spec does not match the local package version.")
