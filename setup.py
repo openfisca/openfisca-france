573145af00ade66fa809288b81629261c9e15e42
kout95 <antoine.bernier@outlook.fr>
2021-09-27 09:50:14 +0200

    Met à jour le changelog et la version du fichier setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "75.0.0",
+    version = "75.1.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
