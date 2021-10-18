556eb7e38df0f1e2d234db74065f7e1854e3bb8e
FAVROT Tom <92153911+TomFavrotSopraSteria@users.noreply.github.com>
2021-10-18 11:34:21 +0200

    Création de l'éligibilité de la catégorie d'un demandeur d'emploi à l'AGEPI

--- openfisca_france/model/prestations/agepi.py
+++ openfisca_france/model/prestations/agepi.py
@@ -3,7 +3,7 @@ from openfisca_france.model.base import Famille, Individu, Variable, MONTH, ADD,
 
 
 class pe_nbenf(Variable):
-    value_type = float
+    value_type = int
     entity = Famille
     definition_period = MONTH
     set_input = set_input_dispatch_by_period
@@ -29,10 +29,12 @@ class agepi_temps_travail_semaine(Variable):
         heures_remunerees_volume = individu('heures_remunerees_volume', period)
         return heures_remunerees_volume / 52 * 12  # Passage en heures par semaine
 
-class TypesCategorie(Enum):
-    __order__ = 'categorie_1 categorie_2 categorie_3 categorie_4 categorie_5 categorie_6 categorie_7 categorie_8 ' \
-                'pas_de_categorie'  # Needed to preserve the enum order in Python 2
-    categorie_1 = "1"
+
+class TypesCategoriesDemandeurEmploi(Enum):
+    __order__ = 'pas_de_categorie categorie_1 categorie_2 categorie_3 categorie_4 categorie_5 categorie_6 categorie_7 categorie_8 ' \
+                # Needed to preserve the enum order in Python 2
+    pas_de_categorie = "NA"
+    categorie_1 = "Catégorie 1 - Personnes sans emploi immédiatement disponibles en recherche de CDI plein temps"
     categorie_2 = "2"
     categorie_3 = "3"
     categorie_4 = "4"
@@ -40,20 +42,19 @@ class TypesCategorie(Enum):
     categorie_6 = "6"
     categorie_7 = "7"
     categorie_8 = "8"
-    pas_de_categorie = "NA"
 
-class pe_categorie_demandeur_emploi(Variable):
+
+class pole_emploi_categorie_demandeur_emploi(Variable):
     reference = [
         "http://www.bo-pole-emploi.org/bulletinsofficiels/instruction-n2016-33-du-6-octobr.html?type=dossiers/2016/bope-n2016-80-du-17-novembre-201#",
-        "Annexes"
-        ]
+        "Annexe 3 : la fiche 3 - Les effets de l’inscription"
+    ]
     value_type = Enum
-    possible_values = TypesCategorie
-    default_value = TypesCategorie.pas_de_categorie
+    possible_values = TypesCategoriesDemandeurEmploi
+    default_value = TypesCategoriesDemandeurEmploi.pas_de_categorie
     entity = Individu
-    label = ""  # TODO
+    label = "Le classement des demandeurs d’emploi dans les différentes catégories d’inscription à Pôle Emploi"
     definition_period = MONTH
-    set_input = set_input_dispatch_by_period  # FIXME TODO
 
 
 class agepi_eligible(Variable):
@@ -69,31 +70,41 @@ class agepi_eligible(Variable):
 
     def formula(famille, period, parameters):
         # Renvoi true si :
-        #   - l'individu n'a pas touché l'AGEPI dans les 12 derniers mois,
+        #   - l'individu n'a pas touché l'AGEPI dans les 12 derniers mois
         #   - Est inscrit en catégorie 1, 2, 3, 4 ou 5
+        #   - Sa demande s'effectue au plus tard dans le mois qui suit sa reprise d'emploi ou de formation
         #   - Elève seul son enfant dont l'age est inférieur à 10 ans
         #   - Est non indemnisé ou que son ARE est inférieure ou égale à l'ARE minimale
         #   - Est en reprise d'emploi du type CDI, CDD, CTT d'au moins 3 mois consécutifs
         #       - Ou en processur d'entrée en formation supérieure ou égale à 40 heures
 
-        annee_glissante = period.start.period('year').offset(-1).offset(-1, 'month')
-        pe_categorie_demandeur_emploi = famille.members('pe_categorie_demandeur_emploi', period)#  Famille avec 0 parents? FIXME
-        categorie_eligible = pe_categorie_demandeur_emploi in [
-            TypesCategorie.categorie_1,
-            TypesCategorie.categorie_2,
-            TypesCategorie.categorie_3,
-            TypesCategorie.categorie_4,
-            TypesCategorie.categorie_5
-        ]
+        #  L'individu n'a pas touché l'AGEPI dans les 12 derniers mois
 
+        annee_glissante = period.start.period('year').offset(-1).offset(-1, 'month')
         agepi_non_percue = famille('agepi', annee_glissante, options=[ADD]) == 0
 
-        #ARE_max = famille('allocation_retour_emploi', period) <= parameters.montant_minimum
-        #print(ARE_max)
+        #  L'individu est inscrit en catégorie 1, 2, 3, 4 ou 5
 
-        #return agepi_non_percue * pe_categorie_demandeur_emploi * ARE_max != 0
-        print(categorie_eligible)
-        return agepi_non_percue and categorie_eligible
+        pe_categorie_demandeur_emploi = famille.members('pole_emploi_categorie_demandeur_emploi', period)
+
+        print("pe_categorie_demandeur_emploi = " + str(pe_categorie_demandeur_emploi))
+        categories_eligibles = ((pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_1) +
+                                (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_2) +
+                                (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_3) +
+                                (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_4) +
+                                (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_5))
+
+        # ARE_max = famille('allocation_retour_emploi', period) <= parameters.montant_minimum
+        # print(ARE_max)
+
+        # return agepi_non_percue * pe_categorie_demandeur_emploi * ARE_max != 0
+        
+        # print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+        # print("categorie_eligible = " + str(categories_eligibles))
+        # print("agepi_non_percue = " + agepi_non_percue)
+
+        return agepi_non_percue * categories_eligibles != 0
+
 
 class agepi(Variable):
     value_type = float
@@ -119,19 +130,3 @@ class agepi(Variable):
         montant = (nb_heures < 15) * montant_moins_de_15h + (nb_heures >= 15) * montant_plus_de_15h
 
         return agepi_eligible * isole * (nb_heures > 0) * montant
-
-
-class pe_categorie_demandeur_emploi(Variable):
-    value_type = bool
-    entity = Individu
-    definition_period = MONTH
-    label = "Catégorie du demandeur d'emploi"
-    reference = [
-        "Annexe 3 : la fiche 3 - Les effets de l’inscription - 1. Catégories administratives"
-        "http://www.bo-pole-emploi.org/bulletinsofficiels/instruction-n2016-33-du-6-octobr.html?type=dossiers/2016/bope-n2016-80-du-17-novembre-201#"
-    ]
-
-    def formula(individu, period, parameters):
-        # Renvoi true si l'individu appartient à une catégorie et par conséquent inscrit comme demandeur d'emploi chez Pôle Emploi
-        agepi = individu('agepi', period)
-        return agepi != 0
