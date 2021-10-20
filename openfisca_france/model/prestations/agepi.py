471e38be783c71f75de49eed0eadf0859653f536
FAVROT Tom <92153911+TomFavrotSopraSteria@users.noreply.github.com>
2021-10-20 15:47:49 +0200

    Conditions d'égibilité ajoutées : parent isolé + montant ARE maximum

--- openfisca_france/model/prestations/agepi.py
+++ openfisca_france/model/prestations/agepi.py
@@ -1,3 +1,5 @@
+from openfisca_core.periods import Period, Instant, instant
+
 from openfisca_france.model.base import Famille, Individu, Variable, MONTH, ADD, set_input_dispatch_by_period, \
     set_input_divide_by_period, Enum
 
@@ -34,14 +36,14 @@ class TypesCategoriesDemandeurEmploi(Enum):
     __order__ = 'pas_de_categorie categorie_1 categorie_2 categorie_3 categorie_4 categorie_5 categorie_6 categorie_7 categorie_8 ' \
                 # Needed to preserve the enum order in Python 2
     pas_de_categorie = "NA"
-    categorie_1 = "Catégorie 1 - Personnes sans emploi, immédiatement disponibles en recherche de CDI plein temps"
-    categorie_2 = "Catégorie 2 - Personnes sans emploi, immédiatement disponibles en recherche de CDI à temps partiel"
-    categorie_3 = "Catégorie 3 - Personnes sans emploi, immédiatement disponibles en recherche de CDD"
+    categorie_1 = "Catégorie 1 - Personnes sans emploi, immédiatement disponibles en recherche de CDI plein temps."
+    categorie_2 = "Catégorie 2 - Personnes sans emploi, immédiatement disponibles en recherche de CDI à temps partiel."
+    categorie_3 = "Catégorie 3 - Personnes sans emploi, immédiatement disponibles en recherche de CDD."
     categorie_4 = "Catégorie 4 - Personnes sans emploi, non immédiatement disponibles et à la recherche d’un emploi."
     categorie_5 = "Catégorie 5 - Personnes non immédiatement disponibles, parce que titulaires d'un ou de plusieurs emplois, et à la recherche d'un autre emploi."
-    categorie_6 = "Catégorie 6 - Personnes non immédiatement disponibles, en recherche d'un autre emploi en CDI à plein temps"
-    categorie_7 = "Catégorie 7 - Personnes non immédiatement disponibles, en recherche d'un autre emploi en CDI à temps partiel"
-    categorie_8 = "Catégorie 8 - Personnes non immédiatement disponibles, en recherche d'un autre emploi en CDD"
+    categorie_6 = "Catégorie 6 - Personnes non immédiatement disponibles, en recherche d'un autre emploi en CDI à plein temps."
+    categorie_7 = "Catégorie 7 - Personnes non immédiatement disponibles, en recherche d'un autre emploi en CDI à temps partiel."
+    categorie_8 = "Catégorie 8 - Personnes non immédiatement disponibles, en recherche d'un autre emploi en CDD."
 
 
 class pole_emploi_categorie_demandeur_emploi(Variable):
@@ -64,46 +66,75 @@ class agepi_eligible(Variable):
     definition_period = MONTH
     # set_input = set_input_divide_by_period
     reference = [
-        "Article 4 de la délibération n°2013-46 du 18 décembre 2013 du Pôle Emploi"
+        "Article 4 de la délibération n°2013-46 du 18 décembre 2013 du Pôle Emploi",
         "http://www.bo-pole-emploi.org/bulletinsofficiels/deliberation-n2013-46-du-18-dece.html?type=dossiers/2013/bope-n2013-128-du-24-decembre-20"
     ]
 
+
     def formula(famille, period, parameters):
         # Renvoi true si :
-        #   - l'individu n'a pas touché l'AGEPI dans les 12 derniers mois
-        #   - Est inscrit en catégorie 1, 2, 3, 4 ou 5
-        #   - Sa demande s'effectue au plus tard dans le mois qui suit sa reprise d'emploi ou de formation
-        #   - Elève seul son enfant dont l'age est inférieur à 10 ans
-        #   - Est non indemnisé ou que son ARE est inférieure ou égale à l'ARE minimale
-        #   - Est en reprise d'emploi du type CDI, CDD, CTT d'au moins 3 mois consécutifs
+        #   1- L'individu élève seul son enfant dont l'age est inférieur à 10 ans
+        #   2- L'individu n'a pas touché l'AGEPI dans les 12 derniers mois
+        #   3- L'individu est inscrit en catégorie 1, 2, 3, 4 ou 5
+        #   4- L'individu effectue sa demande au plus tard dans le mois qui suit sa reprise d'emploi ou de formation
+        #   5- L'individu est non indemnisé ou que son ARE est inférieure ou égale à l'ARE minimale
+        #   6- L'individu est en reprise d'emploi du type CDI, CDD, CTT d'au moins 3 mois consécutifs
         #       - Ou en processur d'entrée en formation supérieure ou égale à 40 heures
 
-        #  L'individu n'a pas touché l'AGEPI dans les 12 derniers mois
+        #  1- L'individu élève seul son enfant dont l'age est inférieur à 10 ans
+
+        parent_isole = famille('nb_parents', period) == 1
+        #  TODO enfants_eligibles
+
+        #  2- L'individu n'a pas touché l'AGEPI dans les 12 derniers mois
 
         annee_glissante = period.start.period('year').offset(-1).offset(-1, 'month')
         agepi_non_percue = famille('agepi', annee_glissante, options=[ADD]) == 0
 
-        #  L'individu est inscrit en catégorie 1, 2, 3, 4 ou 5
+        #  3- L'individu est inscrit en catégorie 1, 2, 3, 4 ou 5
 
         pe_categorie_demandeur_emploi = famille.members('pole_emploi_categorie_demandeur_emploi', period)
 
-        print("pe_categorie_demandeur_emploi = " + str(pe_categorie_demandeur_emploi))
+        # print("pe_categorie_demandeur_emploi = " + str(pe_categorie_demandeur_emploi))
         categories_eligibles = ((pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_1) +
                                 (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_2) +
                                 (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_3) +
                                 (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_4) +
                                 (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_5))
 
-        # ARE_max = famille('allocation_retour_emploi', period) <= parameters.montant_minimum
-        # print(ARE_max)
+        #  4- L'individu effectue sa demande au plus tard dans le mois qui suit sa reprise d'emploi ou de formation
 
-        # return agepi_non_percue * pe_categorie_demandeur_emploi * ARE_max != 0
+        # date_demande_agepi = str(period.date)
+        # print(f"date_demande_agepi: {date_demande_agepi}")
+        #
+        # test_date_plus_1_mois = str((period.offset(1, 'month')).date)
+        # print(f"test_date_plus_1_mois: {test_date_plus_1_mois}")
+        #
+        # date_debut_contrat_de_travail_plus_un_mois = famille.members('contrat_de_travail_debut', period.offset(1, 'month'))
+        #
+        # print(f"contrat_de_travail_debut: {famille.members('contrat_de_travail_debut', period)}")
+        # print(f"date_debut_contrat_de_travail_plus_un_mois: {date_debut_contrat_de_travail_plus_un_mois}")
 
-        # print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
-        # print("categorie_eligible = " + str(categories_eligibles))
-        # print("agepi_non_percue = " + agepi_non_percue)
+        #date_demande_agepi_eligible = (date_demande_agepi <= date_debut_contrat_de_travail_plus_un_mois)
 
-        return agepi_non_percue * categories_eligibles != 0
+        #  5- L'individu est non indemnisé ou que son ARE est inférieure ou égale à l'ARE minimale
+
+        allocation_individu = famille.members('allocation_retour_emploi', period)
+        print(f"alloc_user: {allocation_individu}")
+
+        allocation_minimale = parameters(period).allocation_retour_emploi.montant_minimum
+        print(f"alloc_mini: {allocation_minimale}")
+
+        montant_ARE_eligible = allocation_individu <= allocation_minimale
+        print(f"montant_ARE_eligible: {montant_ARE_eligible}")
+
+        #   TODO 6- L'individu est en reprise d'emploi du type CDI, CDD, CTT d'au moins 3 mois consécutifs
+        #       - Ou en processur d'entrée en formation supérieure ou égale à 40 heures
+        #
+
+        date_demande_agepi_eligible = True
+
+        return parent_isole * agepi_non_percue * categories_eligibles * date_demande_agepi_eligible * montant_ARE_eligible != 0
 
 
 class agepi(Variable):
@@ -118,7 +149,6 @@ class agepi(Variable):
     ]
 
     def formula(famille, period, parameters):
-        isole = famille('nb_parents', period) == 1
         nb_heures = famille.sum(famille.members('agepi_temps_travail_semaine', period), role=Famille.PARENT)
         nbenf = famille('pe_nbenf', period)
         agepi_eligible = famille('agepi_eligible', period)
@@ -129,4 +159,4 @@ class agepi(Variable):
 
         montant = (nb_heures < 15) * montant_moins_de_15h + (nb_heures >= 15) * montant_plus_de_15h
 
-        return agepi_eligible * isole * (nb_heures > 0) * montant
+        return agepi_eligible * (nb_heures > 0) * montant
