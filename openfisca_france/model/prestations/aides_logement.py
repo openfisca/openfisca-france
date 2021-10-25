1ef860f3da0604da0e5c14e77f9713f2c0f8740f
TOMAS Frederic <frederic.tomas@soprasteria.com>
2021-10-25 10:11:03 +0200

    Supprime les abattements superflus pour les stagiaires et apprentis

--- openfisca_france/model/prestations/aides_logement.py
+++ openfisca_france/model/prestations/aides_logement.py
@@ -542,13 +542,10 @@ class al_revenu_assimile_salaire(Variable):
         f1tt = individu('f1tt', period_f1tt_f3vj)
         f3vj = individu('f3vj', period_f1tt_f3vj)
 
-        # Application du plafond d'exoneration fiscal pour les salaires des stagiaires et des apprentis
         remuneration_apprenti = individu('remuneration_apprenti', period_salaire_chomage, options=[ADD])
-        remuneration_apprenti_apres_abattement = max(0, sum(remuneration_apprenti) - smic_annuel_brut)
         indemnites_stage = individu('indemnites_stage', period_salaire_chomage, options=[ADD])
-        indemnites_stage_apres_abattement = max(0, sum(indemnites_stage) - smic_annuel_brut)
 
-        return salaire_imposable + chomage_imposable + f1tt + f3vj + remuneration_apprenti_apres_abattement + indemnites_stage_apres_abattement
+        return salaire_imposable + chomage_imposable + f1tt + f3vj + remuneration_apprenti + indemnites_stage
 
 
 class al_biactivite(Variable):
