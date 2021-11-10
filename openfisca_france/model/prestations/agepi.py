1b8dba2a774c8b1e34a8ceef8a8a88f7e4b9e056
FAVROT Tom <92153911+TomFavrotSopraSteria@users.noreply.github.com>
2021-11-10 12:21:07 +0100

    Filtre catégories 4 et 5 pour ne sélectionner que les stagiaires de la formation professionnelle et les contrats aidés

--- openfisca_france/model/prestations/agepi.py
+++ openfisca_france/model/prestations/agepi.py
@@ -18,7 +18,9 @@ class pe_nbenf(Variable):
 
     def formula(famille, period):
         age_enfant_eligible = famille('agepi_eligible', period)
+        print("age_enfant_eligible", age_enfant_eligible)
         nb_enfants_eligibles = famille.sum(age_enfant_eligible, role=Famille.ENFANT)
+        print("nb_enfants_eligibles", nb_enfants_eligibles)
         return nb_enfants_eligibles
 
 
@@ -51,14 +53,14 @@ class emploi_ou_formation_en_france(Variable):
 
 
 class TypesCategoriesDemandeurEmploi(Enum):
-    __order__ = 'pas_de_categorie categorie_1 categorie_2 categorie_3 categorie_4_stagiaire_formation_professionnelle categorie_5_contrat_aide categorie_6 categorie_7 categorie_8' \
+    __order__ = 'pas_de_categorie categorie_1 categorie_2 categorie_3 categorie_4 categorie_5 categorie_6 categorie_7 categorie_8' \
                 # Needed to preserve the enum order in Python 2
     pas_de_categorie = "Aucune catégorie"
     categorie_1 = "Catégorie 1 - Personnes sans emploi, immédiatement disponibles en recherche de CDI plein temps."
     categorie_2 = "Catégorie 2 - Personnes sans emploi, immédiatement disponibles en recherche de CDI à temps partiel."
     categorie_3 = "Catégorie 3 - Personnes sans emploi, immédiatement disponibles en recherche de CDD."
-    categorie_4_stagiaire_formation_professionnelle = "Catégorie 4 - stagiaire de la formation professionnelle - Personnes sans emploi, non immédiatement disponibles et à la recherche d’un emploi."
-    categorie_5_contrat_aide = "Catégorie 5 - contrat aidé - Personnes non immédiatement disponibles, parce que titulaires d'un ou de plusieurs emplois, et à la recherche d'un autre emploi."
+    categorie_4 = "Catégorie 4 - Personnes sans emploi, non immédiatement disponibles et à la recherche d’un emploi."
+    categorie_5 = "Catégorie 5 - Personnes non immédiatement disponibles, parce que titulaires d'un ou de plusieurs emplois, et à la recherche d'un autre emploi."
     categorie_6 = "Catégorie 6 - Personnes non immédiatement disponibles, en recherche d'un autre emploi en CDI à plein temps."
     categorie_7 = "Catégorie 7 - Personnes non immédiatement disponibles, en recherche d'un autre emploi en CDI à temps partiel."
     categorie_8 = "Catégorie 8 - Personnes non immédiatement disponibles, en recherche d'un autre emploi en CDD."
@@ -75,6 +77,14 @@ class pole_emploi_categorie_demandeur_emploi(Variable):
     entity = Individu
     label = "Le classement des demandeurs d’emploi dans les différentes catégories d’inscription à Pôle Emploi"
     definition_period = MONTH
+
+
+class en_contrat_aide(Variable):
+    value_type = bool
+    entity = Individu
+    label = "L'individu est en contrat aidé"
+    definition_period = MONTH
+    set_input = set_input_dispatch_by_period
 
 
 class reside_en_region_mayotte(Variable):
@@ -140,13 +150,14 @@ class agepi_eligible(Variable):
         "http://www.bo-pole-emploi.org/bulletinsofficiels/deliberation-n2013-46-du-18-dece.html?type=dossiers/2013/bope-n2013-128-du-24-decembre-20"
         ]
     documentation = '''
-        1- L'individu élève seul son enfant dont l'âge est inférieur à 10 ans (condition de garde d'enfant)
-        2- L'individu n'a pas touché l'AGEPI dans les 12 derniers mois (condition de durée entre faits générateurs)
-        3- L'individu est inscrit en catégorie 1, 2, 3, 4 "stagiaire de la formation professionnelle" ou 5 "contrat aidé"
-        4- L'emploi ou la formation se situe en France
-        5- L'individu effectue sa demande au plus tard dans le mois qui suit sa reprise d'emploi ou de formation
-        6- L'individu est non indemnisé ou son ARE est inférieure ou égale à l'ARE minimale
-        7- L'individu est en reprise d'emploi du type CDI, CDD ou CTT d'au moins 3 mois consécutifs ou en processus d'entrée en formation d'une durée supérieure ou égale à 40 heures
+        1- L'individu élève seul son enfant (parent isolé)
+        2- L'âge du ou des enfants dont il a la garde est inférieur à 10 ans (condition de garde d'enfant)
+        3- L'individu n'a pas touché l'AGEPI dans les 12 derniers mois (condition de durée entre faits générateurs)
+        4- L'individu est inscrit en catégorie 1, 2, 3, 4 "stagiaire de la formation professionnelle" ou 5 "contrat aidé"
+        5- L'emploi ou la formation se situe en France
+        6- L'individu effectue sa demande au plus tard dans le mois qui suit sa reprise d'emploi ou de formation
+        7- L'individu est non indemnisé ou son ARE est inférieure ou égale à l'ARE minimale
+        8- L'individu est en reprise d'emploi du type CDI, CDD ou CTT d'au moins 3 mois consécutifs ou en processus d'entrée en formation d'une durée supérieure ou égale à 40 heures
     '''
 
     def formula(famille, period, parameters):
@@ -158,49 +169,50 @@ class agepi_eligible(Variable):
         #  1
         parents_isoles = famille('nb_parents', period) == 1
 
-        age_enfants = famille.members('age', period)
-        age_enfants_eligible = (age_enfants < parameters(period).prestations.agepi.age_enfant_maximum) * (age_enfants > 0)
-
         #  2
-
-        agepi_non_percues = not_(famille.members('agepi_percue_12_derniers_mois', period))
+        age_membres_famille = famille.members('age', period)
+        age_enfants_eligible = (age_membres_famille < parameters(period).prestations.agepi.age_enfant_maximum) * (age_membres_famille > 0)
 
         #  3
+        agepi_non_percues = not_(famille.members('agepi_percue_12_derniers_mois', period))
 
+        #  4
         pe_categorie_demandeur_emploi = famille.members('pole_emploi_categorie_demandeur_emploi', period)
+
+        stagiaire_formation_professionnelle = famille.members('stagiaire', period)
+        contrat_aide = famille.members('en_contrat_aide', period)
+
+        categorie_4 = pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_4
+        categorie_4_stagiaire_formation_professionnelle = categorie_4 * stagiaire_formation_professionnelle
+
+        categorie_5 = pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_5
+        categorie_5_contrat_aide = categorie_5 * contrat_aide
 
         categories_eligibles = ((pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_1)
                                 + (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_2)
                                 + (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_3)
-                                + (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_4_stagiaire_formation_professionnelle)
-                                + (pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_5_contrat_aide)
                                 + (not_(pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_6))
+                                * (categorie_4_stagiaire_formation_professionnelle + categorie_5_contrat_aide)
                                 * (not_(pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_7))
                                 * (not_(pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.categorie_8))
                                 * (not_(pe_categorie_demandeur_emploi == TypesCategoriesDemandeurEmploi.pas_de_categorie)))
 
-        #  4
-
-        reprises_activites_en_france = famille.members('emploi_ou_formation_en_france', period)
-
         #  5
+        lieux_activite_eligibles = famille.members('emploi_ou_formation_en_france', period)
 
+        #  6
         contrat_de_travail_debut = famille.members('contrat_de_travail_debut', period)  # numpy.datetime64
         contrat_de_travail_debut_en_mois = contrat_de_travail_debut.astype('M8[M]')
-        print("contrat_de_travail_debut_en_mois = ", contrat_de_travail_debut_en_mois)
 
         date_limite_eligibilite_contrat = min_(
             (contrat_de_travail_debut_en_mois + 1) + (contrat_de_travail_debut - contrat_de_travail_debut_en_mois), 
             (contrat_de_travail_debut_en_mois + 2) - np.timedelta64(1, 'D')
             )
-        print("date_limite_eligibilite_contrat = ", date_limite_eligibilite_contrat)
         
-        agepi_date_demande = famille.members("agepi_date_demande", period)
-        date_demande_agepi_eligible = agepi_date_demande <= date_limite_eligibilite_contrat
-        print("date_demande_agepi_eligible", date_demande_agepi_eligible)
+        agepi_date_de_demande = famille.members("agepi_date_demande", period)
+        dates_demandes_agepi_eligibles = agepi_date_de_demande <= date_limite_eligibilite_contrat
 
-        #  6
-
+        #  7
         resident_en_mayotte = famille.members('reside_en_region_mayotte', period) == 1
         ne_resident_pas_en_mayotte = not_(resident_en_mayotte)
 
@@ -218,8 +230,7 @@ class agepi_eligible(Variable):
 
         montants_are_eligibles = are_individu_inferieure_are_min + are_individu_egale_are_min
 
-        #  7
-
+        #  8
         reprises_types_activites = famille.members('types_activite_condition', period)
 
         reprises_types_activites_aucune_activite = not_(reprises_types_activites == TypesActiviteCondition.aucune_activite)
@@ -245,14 +256,12 @@ class agepi_eligible(Variable):
             + reprises_types_activites_cdd_eligible
             + reprises_types_activites_ctt_eligible)
 
-        # date_demande_agepi_eligible = True
-
         return parents_isoles \
             * age_enfants_eligible \
             * agepi_non_percues \
-            * reprises_activites_en_france \
+            * lieux_activite_eligibles \
             * categories_eligibles \
-            * date_demande_agepi_eligible \
+            * dates_demandes_agepi_eligibles \
             * montants_are_eligibles \
             * types_et_duree_activite_eligible != 0
 
@@ -293,7 +302,6 @@ class agepi(Variable):
         montant_maximum_hors_intensite_hors_mayotte = montants_hors_mayotte.maximum.calc(nb_enfants_eligibles) * ne_resident_pas_en_mayotte
         montant_maximum_hors_intensite_mayotte = montants_mayotte.maximum.calc(nb_enfants_eligibles) * resident_en_mayotte
 
-        ################################################################################################################
 
         #  Montants en fonction du nombre d'enfants pour une reprise emploi ou formation < 15h/sem - HORS MAYOTTE
         montants_minimum_intensite_hebdo_hors_mayotte = montants_hors_mayotte.minimum.calc(nb_enfants_eligibles) * ne_resident_pas_en_mayotte * intensite_hebdomadaire
@@ -303,7 +311,6 @@ class agepi(Variable):
         montants_minimum_intensite_mensuelle_hors_mayotte = montants_hors_mayotte.minimum.calc(nb_enfants_eligibles) * ne_resident_pas_en_mayotte * intensite_mensuelle
         montants_maximum_intensite_mensuelle_hors_mayotte = montants_hors_mayotte.maximum.calc(nb_enfants_eligibles) * ne_resident_pas_en_mayotte * intensite_mensuelle
 
-        ################################################################################################################
 
         #  Montants en fonction du nombre d'enfants pour une reprise emploi ou formation < 15h/sem - MAYOTTE
         montants_minimum_intensite_hebdo_mayotte = montants_mayotte.minimum.calc(nb_enfants_eligibles) * resident_en_mayotte * intensite_hebdomadaire
@@ -312,8 +319,6 @@ class agepi(Variable):
         #  Montants en fonction du nombre d'enfants pour une reprise emploi ou formation < 64h/mensuelle - MAYOTTE
         montants_minimum_intensite_mensuelle_mayotte = montants_mayotte.minimum.calc(nb_enfants_eligibles) * resident_en_mayotte * intensite_mensuelle
         montants_maximum_intensite_mensuelle_mayotte = montants_mayotte.maximum.calc(nb_enfants_eligibles) * resident_en_mayotte * intensite_mensuelle
-
-        ################################################################################################################
 
         #  Montants minorés / majorés - Hors Mayotte - intensité hebdomadaire
         montants_minimums_hors_mayotte = montants_minimum_intensite_hebdo_hors_mayotte + montants_minimum_intensite_mensuelle_hors_mayotte
@@ -328,7 +333,6 @@ class agepi(Variable):
         montants_minimums_en_fonction_de_la_region = montants_minimums_hors_mayotte + montants_minimums_mayotte
         montants_maximums_en_fonction_de_la_region = montants_maximums_hors_mayotte + montants_maximums_mayotte
 
-        ################################################################################################################
 
         # Calcul du montant en fonction du nombre d'heures de la reprise d'emploi ou de formation
 
@@ -341,7 +345,6 @@ class agepi(Variable):
         condition_montants_minimums = condition_montants_minimums_hebdomadaire + condition_montants_minimums_mensuelle
         condition_montants_maximums = condition_montants_maximums_hebdomadaire + condition_montants_maximums_mensuelle
 
-        ################################################################################################################
 
         montant_avec_intensite = ((condition_montants_minimums * montants_minimums_en_fonction_de_la_region)
             + (condition_montants_maximums * montants_maximums_en_fonction_de_la_region)) * contrat_autre_que_cdi
