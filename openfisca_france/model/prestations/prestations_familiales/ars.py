3fc9811c23fc1640a81cd0ff68e6d5b7357d7d4a
Sasha-Laniece <sasha.laniece@gmail.com>
2021-12-23 17:23:51 +0100

    Typo ARS

--- openfisca_france/model/prestations/prestations_familiales/ars.py
+++ openfisca_france/model/prestations/prestations_familiales/ars.py
@@ -17,36 +17,37 @@ class ars(Variable):
         septembre = period.start.offset('first-of', 'year').offset(9, 'month').period('month')
         af_nbenf = famille('af_nbenf', septembre)
         base_ressources = famille('prestations_familiales_base_ressources', janvier)
-        P = parameters(septembre).prestations_sociales.prestations_familiales.education_presence_parentale
+        ars = parameters(septembre).prestations_sociales.prestations_familiales.education_presence_parentale.ars
+        af = parameters(septembre).prestations_sociales.prestations_familiales.prestations_generales.af
         # TODO: convention sur la mensualisation
         # On tient compte du fait qu'en cas de léger dépassement du plafond, une allocation dégressive
         # (appelée allocation différentielle), calculée en fonction des revenus, peut être versée.
 
-        bmaf = P.af.bmaf
+        bmaf = af.bmaf
         # On doit prendre l'âge en septembre
-        enf_05 = nb_enf(famille, septembre, P.ars.age_entree_primaire - 1, P.ars.age_entree_primaire - 1)  # 5 ans et 6 ans avant le 31 décembre
+        enf_05 = nb_enf(famille, septembre, ars.age_entree_primaire - 1, ars.age_entree_primaire - 1)  # 5 ans et 6 ans avant le 31 décembre
         # enf_05 = 0
         # Un enfant scolarisé qui n'a pas encore atteint l'âge de 6 ans
         # avant le 1er février 2012 peut donner droit à l'ARS à condition qu'il
         # soit inscrit à l'école primaire. Il faudra alors présenter un
         # certificat de scolarité.
-        enf_primaire = enf_05 + nb_enf(famille, septembre, P.ars.age_entree_primaire, P.ars.age_entree_college - 1)
-        enf_college = nb_enf(famille, septembre, P.ars.age_entree_college, P.ars.age_entree_lycee - 1)
-        enf_lycee = nb_enf(famille, septembre, P.ars.age_entree_lycee, P.ars.age_sortie_lycee)
+        enf_primaire = enf_05 + nb_enf(famille, septembre, ars.age_entree_primaire, ars.age_entree_college - 1)
+        enf_college = nb_enf(famille, septembre, ars.age_entree_college, ars.age_entree_lycee - 1)
+        enf_lycee = nb_enf(famille, septembre, ars.age_entree_lycee, ars.age_sortie_lycee)
 
         arsnbenf = enf_primaire + enf_college + enf_lycee
 
         # Plafond en fonction du nb d'enfants A CHARGE (Cf. article R543)
-        ars_plaf_res = P.ars.plafond_ressources * (1 + af_nbenf * P.ars.majoration_par_enf_supp)
+        ars_plaf_res = ars.plafond_ressources * (1 + af_nbenf * ars.majoration_par_enf_supp)
 
         arsbase = bmaf * (
-            P.ars.taux_primaire * enf_primaire
-            + P.ars.taux_college * enf_college
-            + P.ars.taux_lycee * enf_lycee
+            ars.taux_primaire * enf_primaire
+            + ars.taux_college * enf_college
+            + ars.taux_lycee * enf_lycee
             )
 
         # Forme de l'ARS  en fonction des enfants a*n - (rev-plaf)/n
         # ars_diff = (ars_plaf_res + arsbase - base_ressources) / arsnbenf
         ars = (arsnbenf > 0) * max_(0, arsbase - max_(0, (base_ressources - ars_plaf_res) / max_(1, arsnbenf)))
 
-        return ars * (ars >= P.ars.montant_seuil_non_versement)
+        return ars * (ars >= ars.montant_seuil_non_versement)
