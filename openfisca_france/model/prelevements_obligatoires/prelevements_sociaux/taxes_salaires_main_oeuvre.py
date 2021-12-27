c7a250e078ad11afdf4efd019ab0e6d5c9ceeb6d
Sasha-Laniece <sasha.laniece@gmail.com>
2021-12-27 21:21:40 +0100

    Typo Lint

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/taxes_salaires_main_oeuvre.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/taxes_salaires_main_oeuvre.py
@@ -444,10 +444,8 @@ class contribution_formation_professionnelle(Variable):
         assiette_cotisations_sociales = individu('assiette_cotisations_sociales', period)
         contribution = parameters(period).prelevements_sociaux.autres_taxes_participations_assises_salaires.contribution_unique_formation.contrib_formation_pro
 
-        taux_contribution = (
-            (effectif_entreprise >= 11) * contribution.11_salaries_et_plus
-            + (effectif_entreprise < 11) * not_(apprenti) * contribution.moins_11_salaries
-            )
+        taux_contribution = ((effectif_entreprise >= 11) * contribution.onze_et_plus_salaries
+            + (effectif_entreprise < 11) * not_(apprenti) * contribution.moins_11_salaries)
 
         return - taux_contribution * assiette_cotisations_sociales
 
