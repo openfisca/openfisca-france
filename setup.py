aea3014113179d2b011de83566bb67c0ae835e11
Sasha-Laniece <sasha.laniece@gmail.com>
2021-12-27 21:54:34 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-France",
-    version = "89.0.0",
+    version = "90.0.0",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
     classifiers = [
