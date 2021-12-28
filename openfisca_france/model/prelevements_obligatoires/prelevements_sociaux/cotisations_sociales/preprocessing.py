9a16a4a8eb252b5bb8d0d7acd58f5ed16bb34b5b
Sasha-Laniece <sasha.laniece@gmail.com>
2021-12-28 11:59:36 +0100

    Harmonise `mmid.etat`

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/preprocessing.py
@@ -97,13 +97,15 @@ def build_pat(node_json):  # Ici node_json c'est le dossier 'parameters'
     pat.children['fonc'].children['contract'].children.update(commun.children)
 
     # Etat
-    pat.children['fonc'].children['etat'].children.update(public.mmid.etat.children)
+    pat.children['fonc'].children['militaire'].children.update(public.mmid.etat.tout_traitement.employeur.children)
+    # pat.children['fonc'].children['militaire'].children.update(public.mmid.etat.sous_plafond.employeur.children)
     pat.children['fonc'].children['etat'].children.update(public.rafp.employeur.children)
     pat.children['fonc'].children['etat'].children.update(public.retraite.ati.children)
     pat.children['fonc'].children['etat'].children.update(public.retraite.pension.employeur.civils.children)
 
     # Militaires
-    pat.children['fonc'].children['militaire'].children.update(public.mmid.etat.children)
+    pat.children['fonc'].children['militaire'].children.update(public.mmid.etat.tout_traitement.employeur.children)
+    # pat.children['fonc'].children['militaire'].children.update(public.mmid.etat.sous_plafond.employeur.children)
     pat.children['fonc'].children['militaire'].children.update(public.rafp.employeur.children)
     pat.children['fonc'].children['militaire'].children.update(public.retraite.pension.employeur.militaires.children)
 
@@ -112,6 +114,7 @@ def build_pat(node_json):  # Ici node_json c'est le dossier 'parameters'
     pat.children['fonc'].children['colloc'].children['territoriale'] = public.cnracl.employeur.territoriale
     pat.children['fonc'].children['colloc'].children.update(public.cnracl.employeur.children)
     pat.children['fonc'].children['colloc'].children.update(public.mmid.colloc.tout_traitement.employeur.children)
+    # pat.children['fonc'].children['colloc'].children.update(public.mmid.colloc.sous_plafond.employeur.children)
     pat.children['fonc'].children['colloc'].children.update(public.rafp.employeur.children)
 
     # Renaming
@@ -230,11 +233,14 @@ def build_sal(node_json):
     # Etat
     sal.children['fonc'].children['etat'].children.update(public.rafp.salarie.children)
     sal.children['fonc'].children['etat'].children.update(public.retraite.pension.salarie.children)
+    sal.children['fonc'].children['colloc'].children.update(public.mmid.etat.tout_traitement.salarie.children)
+    # sal.children['fonc'].children['colloc'].children.update(public.mmid.etat.sous_plafond.salarie.children)
     sal.children['public_titulaire_etat'] = sal.children['fonc'].children['etat']
 
     # Collectivités Locales
     sal.children['fonc'].children['colloc'].children.update(public.cnracl.salarie.children)
     sal.children['fonc'].children['colloc'].children.update(public.mmid.colloc.tout_traitement.salarie.children)
+    # sal.children['fonc'].children['colloc'].children.update(public.mmid.colloc.sous_plafond.salarie.children)
     sal.children['public_titulaire_territoriale'] = sal.children['fonc'].children['colloc']
     sal.children['public_titulaire_hospitaliere'] = sal.children['fonc'].children['colloc']
 
