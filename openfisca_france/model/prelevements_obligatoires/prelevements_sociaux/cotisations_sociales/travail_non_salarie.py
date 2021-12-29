e39e1a9e9bdd231400f0de4ee2f88267dc32d04b
Sasha-Laniece <sasha.laniece@gmail.com>
2021-12-29 15:24:47 +0100

    Harmonise `ret_ac` et ajoute la distinction artisans/commercant sous PSS dans la formule

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/travail_non_salarie.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/travail_non_salarie.py
@@ -249,10 +249,15 @@ class vieillesse_artisan_commercant(Variable):
         plafond_securite_sociale_annuel = parameters(period).prelevements_sociaux.pss.plafond_securite_sociale_annuel
         vieillesse_artisan_commercant = parameters(period).prelevements_sociaux.cotisations_taxes_independants_artisans_commercants.ret_ac
         bareme = MarginalRateTaxScale(name = 'vieillesse')
-        bareme.add_bracket(0, vieillesse_artisan_commercant.artisans.sous_pss + vieillesse_artisan_commercant.tous_independants.tout_salaire)
+        categorie_non_salarie = individu('categorie_non_salarie', period)
+        # Les taux sous_pss sont les mêmes pour artisans et commercants, mais tant qu'on a 2 paramètres distincts, il faut le prendre en compte dans la formule
+        artisan = (categorie_non_salarie == TypesCategorieNonSalarie.artisan)
+        commercant = (categorie_non_salarie == TypesCategorieNonSalarie.commercant)
+        sous_pss = (vieillesse_artisan_commercant.artisans.sous_pss) * artisan + (vieillesse_artisan_commercant.industriels_et_commercants.sous_pss) * commercant
+        # Barème final
+        bareme.add_bracket(0, sous_pss + vieillesse_artisan_commercant.tous_independants.tout_salaire)
         bareme.add_bracket(1, vieillesse_artisan_commercant.tous_independants.tout_salaire)
         bareme.multiply_thresholds(plafond_securite_sociale_annuel)
-        categorie_non_salarie = individu('categorie_non_salarie', period)
         assiette = (
             (categorie_non_salarie == TypesCategorieNonSalarie.artisan)
             + (categorie_non_salarie == TypesCategorieNonSalarie.commercant)
