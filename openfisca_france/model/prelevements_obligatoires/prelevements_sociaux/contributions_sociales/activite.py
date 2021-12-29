abb31f912ba662cc9d93c69992239996d0df06b9
Sasha-Laniece <sasha.laniece@gmail.com>
2021-12-29 17:19:46 +0100

    Harmonise `auto_entrepreneur.cotisations_prestations`

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/contributions_sociales/activite.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/contributions_sociales/activite.py
@@ -293,9 +293,9 @@ class rev_microsocial(Variable):
         assiette_proflib = foyer_fiscal('assiette_proflib', period)
         _P = parameters(period)
 
-        P = _P.cotsoc.sal.microsocial
+        P = _P.cotsoc.sal.microsocial.cotisations_prestations
         total = assiette_service + assiette_vente + assiette_proflib
-        prelsoc_ms = assiette_service * P.servi + assiette_vente * P.vente + assiette_proflib * P.rsi
+        prelsoc_ms = assiette_service * P.servi + assiette_vente * P.vente + assiette_proflib * P.cipav
         return total - prelsoc_ms
 
 
