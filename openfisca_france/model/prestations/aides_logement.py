a7ed9da852e17502225cc47cbda5dbb58f242095
Sasha-Laniece <sasha.laniece@gmail.com>
2022-01-05 09:26:45 +0100

    Déplace et renomme `participation_min.taux` vers `al_plaf_loc2.montant_forfaitaire.montant_proportionnel_participation_minimale_po`

--- openfisca_france/model/prestations/aides_logement.py
+++ openfisca_france/model/prestations/aides_logement.py
@@ -1257,7 +1257,7 @@ class aide_logement_participation_personnelle(Variable):
         loyer_retenu = famille('aide_logement_loyer_retenu', period)
         charges_retenues = famille('aide_logement_charges', period)
         E = loyer_retenu + charges_retenues
-        P0 = max_(al.participation_min.taux * E, al.participation_min.montant_forfaitaire)  # Participation personnelle minimale
+        P0 = max_(al.al_plaf_loc2.montant_forfaitaire.montant_proportionnel_participation_minimale_po * E, al.participation_min.montant_forfaitaire)  # Participation personnelle minimale
 
         Tf = famille('aide_logement_taux_famille', period)
         Tl = famille('aide_logement_taux_loyer', period)
