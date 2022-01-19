128a08bc3eab496371fd9ee93545a48b4da7b44c
Sasha-Laniece <sasha.laniece@gmail.com>
2022-01-19 16:29:08 +0100

    Fusionne `rsa_maj.majoration_montant_max_rsa.pac0` vers `rsa_maj.majoration_isolement_en_base_rsa.femmes_enceintes`

--- openfisca_france/model/prestations/minima_sociaux/rsa.py
+++ openfisca_france/model/prestations/minima_sociaux/rsa.py
@@ -362,7 +362,7 @@ class rsa_enfant_a_charge(Variable):
             taux_personne_supp = P_rsa.rsa_maj.majoration_rsa.taux_personne_supp
         else:
             age_pac = P_rmi.rmi_cond.age_pac
-            majo_rsa = P_rmi.rmi_maj.majoration_montant_max_rsa
+            majo_rsa = P_rmi.rmi_maj.majo_rsa
             montant_base_rsa = P_rmi.rmi_m.rmi
             taux_personne_supp = P_rmi.rmi_maj.txps
 
@@ -1011,7 +1011,7 @@ class rsa_socle_majore(Variable):
         nbenf = famille('rsa_nb_enfants', period)
 
         rsa = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.rsa
-        taux = rsa.rsa_maj.majoration_montant_max_rsa.pac0 + rsa.rsa_maj.majoration_montant_max_rsa.pac_enf_sup * nbenf
+        taux = rsa.rsa_maj.majoration_isolement_en_base_rsa.femmes_enceintes + rsa.rsa_maj.majoration_montant_max_rsa.pac_enf_sup * nbenf
         socle = rsa.rsa_m.montant_de_base_du_rsa
 
         return eligib * socle * taux
