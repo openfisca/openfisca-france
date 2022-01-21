724d915224391163bd93f54ad2423805db6b6c8c
Sasha-Laniece <sasha.laniece@gmail.com>
2022-01-21 14:06:57 +0100

    Fusionne `rmi_fl.forfait_logement_en_montant_forfaitaire_rmi` vers `rmi_fl.forfait_logement`

--- openfisca_france/model/prestations/minima_sociaux/rsa.py
+++ openfisca_france/model/prestations/minima_sociaux/rsa.py
@@ -846,9 +846,9 @@ class rsa_forfait_logement(Variable):
         else:
             params = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.rmi
             montant_base = params.rmi_m.rmi
-            forf_logement_taux_1p = params.rmi_fl.forfait_logement.taux1
-            forf_logement_taux_2p = params.rmi_fl.forfait_logement.taux2
-            forf_logement_taux_3p = params.rmi_fl.forfait_logement.taux3
+            forf_logement_taux_1p = params.rmi_fl.forfait_logement.taux_1_personne
+            forf_logement_taux_2p = params.rmi_fl.forfait_logement.taux_2_personnes
+            forf_logement_taux_3p = params.rmi_fl.forfait_logement.taux_3_personnes_ou_plus
 
         montant_forfait = montant_base * (
             (np_pers == 1) * forf_logement_taux_1p
