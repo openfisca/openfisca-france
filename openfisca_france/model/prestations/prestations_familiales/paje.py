ff4a2cf019cf1f9f7b4605ba3e6e025e8cb4e612
Sasha-Laniece <sasha.laniece@gmail.com>
2022-01-27 08:57:36 +0100

    Déplace, renomme et harmonise `paje.base.apres_2014.taux_allocation_base` vers `paje.paje_cm.montant.allocation_base_taux_plein.apres_2014.taux`

--- openfisca_france/model/prestations/prestations_familiales/paje.py
+++ openfisca_france/model/prestations/prestations_familiales/paje.py
@@ -117,7 +117,7 @@ class paje_base(Variable):
         indice = parameters(date_gel_paje).prestations_sociales.prestations_familiales.bmaf.bmaf if periode_de_gel else bmaf
         # Le taux ne dépend pas de l'année en cours mais de la réforme en vigueur pour l'année de naissance:
         montant_taux_plein_avant_2014 = indice * paje.paje_cm.montant.allocation_base_taux_plein.avant_2014.taux
-        montant_taux_plein_2014_2018 = indice * paje.base.apres_2014.taux_allocation_base
+        montant_taux_plein_2014_2018 = indice * paje.paje_cm.montant.allocation_base_taux_plein.apres_2014.taux
         montant_taux_plein_apres_2018 = indice * paje.base.apres_2018.taux_allocation_base
 
         # Avant réforme d'avril 2014 (enfants nés avant avril 2014)
