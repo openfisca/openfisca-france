8d89e06f7565587c13fe067e54ebdc70f24bb8e1
Sasha-Laniece <sasha.laniece@gmail.com>
2022-02-01 17:31:08 +0100

    Déplace, renomme et harmonise `clca.avecab_tx_inactif, avecab_tx_partiel1, et avecab_tx_partiel2` vers `paje_clca.beneficiaires_enfant_ne_adopte_apres_04_2014.taux_plein, taux_partiel_50 et taux_partiel_50_80`

--- openfisca_france/model/prestations/prestations_familiales/paje.py
+++ openfisca_france/model/prestations/prestations_familiales/paje.py
@@ -835,9 +835,9 @@ class paje_clca(Variable):
                     + partiel2 * paje.paje_clca.sansab_tx_partiel2
                     )
                 + paje_non_nul * (
-                    inactif * paje.paje_clca.avecab_tx_inactif
-                    + partiel1 * paje.paje_clca.avecab_tx_partiel1
-                    + partiel2 * paje.paje_clca.avecab_tx_partiel2
+                    inactif * paje.paje_clca.beneficiaires_enfant_ne_adopte_apres_04_2014.taux_plein
+                    + partiel1 * paje.paje_clca.beneficiaires_enfant_ne_adopte_apres_04_2014.taux_partiel_50
+                    + partiel2 * paje.paje_clca.beneficiaires_enfant_ne_adopte_apres_04_2014.taux_partiel_50_80
                     )
                 )
             )
