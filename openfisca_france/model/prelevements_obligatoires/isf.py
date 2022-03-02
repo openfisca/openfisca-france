0b9e6c150f2afb9e26b7ef998aa346439933badc
Sasha-Laniece <sasha.laniece@gmail.com>
2022-03-02 07:43:35 +0100

    Corrige typo

--- openfisca_france/model/prelevements_obligatoires/isf.py
+++ openfisca_france/model/prelevements_obligatoires/isf.py
@@ -571,7 +571,7 @@ class decote_isf_ifi(Variable):
         elig = (assiette_isf_ifi >= P.borne_inferieur_decote) & (assiette_isf_ifi <= P.borne_superieure_decote)
         LB = P.parametre_calcul_decote - P.taux_decote * assiette_isf_ifi
         return LB * elig
-    
+
     def formula_2013(foyer_fiscal, period, parameters):
         assiette_isf_ifi = foyer_fiscal('assiette_isf_ifi', period)
         P = parameters(period).taxation_capital.impot_solidarite_fortune_isf_1989_2017.decote
