96a8ad7ce50c55e7bd43eedf2b10f1e2d936f7fb
Sasha-Laniece <sasha.laniece@gmail.com>
2022-03-08 12:35:07 +0100

    Corrige lint

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/travail_non_salarie.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/travail_non_salarie.py
@@ -161,10 +161,10 @@ class maladie_maternite_artisan_commercant_taux(Variable):
         assiette_pss = assiette / plafond_securite_sociale_annuel
 
         taux = where(assiette_pss != 0, (
-                0.0085 + ((0.041 - 0.0085) * min_(max_(assiette_pss, 0), 0.4) / 0.4)
-                + ((0.072 - 0.041) * min_(max_((assiette_pss) - 0.4, 0), 0.7) / (1.1 - 0.4))
-                - (0.007 * (assiette_pss > 5) * ((assiette_pss - 5) / assiette_pss))
-                ), 0)
+            0.0085 + ((0.041 - 0.0085) * min_(max_(assiette_pss, 0), 0.4) / 0.4)
+            + ((0.072 - 0.041) * min_(max_((assiette_pss) - 0.4, 0), 0.7) / (1.1 - 0.4))
+            - (0.007 * (assiette_pss > 5) * ((assiette_pss - 5) / assiette_pss))
+            ), 0)
 
         return artisan * taux
 
