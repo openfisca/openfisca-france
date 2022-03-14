158e2ac631c8faffbdf03cfcab66723169d1f98c
Sasha-Laniece <sasha.laniece@gmail.com>
2022-03-14 23:23:45 +0100

    Déplace `.bouclier_fiscal` vers `.calcul_impot_revenu.bouclier_fiscal`

--- openfisca_france/model/prelevements_obligatoires/isf.py
+++ openfisca_france/model/prelevements_obligatoires/isf.py
@@ -955,6 +955,6 @@ class bouclier_fiscal(Variable):
     def formula_2006(foyer_fiscal, period, parameters):
         bouclier_sumimp = foyer_fiscal('bouclier_sumimp', period)
         bouclier_rev = foyer_fiscal('bouclier_rev', period)
-        P = parameters(period).impot_revenu.bouclier_fiscal
+        P = parameters(period).impot_revenu.calcul_impot_revenu.bouclier_fiscal
 
         return max_(0, bouclier_sumimp - (bouclier_rev * P.taux))
