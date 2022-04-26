2a1f9b14ceed00e89abef694c1cf2120db44721c
Emmanuel Raviart <emmanuel@raviart.com>
2022-04-26 11:20:12 +0200

    Convert to single quotes instead of double quotes.

--- openfisca_france/scripts/normalize_string_quotes.py
+++ openfisca_france/scripts/normalize_string_quotes.py
@@ -5,22 +5,27 @@ import re
 import tokenize
 
 
-SINGLE_QUOTE_DOCSTRING_RE = re.compile(
-    r"""^(?P<prefix>[furbFURB]*)'''(?P<content>.*?)'''$""",
+DOUBLE_QUOTE_DOCSTRING_RE = re.compile(
+    r'''^(?P<prefix>[furbFURB]*)"""(?P<content>.*?)"""$''',
     re.DOTALL)
-SINGLE_QUOTE_STRING_RE = re.compile(
-    r"""^(?P<prefix>[furbFURB]*)'(?P<content>[^"]*)'$""")
+DOUBLE_QUOTE_STRING_RE = re.compile(
+    r"""^(?P<prefix>[furbFURB]*)"(?P<content>[^']*)"$""")
+# SINGLE_QUOTE_DOCSTRING_RE = re.compile(
+#     r"""^(?P<prefix>[furbFURB]*)'''(?P<content>.*?)'''$""",
+#     re.DOTALL)
+# SINGLE_QUOTE_STRING_RE = re.compile(
+#     r"""^(?P<prefix>[furbFURB]*)'(?P<content>[^"]*)'$""")
 
 
 def iterate_tokens(file_path):
-    with open(file_path, encoding="utf-8") as python_file:
+    with open(file_path, encoding='utf-8') as python_file:
         for token in tokenize.generate_tokens(python_file.readline):
             if token.type == tokenize.STRING:
-                token = token._replace(string=SINGLE_QUOTE_DOCSTRING_RE.sub(
-                    '\g<prefix>"""\g<content>"""',  # noqa: W605
+                token = token._replace(string=DOUBLE_QUOTE_DOCSTRING_RE.sub(
+                    "\g<prefix>'''\g<content>'''",  # noqa: W605
                     token.string))
-                token = token._replace(string=SINGLE_QUOTE_STRING_RE.sub(
-                    '\g<prefix>"\g<content>"',  # noqa: W605
+                token = token._replace(string=DOUBLE_QUOTE_STRING_RE.sub(
+                    "\g<prefix>'\g<content>'",  # noqa: W605
                     token.string))
             yield token
 
@@ -29,12 +34,12 @@ project_dir = os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.abspath(__file__))))
 for path, dirs, files in os.walk(project_dir):
     for dir in dirs:
-        if dir.startswith("."):
+        if dir.startswith('.'):
             dirs.remove(dir)
     for file in files:
-        if file.startswith(".") or not file.endswith(".py"):
+        if file.startswith('.') or not file.endswith('.py'):
             continue
         file_path = os.path.join(path, file)
         normalized_source_code = tokenize.untokenize(iterate_tokens(file_path))
-        with open(file_path, "w", encoding="utf-8") as python_file:
+        with open(file_path, 'w', encoding='utf-8') as python_file:
             python_file.write(normalized_source_code)
