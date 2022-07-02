b2fe106a1cae2a1c277fec7bae0642fc6edbf6cc
eraviart <emmanuel@raviart.com>
2022-07-02 10:06:05 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '116.13.1',
+    version = '116.13.2',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
