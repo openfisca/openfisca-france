a78f73d98531eaf5962a5372c75256a5f3242049
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-07-04 20:29:18 +0200

    Ajout de print dans le check path length

--- openfisca_france/scripts/check_longueur_chemins.py
+++ openfisca_france/scripts/check_longueur_chemins.py
@@ -6,10 +6,15 @@ Il est à utiliser avant de contribuer à l'harmonisation
 
 
 import os
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 def extract_paths_too_long(root_dir):
     chemin_trop_long_trouve = False
+    maxlen = 0
     root_dir = root_dir.rstrip(os.sep)
 
     with open('chemins_trop_longs.txt', 'w') as outfile:
@@ -24,12 +29,15 @@ def extract_paths_too_long(root_dir):
                     # Ignore hidden files.
                     continue
                 relative_file_path = os.path.join(relative_path, file)
+                maxlen = max(maxlen, len(relative_file_path))
                 if len(relative_file_path) > 150:
                     chemin_trop_long_trouve = True
+                    logging.error(f"Chemin trop long de {len(relative_file_path)-150} caractères : {relative_file_path}")
                     outfile.write('{} here: {}\n'.format(
                         len(relative_file_path),
                         relative_file_path,
                         ))
+    logging.info(f"Longueur maximale trouvée : {maxlen} caractères.")
     return chemin_trop_long_trouve
 
 
