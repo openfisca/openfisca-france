b7181c5337dc46c8cd61177da522c00660809892
DorineLam <46896006+DorineLam@users.noreply.github.com>
2022-07-13 13:52:04 +0200

    Met à jour la version openfisca

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '116.13.2',
+    version = '116.13.3',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
