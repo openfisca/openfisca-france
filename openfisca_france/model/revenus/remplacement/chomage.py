e0abadef2bcff8c0c62b70f4510299846660962f
sandcha <sandcha@users.noreply.github.com>
2022-08-31 19:04:43 +0200

    Corrige chomage_brut avec le Complément ARE brut Complément ARE contenant allocation + crds + csg + corisation retraite complémentaire

--- openfisca_france/model/revenus/remplacement/chomage.py
+++ openfisca_france/model/revenus/remplacement/chomage.py
@@ -27,7 +27,7 @@ class chomage_brut(Variable):
     calculate_output = calculate_output_add
 
     def formula(individu, period):
-        return individu('allocation_retour_emploi', period) + individu('complement_are_net', period)
+        return individu('allocation_retour_emploi', period) + individu('complement_are_brut', period)
 
 
 class indemnites_chomage_partiel(Variable):
