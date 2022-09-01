980beff49488d677d802ffaac530b564e6be21fe
Sasha-Laniece <sasha.laniece@gmail.com>
2022-09-01 10:11:25 +0200

    Passage RMI-RSA et CMU-CSS

--- openfisca_france/model/prestations/minima_sociaux/cs/cmu.py
+++ openfisca_france/model/prestations/minima_sociaux/cs/cmu.py
@@ -27,9 +27,25 @@ class cmu_forfait_logement_base(Variable):
     Calcule le forfait logement en fonction du nombre de personnes dans le "foyer CMU" et d'un jeu de taux.
     '''
 
-    def formula_2009_06_01(famille, period, parameters):
+    def formula_2019_10_01(famille, period, parameters):
         nbp_foyer = famille('cmu_nbp_foyer', period)
         P = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.cs.css.forfait_logement
+        law_rmi_rsa = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.rsa
+
+        montant_rsa_socle = law_rmi_rsa.rsa_m.montant_de_base_du_rsa * (
+            1
+            + law_rmi_rsa.rsa_maj.maj_montant_max.couples_celibataire_avec_enfant * (nbp_foyer >= 2)
+            + law_rmi_rsa.rsa_maj.maj_montant_max.couple_1_enfant_ou_2e_enfant * (nbp_foyer >= 3)
+            )
+
+        return 12 * montant_rsa_socle * select(
+            [nbp_foyer == 1, nbp_foyer == 2, nbp_foyer > 2],
+            [P.taux_1p, P.taux_2p, P.taux_3p_plus]
+            )
+    
+    def formula_2009_06_01(famille, period, parameters):
+        nbp_foyer = famille('cmu_nbp_foyer', period)
+        P = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.cs.cmu.forfait_logement
         law_rmi_rsa = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.rsa
 
         montant_rsa_socle = law_rmi_rsa.rsa_m.montant_de_base_du_rsa * (
@@ -67,10 +83,29 @@ class cmu_forfait_logement_al(Variable):
     definition_period = MONTH
     set_input = set_input_divide_by_period
 
-    def formula_2009_06_01(famille, period, parameters):
+    def formula_2019_10_01(famille, period, parameters):
         nbp_foyer = famille('cmu_nbp_foyer', period)
         aide_logement = famille('aide_logement', period)
         P = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.cs.css.forfait_logement
+        law_rmi_rsa = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.rsa
+
+        montant_rsa_socle = law_rmi_rsa.rsa_m.montant_de_base_du_rsa * (
+            1
+            + law_rmi_rsa.rsa_maj.maj_montant_max.couples_celibataire_avec_enfant * (nbp_foyer >= 2)
+            + law_rmi_rsa.rsa_maj.maj_montant_max.couple_1_enfant_ou_2e_enfant * (nbp_foyer >= 3)
+            )
+
+        forfait_logement = 12 * montant_rsa_socle * select(
+            [nbp_foyer == 1, nbp_foyer == 2, nbp_foyer > 2],
+            [P.taux_1p, P.taux_2p, P.taux_3p_plus]
+            )
+
+        return (aide_logement > 0) * min_(12 * aide_logement, forfait_logement)
+
+    def formula_2009_06_01(famille, period, parameters):
+        nbp_foyer = famille('cmu_nbp_foyer', period)
+        aide_logement = famille('aide_logement', period)
+        P = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.cs.cmu.forfait_logement
         law_rmi_rsa = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.rsa
 
         montant_rsa_socle = law_rmi_rsa.rsa_m.montant_de_base_du_rsa * (
