6278a15ce417e046bc14fb816e756908e40bb6c9
sandcha <sandcha@users.noreply.github.com>
2022-09-02 23:39:39 +0200

    Précise le statut d'activité du bénéficiaire potentiel de Complément ARE

--- openfisca_france/model/prestations/complement_are.py
+++ openfisca_france/model/prestations/complement_are.py
@@ -1,13 +1,14 @@
 from numpy import ceil, divide, where, zeros_like
 
 from openfisca_france.model.base import Individu, Variable, MONTH, \
-    set_input_divide_by_period, set_input_dispatch_by_period, round_, max_, min_
+    set_input_divide_by_period, set_input_dispatch_by_period, TypesActivite, \
+        round_, max_, min_
 
 
 class eligibilite_complement_are(Variable):
     value_type = bool
     entity = Individu
-    label = "L'individu est éligible au complément ARE car il perçoit un salaire"
+    label = "L'individu est éligible au Complément ARE car il perçoit un salaire"
     definition_period = MONTH
     reference = [
         'https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000044345334',
@@ -16,8 +17,12 @@ class eligibilite_complement_are(Variable):
     set_input = set_input_divide_by_period
 
     def formula(individu, period):
-        # le complément ARE prend le pas sur l'ARE dès lors qu'un salaire est perçu
-        return individu('salaire_de_base', period) > 0
+        activite = individu('activite', period)
+        reprise_activite = (activite == TypesActivite.actif)
+        
+        # le complément ARE prend le pas sur l'ARE lorsqu'un salaire est perçu
+        return reprise_activite * individu('salaire_de_base', period) > 0
+
 
 class complement_are_plafond(Variable):
     value_type = float
