f4ff789112aebccc920ad248759559a0eb32f7fa
fjacquetin <florian.jacquetin@gmail.com>
2022-10-20 17:57:40 +0200

    Crée test prime_exceptionnelle_rentrée

--- openfisca_france/model/prestations/minima_sociaux/per.py
+++ openfisca_france/model/prestations/minima_sociaux/per.py
@@ -42,20 +42,20 @@ class eligibilite_per(Variable):
 #        eligibilite_etudiant = (individu.famille('bourse_enseignement_sup',juin_2022) > 0) * (individu.famille('aide_logement',juin_2022) == 0)
 
 #        return (eligibilite_etudiant) > 0
-        
+
 class eligibilite_per_ppa(Variable):
-    entity = Individu
+    entity = Famille
     value_type = bool
     reference = 'https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000046289935'
     label = "Eligibilité à la prime exceptionnelle de rentrée des bénéficiaires de la prime d'activité"
     definition_period = ETERNITY
 
-    def formula(individu,period):
+    def formula(famille,period):
         juin_2022 = periods.period('2022-06')
-        eligibilite_ppa = (individu('ppa',juin_2022) > 0)
+        eligibilite_ppa = (famille('ppa',juin_2022) > 0)
 
         return (eligibilite_ppa) > 0
-        
+
 
 
 class prime_exceptionnelle_rentree(Variable):
@@ -68,11 +68,11 @@ class prime_exceptionnelle_rentree(Variable):
     def formula(famille,period,parameters):
 
         enfant_i = famille.members.has_role(Famille.ENFANT)
-        nb_enfants = famille.sum(enfant_i)  
+        nb_enfants = famille.sum(enfant_i)
         parametres_per = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per
         ppa=0
 
-        if famille('eligibilite_per',period) > 0:    
+        if famille('eligibilite_per',period) > 0:
             ppa = parametres_per.per + nb_enfants * parametres_per.per_enfant
 
         #elif ((famille('eligibilite_per',period)) == 0 & (famille('eligibilite_per_etudiant',period) > 0)):
@@ -82,4 +82,4 @@ class prime_exceptionnelle_rentree(Variable):
         # & (famille('eligibilite_per_etudiants') == 0)
             ppa = parametres_per.per_ppa + nb_enfants * parametres_per.per_ppa_enfant
 
-        return ppa
+        return parametres_per.per
