68ee760e83042e46e933fe2a0681e973db04eb7b
fjacquetin <florian.jacquetin@gmail.com>
2022-10-20 17:57:40 +0200

    Crée test prime_exceptionnelle_rentrée

--- openfisca_france/model/prestations/minima_sociaux/per.py
+++ openfisca_france/model/prestations/minima_sociaux/per.py
@@ -3,78 +3,83 @@ from openfisca_core import periods
 from numpy import logical_or as or_, logical_and as and_
 
 class eligibilite_per(Variable):
-    entity = Individu
+    entity = Famille
     value_type = bool
     reference = 'https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000046289935'
     label = "Eligibilité à la prime exceptionnelle de rentrée"
-    definition_period = YEAR
+    definition_period = ETERNITY
 
-    def formula(individu, period, parameters):
+    def formula(famille,period):
 
-        juin_2022 = periods.period('2021-06')
+        juin_2022 = periods.period('2022-06')
 
-        eligibilite = individu.famille('rsa', juin_2022) * individu.has_role(Famille.PARENT)
-        + individu.famille('apl', juin_2022)
-        + individu.famille('alf', juin_2022)
-        + individu.famille('als', juin_2022)
-        + individu('ass', juin_2022)
-        + individu('aer',juin_2022)
-        + individu.famille('aspa',juin_2022)
-        + individu('aah',juin_2022)
+        eligibilite = famille('rsa', juin_2022)
+        + famille('apl', juin_2022)
+        + famille('alf', juin_2022)
+        + famille('als', juin_2022)
+        + famille('aspa',juin_2022)
 
-        eligibilite =  individu.famille('ppa',juin_2022)
-        
-        return eligibilite > 0
+        eligibilite_ass_i = famille.members('ass', juin_2022)
+        eligibilite_ass = famille.sum(eligibilite_ass_i)
 
-class eligibilite_per_etudiants(Variable):
-    entity = Individu
-    value_type = bool
-    reference = 'https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000046289843'
-    label = "Eligibilité à la prime exceptionnelle de rentrée des étudiants boursiers"
-    definition_period = YEAR
+        eligibilite_aer_i = famille.members('aer',juin_2022)
+        eligibilite_aer = famille.sum(eligibilite_aer_i)
 
-    def formula(individu, period, parameters):
-        eligibilite_etudiant = (individu.famille('ppa',juin_2022) > 0) * (individu.famille('apl',juin_2022) > 0)
+        eligibilite_aah_i = famille.members('aah',juin_2022)
+        eligibilite_aah = famille.sum(eligibilite_aah_i)
 
-        return (eligibilite_etudiant) > 0
+        return (eligibilite + eligibilite_ass + eligibilite_aer + eligibilite_aah ) > 0
+
+#class eligibilite_per_etudiant(Variable):
+#    entity = Individu
+#    value_type = bool
+#    reference = 'https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000046289843'
+#    label = "Eligibilité à la prime exceptionnelle de rentrée des étudiants boursiers"
+#    definition_period = YEAR
+
+#    def formula(individu):
+#        juin_2022 = periods.period('2022-06')
+#        eligibilite_etudiant = (individu.famille('bourse_enseignement_sup',juin_2022) > 0) * (individu.famille('aide_logement',juin_2022) == 0)
+
+#        return (eligibilite_etudiant) > 0
         
 class eligibilite_per_ppa(Variable):
     entity = Individu
     value_type = bool
     reference = 'https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000046289935'
     label = "Eligibilité à la prime exceptionnelle de rentrée des bénéficiaires de la prime d'activité"
-    definition_period = YEAR
+    definition_period = ETERNITY
 
-    def formula(individu, period, parameters):
-        eligibilite_ppa = (individu('bourse_enseignement_sup',juin_2022) > 0) * (individu.famille('apl',juin_2022) > 0)
+    def formula(individu,period):
+        juin_2022 = periods.period('2022-06')
+        eligibilite_ppa = (individu('ppa',juin_2022) > 0)
 
         return (eligibilite_ppa) > 0
         
 
 
 class prime_exceptionnelle_rentree(Variable):
-    entity = Individu
+    entity = Famille
     value_type = bool
     reference = 'https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000046289935'
     label = "Prime exceptionnelle de rentrée"
     definition_period = YEAR
 
-    def formula(individu,period,parameters):
+    def formula(famille,period,parameters):
 
-    enfant_i = individu.menage.members.has_role(Famille.ENFANT)
-    nb_enfants = menage.sum(enfant_i)  
-    ppa=0
+        enfant_i = famille.members.has_role(Famille.ENFANT)
+        nb_enfants = famille.sum(enfant_i)  
+        parametres_per = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per
+        ppa=0
 
-        if individu('eligibilite_per',period) > 0:    
-            ppa = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per
-            + nb_enfants * parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per
+        if famille('eligibilite_per',period) > 0:    
+            ppa = parametres_per.per + nb_enfants * parametres_per.per_enfant
 
-        elif ((individu('eligibilite_per',period)) == 0 & (individu('eligibilite_per_etudiants',period) > 0)):
-            ppa = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per_enfant
-            + nb_enfants * parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per_enfant
+        #elif ((famille('eligibilite_per',period)) == 0 & (famille('eligibilite_per_etudiant',period) > 0)):
+        #    ppa = parametres_per.per_etudiant + nb_enfants * parametres_per.per_etudiant_enfant
 
-        elif individu('eligibilite_per',period) == 0 & individu('eligibilite_per_etudiants',period) > 0 & individu.famille(eligibilite_per_ppa,period)
-            ppa = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per_ppa
-            + nb_enfants * parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per_ppa_activite
+        elif (famille('eligibilite_per',period) == 0)  & (famille('eligibilite_per_ppa',period) > 0):
+        # & (famille('eligibilite_per_etudiants') == 0)
+            ppa = parametres_per.per_ppa + nb_enfants * parametres_per.per_ppa_enfant
 
         return ppa
