bfaf51901316267170a262efc3dd1d11612be8e8
DorineLam <46896006+DorineLam@users.noreply.github.com>
2022-10-24 17:24:59 +0200

    Fix majuscules regime_securite_sociale

--- openfisca_france/model/caracteristiques_socio_demographiques/demographie.py
+++ openfisca_france/model/caracteristiques_socio_demographiques/demographie.py
@@ -398,10 +398,10 @@ class enfant_place(Variable):
 
 class RegimeSecuriteSociale(Enum):
     regime_general = 'Régime général'
-    regime_agricole = 'Régime Agricole'
-    regime_retraite_fonctionnaires_civils_militaires_etat = 'Régime de retraite des fonctionnaires civils et militaires de l’Etat'
+    regime_agricole = 'Régime agricole'
+    regime_retraite_fonctionnaires_civils_militaires_etat = 'Régime de retraite des fonctionnaires civils et militaires de l’État'
     regime_special_fonctionnaires_territoriaux_hospitaliers = 'Régime spécial des fonctionnaires territoriaux et hospitaliers'
-    fond_special_pensions_ouvriers_etablissements_industriels_etat = 'Le Fonds spécial des pensions des ouvriers des établissements industriels de l’Etat (FSPOEIE)'
+    fond_special_pensions_ouvriers_etablissements_industriels_etat = 'Le Fonds spécial des pensions des ouvriers des établissements industriels de l’État (FSPOEIE)'
     regime_special_agents_sncf = 'Régime spécial des agents de la SNCF'
     regime_special_agents_ratp = 'Régime spécial des agents de la RATP'
     regime_special_industries_electriques_gazieres = 'Régime spécial des industries électriques et gazières (CNIEG)'
