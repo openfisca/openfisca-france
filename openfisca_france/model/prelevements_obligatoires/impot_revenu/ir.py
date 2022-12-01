ded3e277a7c5b7431064dda184941245f07ae511
Sasha-Laniece <sasha.laniece@gmail.com>
2022-12-01 15:47:20 +0100

    Renomme et harmonise `ppe.eligi2` dans `ppe.seuil_rfr_eligibilite.couple_marie_pacse`

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
@@ -3368,7 +3368,7 @@ class ppe_elig(Variable):
 
         seuil = (
             (veuf | celibataire_ou_divorce) * (ppe.seuil_rfr_eligibilite.personne_seule + 2 * max_(nbptr - 1, 0) * ppe.seuil_rfr_eligibilite.eligi3)
-            + maries_ou_pacses * (ppe.seuil_rfr_eligibilite.eligi2 + 2 * max_(nbptr - 2, 0) * ppe.seuil_rfr_eligibilite.eligi3)
+            + maries_ou_pacses * (ppe.seuil_rfr_eligibilite.couple_marie_pacse + 2 * max_(nbptr - 2, 0) * ppe.seuil_rfr_eligibilite.eligi3)
             )
 
         return (rfr * ppe_coef) <= seuil
