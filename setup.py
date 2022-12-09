eb620f71c533dda348987929d1f5e4620cc4111f
Lucas Détré <lucas.detre@gmail.com>
2022-12-09 16:26:26 +0100

    Modification de la version et du changelog

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '123.1.0',
+    version = '123.2.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
