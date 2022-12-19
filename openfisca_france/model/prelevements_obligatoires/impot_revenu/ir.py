64f1d668fcde67cacfc35c987336fae0333e29fb
Sasha-Laniece <sasha.laniece@gmail.com>
2022-12-19 09:40:58 +0100

    Renomme `taux_prestations_de_services` en `taux_prestations_services` et `taux_ventes_de_marchandises` en `taux_ventes_marchandises`

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
@@ -1645,8 +1645,8 @@ class microentreprise(Variable):
         ebic_impv = foyer_fiscal.sum(ebic_impv_i)
         return (
             max_(0, ebnc_impo - max_(micro.microentreprise.montant_minimum, micro.specialbnc.taux * ebnc_impo))
-            + max_(0, ebic_imps - max_(micro.microentreprise.montant_minimum, micro.microentreprise.taux_prestations_de_services * ebic_imps))
-            + max_(0, ebic_impv - max_(micro.microentreprise.montant_minimum, micro.microentreprise.taux_ventes_de_marchandises * ebic_impv))
+            + max_(0, ebic_imps - max_(micro.microentreprise.montant_minimum, micro.microentreprise.taux_prestations_services * ebic_imps))
+            + max_(0, ebic_impv - max_(micro.microentreprise.montant_minimum, micro.microentreprise.taux_ventes_marchandises * ebic_impv))
             )
 
 
