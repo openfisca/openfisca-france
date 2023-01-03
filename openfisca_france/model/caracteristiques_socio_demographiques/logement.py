9f26ae989eaa271101af2c6c337c05e12193c15d
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-01-03 16:43:07 +0100

    Éligibilité regionale PdlL : vert

--- openfisca_france/model/caracteristiques_socio_demographiques/logement.py
+++ openfisca_france/model/caracteristiques_socio_demographiques/logement.py
@@ -88,6 +88,7 @@ class region(Variable):
             (P.normandie, TypesCodeInseeRegion.normandie),
             (P.hauts_de_france, TypesCodeInseeRegion.hauts_de_france),
             (P.grand_est, TypesCodeInseeRegion.grand_est),
+            (P.pays_de_la_loire, TypesCodeInseeRegion.pays_de_la_loire),
                    ]
         regions_elig = [sum([startswith(depcom, str.encode(code)) for code in PR.departements]) > 0 for (PR,_) in regions]
         regions_value = [RV for (_,RV) in regions]
