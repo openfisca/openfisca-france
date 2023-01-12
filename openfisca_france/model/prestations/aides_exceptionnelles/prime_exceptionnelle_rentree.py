99e7f933e1925b38cfff61aeb4fb3378101b6405
fjacquetin <florian.jacquetin@gmail.com>
2023-01-12 16:37:30 +0100

    Modifie l'éligibilité à la prime étudiant et exclut en partie la majoration par enfant

--- openfisca_france/model/prestations/aides_exceptionnelles/prime_exceptionnelle_rentree.py
+++ openfisca_france/model/prestations/aides_exceptionnelles/prime_exceptionnelle_rentree.py
@@ -29,7 +29,7 @@ class eligibilite_per(Variable):
 
         eligibilite_aah_i = famille.members('aah', juin_2022)
         eligibilite_aah = famille.sum(eligibilite_aah_i)
-        
+
         eligibilite_pfmra_i = famille.members('prime_forfaitaire_mensuelle_reprise_activite', juin_2022)
         eligibilite_pfmra = famille.sum(eligibilite_pfmra_i)
 
@@ -46,11 +46,12 @@ class eligibilite_per_etudiant(Variable):
 
     def formula_2022(individu, period):
         juin_2022 = periods.period('2022-06')
+        annee_2022 = periods.period('2022')
         eligibilite_etudiant = where(
-            not_(or_(individu('bourse_criteres_sociaux', period) > 0, individu('bourse_enseignement_sup') > 0)),
+            not_(or_(individu('bourse_criteres_sociaux', juin_2022) > 0, individu('bourse_enseignement_sup', juin_2022) > 0)),
             False,
             where(
-                and_(individu.has_role(Famille.ENFANT), individu.famille('aide_logement', juin_2022) > 0),
+                and_(individu.has_role(Famille.DEMANDEUR), individu.famille('eligibilite_per', annee_2022)),
                 False,
                 True
                 )
@@ -84,8 +85,9 @@ class prime_exceptionnelle_rentree_non_etudiant(Variable):
 
     def formula_2022(famille, period, parameters):
 
-        enfant_i = famille.members.has_role(Famille.ENFANT)
-        nb_enfants = famille.sum(enfant_i)
+        juin_2022 = periods.period('2022-06')
+        prestations_familiales_enfant_a_charge_i = famille.members('prestations_familiales_enfant_a_charge', juin_2022)
+        nb_enfants = famille.sum(prestations_familiales_enfant_a_charge_i)
         parametres_per = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per
 
         per = where(
@@ -110,8 +112,14 @@ class prime_exceptionnelle_rentree_etudiant(Variable):
     end = '2022-12-31'
 
     def formula_2022(individu, period, parameters):
-        enfant_i = individu.famille.members.has_role(Famille.ENFANT)
-        nb_enfants = individu.famille.sum(enfant_i)
+
+        '''
+        Hypothèse : Si l'étudiant  est un enfant dans sa famille, ses enfants ne sont pas identifiés. La majoration pour enfant ne rentre donc pas en compte dans le calcul.
+        '''
+
+        juin_2022 = periods.period('2022-06')
+        prestations_familiales_enfant_a_charge_i = individu.famille.members('prestations_familiales_enfant_a_charge', juin_2022)
+        nb_enfants = individu.famille.sum(prestations_familiales_enfant_a_charge_i)
         parametres_per = parameters(period).prestations_sociales.solidarite_insertion.minima_sociaux.per
 
         per_etudiant = where(
