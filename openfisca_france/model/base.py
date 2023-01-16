340518e1e0a6bfde7bf41d9d01a0500c6733de1a
DorineLam <46896006+DorineLam@users.noreply.github.com>
2023-01-16 12:27:30 +0100

    Fix lint : change quotes et espace commentaire

--- openfisca_france/model/base.py
+++ openfisca_france/model/base.py
@@ -59,12 +59,12 @@ class TypesCategorieSalarie(Enum):
     __order__ = 'prive_non_cadre prive_cadre public_titulaire_etat public_titulaire_militaire public_titulaire_territoriale public_titulaire_hospitaliere public_non_titulaire non_pertinent'  # Needed to preserve the enum order in Python 2
     prive_non_cadre = 'Non cadre du secteur privé'
     prive_cadre = 'Cadre du secteur privé'
-    public_titulaire_etat = 'Titulaire de la fonction publique d\'État'
-    public_titulaire_militaire = 'Titulaire de la fonction publique militaire'
-    public_titulaire_territoriale = 'Titulaire de la fonction publique territoriale'
-    public_titulaire_hospitaliere = 'Titulaire de la fonction publique hospitalière'
-    public_non_titulaire = 'Agent non-titulaire de la fonction publique' # Les agents non titulaires, c’est-à-dire titulaires d’aucun grade de la fonction publique, peuvent être des contractuels, des vacataires, des auxiliaires, des emplois aidés…Les assistants maternels et familiaux sont eux aussi des non-titulaires.
-    non_pertinent = 'Non pertinent'
+    public_titulaire_etat = "Titulaire de la fonction publique d'État"
+    public_titulaire_militaire = "Titulaire de la fonction publique militaire"
+    public_titulaire_territoriale = "Titulaire de la fonction publique territoriale"
+    public_titulaire_hospitaliere = "Titulaire de la fonction publique hospitalière"
+    public_non_titulaire = "Agent non-titulaire de la fonction publique"  # Les agents non titulaires, c’est-à-dire titulaires d’aucun grade de la fonction publique, peuvent être des contractuels, des vacataires, des auxiliaires, des emplois aidés…Les assistants maternels et familiaux sont eux aussi des non-titulaires.
+    non_pertinent = "Non pertinent"
 
 
 class TypesStatutMarital(Enum):
