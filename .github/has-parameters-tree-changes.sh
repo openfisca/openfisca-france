26f9bc46818e50bac259590927d3a65dfcb378ea
sandcha <sandcha@users.noreply.github.com>
2023-01-24 16:20:20 +0100

    Indique les répertoires ajoutés et perdus Et colore le message en sortie standard

--- .github/has-parameters-tree-changes.sh
+++ .github/has-parameters-tree-changes.sh
@@ -1,13 +1,10 @@
 #! /usr/bin/env bash
 
+PURPLE='\033[0;95m'
+CYAN='\033[0;36m'
+COLOR_RESET='\033[0m'
+
 last_tagged_commit=`git describe --tags --abbrev=0 --first-parent`  # --first-parent ensures we don't follow tags not published in master through an unlikely intermediary merge commit
-# last_tagged_commit="dd99f9bf357c2f438d62a1a025b8e56c4f9261d2"
-
-checked_tree_root="openfisca_france/parameters/"
-checked_tree_depth=3
-checked_tree=`git ls-tree ${last_tagged_commit} -d --name-only -r ${checked_tree_root} | cut -d / -f-${checked_tree_depth} | uniq`
-
-# echo ${checked_tree}
 
 pattern=(
     "pattern_test"
@@ -15,19 +12,27 @@ pattern=(
     "openfisca_france/parameters/marche_travail"
     )
 
-# echo ${pattern}
-
-# uniq -u : lignes non répétées (ajouts ?)
-# uniq -D : lignes répétées (pas de suppression ?)
-# uniq -d : lignes répétées, print unique
+checked_tree_depth=3
+checked_tree_root="openfisca_france/parameters/"
+checked_tree=`git ls-tree ${last_tagged_commit} -d --name-only -r ${checked_tree_root} | cut -d / -f-${checked_tree_depth} | uniq`
 
 common=`echo ${pattern[@]} ${checked_tree[@]} | tr ' ' '\n' | sort | uniq -D | uniq`
-# echo ${common}
 
 # ajouts sur la branche par rapport au pattern
 added=`echo ${common[@]} ${checked_tree[@]} | tr ' ' '\n' | sort | uniq -u | uniq`
-# printf '%s\n' "${added[@]}"
+ 
+if [[ ${added[@]} ]]; then
+    echo "${CYAN}The current branch adds the following directories:${COLOR_RESET}"
+
+    added_array=(${added})
+    if [[ ${added_array[0]} == "openfisca_france" && ${added_array[1]} == "openfisca_france/parameters" ]]; then
+        printf '%s\n' "${added_array[@]:$((2))}"
+    fi
+fi
 
 # hypothèse : le pattern est de même profondeur maximale que checked_tree_depth
 lost=`echo ${common[@]} ${pattern[@]} | tr ' ' '\n' | sort | uniq -u | uniq`
-printf '%s\n' "${lost[@]}"
+if [[ ${added[@]} ]]; then
+    echo "${PURPLE}The current branch removes the following directories:${COLOR_RESET}"
+    printf '%s\n' "${lost[@]}"
+fi
