79046a28f410a711f2b48a5634ba9a2ec8696109
Sasha-Laniece <sasha.laniece@gmail.com>
2023-03-02 15:24:10 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '143.1.3',
+    version = '144.0.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
