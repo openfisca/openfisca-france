a6140c51137377451b971c1dbfd5806549dd8b4f
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-03-13 15:56:54 +0100

    Ignore le warning flake8 print

--- .github/get_minimal_version.py
+++ .github/get_minimal_version.py
@@ -5,4 +5,4 @@ with open('./setup.py') as file:
     for line in file:
         version = re.search(r'(Core|France)\s*>=\s*([\d\.]*)', line)
         if version:
-            print(f'Openfisca-{version[1]}=={version[2]}')
+            print(f'Openfisca-{version[1]}=={version[2]}')  # noqa: T201 <- This is to avoid flake8 print detection.
