32f74fda91926d66b6504c4bd7df7ecaf072f56c
TOMAS Frederic <frederic.tomas@soprasteria.com>
2023-03-15 10:59:43 +0100

    Factorise et nettoie le code

--- openfisca_france/reforms/aah_deconjugalisee.py
+++ openfisca_france/reforms/aah_deconjugalisee.py
@@ -20,7 +20,7 @@ class aah_deconjugalisee(Reform):
 
     class aah_base_ressources(Variable):
         value_type = float
-        label = "Base ressources de l'allocation adulte handicapé"
+        label = 'Base ressources de l\'allocation adulte handicapé'
         entity = Individu
         definition_period = MONTH
         set_input = set_input_divide_by_period
@@ -45,8 +45,9 @@ class aah_deconjugalisee(Reform):
 
             def base_ressource_eval_trim():
                 three_previous_months = period.first_month.start.period('month', 3).offset(-3)
-                base_ressource_activite = individu('aah_base_ressources_activite_eval_trimestrielle', period) - individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
-                base_ressource_hors_activite = individu('aah_base_ressources_hors_activite_eval_trimestrielle', period) + individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
+                base_ressources_activite_milieu_protege = individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
+                base_ressource_activite = individu('aah_base_ressources_activite_eval_trimestrielle', period) - base_ressources_activite_milieu_protege
+                base_ressource_hors_activite = individu('aah_base_ressources_hors_activite_eval_trimestrielle', period) + base_ressources_activite_milieu_protege
 
                 base_ressource_demandeur = assiette_revenu_activite_demandeur(base_ressource_activite) + base_ressource_hors_activite
 
@@ -76,7 +77,7 @@ class aah_deconjugalisee(Reform):
 
     class aah_base_ressources_deconjugalisee(Variable):
         value_type = float
-        label = "Base ressources de l'allocation adulte handicapé"
+        label = 'Base ressources de l\'allocation adulte handicapé'
         entity = Individu
         definition_period = MONTH
         set_input = set_input_divide_by_period
@@ -95,8 +96,9 @@ class aah_deconjugalisee(Reform):
 
             def base_ressource_eval_trim():
                 three_previous_months = period.first_month.start.period('month', 3).offset(-3)
-                base_ressource_activite = individu('aah_base_ressources_activite_eval_trimestrielle', period) - individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
-                base_ressource_hors_activite = individu('aah_base_ressources_hors_activite_eval_trimestrielle', period) + individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
+                base_ressources_activite_milieu_protege = individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
+                base_ressource_activite = individu('aah_base_ressources_activite_eval_trimestrielle', period) - base_ressources_activite_milieu_protege
+                base_ressource_hors_activite = individu('aah_base_ressources_hors_activite_eval_trimestrielle', period) + base_ressources_activite_milieu_protege
 
                 base_ressource_demandeur = assiette_revenu_activite_demandeur(base_ressource_activite) + base_ressource_hors_activite
 
@@ -119,7 +121,7 @@ class aah_deconjugalisee(Reform):
     class aah_base(Variable):
         calculate_output = calculate_output_add
         value_type = float
-        label = "Montant de l'Allocation adulte handicapé (hors complément) pour un individu, mensualisée"
+        label = 'Montant de l\'Allocation adulte handicapé (hors complément) pour un individu, mensualisée'
         entity = Individu
         reference = [
             'Article L821-1 du Code de la sécurité sociale',
@@ -148,7 +150,7 @@ class aah_deconjugalisee(Reform):
     class aah_base_deconjugalisee(Variable):
         calculate_output = calculate_output_add
         value_type = float
-        label = "Montant de l'Allocation adulte handicapé (hors complément) pour un individu, mensualisée"
+        label = 'Montant de l\'Allocation adulte handicapé (hors complément) pour un individu, mensualisée'
         entity = Individu
         reference = [
             'Article L821-1 du Code de la sécurité sociale',
