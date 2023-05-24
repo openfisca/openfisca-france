3216e8d5d8981d15ee884ec0254c4f963157c718
cgl <lallemand.chloe@gmail.com>
2023-05-24 12:02:18 +0200

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '147.0.1',
+    version = '147.1.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
