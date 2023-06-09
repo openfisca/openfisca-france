9e8aae356ea907aecd122e587e016804eacef72c
cgl <lallemand.chloe@gmail.com>
2023-06-09 10:11:15 +0200

    correction double compte bmaf

--- openfisca_france/model/prestations/minima_sociaux/rsa.py
+++ openfisca_france/model/prestations/minima_sociaux/rsa.py
@@ -827,7 +827,7 @@ class rsa_forfait_asf(Variable):
         bmaf = parameters(period).prestations_sociales.prestations_familiales.bmaf.bmaf
 
         asf_verse = famille('asf', period)
-        taux_max_par_enfant = bmaf * minima_sociaux.rsa.rsa_maj.forfait_asf.taux1
+        taux_max_par_enfant = minima_sociaux.rsa.rsa_maj.forfait_asf.taux1
 
         montant_max_retenu_rsa_par_enfant = (
             famille.members('asf_elig_enfant', period)
