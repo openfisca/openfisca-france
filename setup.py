aa08cac66f77a03d1029be5a138b567dd601600e
DURAND-GASSELIN Johan <johan.durand@soprasteria.com>
2023-07-20 11:20:36 +0200

    Màj setup.py pour pouvoir utiliser la version 41.0.0 du core

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
         },
     include_package_data = True,  # Will read MANIFEST.in
     install_requires = [
-        'OpenFisca-Core >=40.0.1, <41',
+        'OpenFisca-Core >=40.0.1, <=41.0.0',
         ],
     packages = find_namespace_packages(exclude = [
         'openfisca_france.tests*',
