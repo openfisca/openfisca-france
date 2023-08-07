d4a129c8058a6cb853bcfb9c1ae388b52f1923db
Dorine Lambinet <dorine.lambinet@gmail.com>
2023-08-07 13:31:30 +0200

    Essai de modification de la formule

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py
@@ -3526,8 +3526,8 @@ class nbptr(Variable):
         note 1 enfants et résidence alternée (formulaire 2041 GV page 10)
 
         quotient_familial.conj : nb part associées au conjoint d'un couple marié ou pacsé
-        quotient_familial.enf1 : nb part premier enfant
-        quotient_familial.enfsup6 : nb part enfants de rang 3 ou plus
+        quotient_familial.enf1 à quotient_familial.enf6 : nb part premier enfant .. jusqu'à 6ème enfant
+        quotient_familial.enfsup6 : nb part enfants de rang 7 ou plus
         quotient_familial.inv1 : nb part supp enfants invalides (I, G)
         quotient_familial.inv2 : nb part supp adultes invalides (R)
         quotient_familial.not31 : nb part supp note 3 : cases W ou G pour veuf, celib ou div
@@ -3570,12 +3570,29 @@ class nbptr(Variable):
         # # nombre de parts liées aux enfants à charge
         # que des enfants en résidence alternée
         enf1 = (no_pac & has_alt) * (quotient_familial.enf1 * min_(nbH, 2) * 0.5
-                                     + quotient_familial.enfsup6 * max_(nbH - 2, 0) * 0.5)
+                                    + quotient_familial.enf2 * min_(nbH, 2) * 0.5
+                                    + quotient_familial.enf3 * min_(nbH, 2) * 0.5
+                                    + quotient_familial.enf4 * min_(nbH, 2) * 0.5
+                                    + quotient_familial.enf5 * min_(nbH, 2) * 0.5
+                                    + quotient_familial.enf6 * min_(nbH, 2) * 0.5
+                                    + quotient_familial.enfsup6 * max_(nbH - 2, 0) * 0.5)
         # pas que des enfants en résidence alternée
         enf2 = (has_pac & has_alt) * ((nb_pac == 1) * (quotient_familial.enf1 * min_(nbH, 1) * 0.5
-            + quotient_familial.enfsup6 * max_(nbH - 1, 0) * 0.5) + (nb_pac > 1) * (quotient_familial.enfsup6 * nbH * 0.5))
+                                    + quotient_familial.enf2 * min_(nbH, 1) * 0.5
+                                    + quotient_familial.enf3 * min_(nbH, 1) * 0.5
+                                    + quotient_familial.enf4 * min_(nbH, 1) * 0.5
+                                    + quotient_familial.enf5 * min_(nbH, 1) * 0.5
+                                    + quotient_familial.enf6 * min_(nbH, 1) * 0.5
+                                    + quotient_familial.enfsup6 * max_(nbH - 1, 0) * 0.5) 
+                                    + (nb_pac > 1) * (quotient_familial.enfsup6 * nbH * 0.5))
         # pas d'enfant en résidence alternée
-        enf3 = quotient_familial.enf1 * min_(nb_pac, 2) + quotient_familial.enfsup6 * max_((nb_pac - 2), 0)
+        enf3 = quotient_familial.enf1 * min_(nb_pac, 2)
+            + quotient_familial.enf2 * min_(nb_pac, 2)
+            + quotient_familial.enf3 * min_(nb_pac, 2)
+            + quotient_familial.enf4 * min_(nb_pac, 2)
+            + quotient_familial.enf5 * min_(nb_pac, 2)
+            + quotient_familial.enf6 * min_(nb_pac, 2)
+            + quotient_familial.enfsup6 * max_((nb_pac - 2), 0)
 
         enf = enf1 + enf2 + enf3
         # # note 2 : nombre de parts liées aux invalides (enfant + adulte)
