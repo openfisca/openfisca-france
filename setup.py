43d337fb8605ff1691d7af39a73fc3bcf758587f
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2023-08-10 11:29:32 +0200

    Met à jour la version et le changelog
    Co-authored-by: Matti Schneider <matti.schneider@openfisca.fr>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '150.4.5',
+    version = '150.4.6',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
