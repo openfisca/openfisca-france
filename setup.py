65cec91bc218da2333da2203e6b4182cffa83f1c
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2023-08-18 13:03:27 +0200

    Bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '150.4.6',
+    version = '151.0.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
