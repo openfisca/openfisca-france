d561a87ebc12afbbc787b64fe4d123f55bd32b4c
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-09-01 14:52:39 +0200

    Met à jour la référence pour le repas à 1 euro et suppression de la documentation obsolete

--- openfisca_france/model/prestations/alimentation.py
+++ openfisca_france/model/prestations/alimentation.py
@@ -8,15 +8,7 @@ class crous_repas_un_euro_eligibilite(Variable):
     entity = Individu
     definition_period = MONTH
     set_input = set_input_dispatch_by_period
-    reference = 'https://www.etudiant.gouv.fr/sites/default/files/2021-01/le-repas-un-euro-4977.pdf'
-    documentation = '''
-    Suite à la crise Covid-19, tous les étudiants, boursiers ou non, peuvent bénéficier
-    de deux repas par jour au tarif de 1 euro.
-    Pour en bénéficier, il suffit d’activer son compte Izly.
-    Pour les étudiants déjà titulaires d’un compte Izly,
-    la modification de tarif a été effectuée à partir du vendredi 22 janvier
-    et jusqu’au début de la semaine du 25 janvier.
-    '''
+    reference = 'https://www.etudiant.gouv.fr/fr/crous-resto-c-est-bon-de-s-y-retrouver-1195'
 
     def formula_2021_01(individu, period):
         return individu('scolarite', period) == TypesScolarite.enseignement_superieur
