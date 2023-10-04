84da8b41b412535adbaf5f420ede8d9ca598479d
Brice Fabre <b.fabre@pse.local>
2023-10-04 18:03:31 +0200

    Actualise le changelog et le setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '153.3.5',
+    version = '154.0.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
