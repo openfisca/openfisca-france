a203b973116c8f2c115544faed7210c37a93cba0
Lola Josseran <l.josseran@pse.local>
2023-11-08 14:01:14 +0100

    log et version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '154.1.0',
+    version = '155.0.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
