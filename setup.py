d5db0d2ab2f19825a23344ce0d0e16ed65b13650
cgl <lallemand.chloe@gmail.com>
2024-01-08 14:40:37 +0100

    changelog

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '155.2.3',
+    version = '156.0.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
