38e88e2e90a07c4916c7066ea53e90b5a35a1d75
cgl <lallemand.chloe@gmail.com>
2024-01-16 14:44:39 +0100

    git gui

--- openfisca_france/model/prestations/prestations_familiales/af.py
+++ openfisca_france/model/prestations/prestations_familiales/af.py
@@ -109,10 +109,10 @@ class af_base(Variable):
             * (af_nbenf == 1)
             * af.af_maj_dom.allocations_familiales_un_enfant
             )
-        
-        # Pour chaque enfant entre nb_enf2 et nb_enf3-1 la famille reçoit un montant = au premier taux de BMAF
-        # Pour chaque enfant à partir du nb_enf3 ème enfant la famille reçoit un montant = au deuxième taux de BMAF
-        # Ex pour une famille de 4 enfants la famille reçoit 1 * taux.enf2 * BMAF (pour le 2e enfant) + 2 * taux.enf3 * BMAF (pour le 3e et le 4e enfant)
+
+        # pour chaque enfant entre nb_enf2 et nb_enf3-1 la famille reçoit un montant = au premier taux de BMAF
+        # pour chaque enfant à partir du nb_enf3 ème enfant la famille reçoit un montant = au deuxième taux de BMAF
+        # ex pour une famille de 4 enfants la famille reçoit 1 * taux.enf2 * BMAF (pour le 2e enfant) + 2 * taux.enf3 * BMAF (pour le 3e et le 4e enfant)
         deux_enfants = (af_nbenf >= nb_enf2) * (min_(af_nbenf - (nb_enf2 - 1), nb_enf3 - nb_enf2)) * af.af_cm.taux.enf2
         plus_de_trois_enfants = max_(af_nbenf - (nb_enf3 - 1), 0) * af.af_cm.taux.enf3
         taux_total = un_seul_enfant + deux_enfants + plus_de_trois_enfants
