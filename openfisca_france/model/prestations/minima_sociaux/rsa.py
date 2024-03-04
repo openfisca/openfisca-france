f40a3af51eb4fd3297048c49e01551da1103fec4
Duchesne Sylvain <sylvain.duchesne@ipp.eu>
2024-03-04 11:29:22 +0100

    ASS pour RSA dépendant des mois précédents
    # Conflicts:
    #	openfisca_france/model/prelevements_obligatoires/impot_revenu/ir.py

--- openfisca_france/model/prestations/minima_sociaux/rsa.py
+++ openfisca_france/model/prestations/minima_sociaux/rsa.py
@@ -197,11 +197,11 @@ class rsa_base_ressources_minima_sociaux(Variable):
         three_previous_months = period.last_3_months
         aspa = famille('aspa', period)
 
-        ass_i = famille.members('ass', period)
+        ass_i = famille.members('ass', three_previous_months, options = [ADD])
         aah_i = famille.members('aah', three_previous_months, options = [ADD])
         asi_i = famille.members('asi', three_previous_months, options = [ADD])
         caah_i = famille.members('caah', three_previous_months, options = [ADD])
-        return aspa + famille.sum(ass_i) + famille.sum(aah_i + asi_i + caah_i) / 3
+        return aspa + famille.sum(ass_i, aah_i + asi_i + caah_i) / 3
 
 
 class rsa_base_ressources_prestations_familiales(Variable):
