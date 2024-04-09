98c174be19bc6a1742fe4a858c3d0cbd1ca5ccd7
cgl <lallemand.chloe@gmail.com>
2024-04-09 15:34:47 +0200

    supprime les variavles cotisations_employeur_contributives et cotisations_employeur_non_contributives

--- openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/travail_totaux.py
+++ openfisca_france/model/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/travail_totaux.py
@@ -14,38 +14,16 @@ class cotisations_employeur(Variable):
     calculate_output = calculate_output_add
 
     def formula(individu, period, parameters):
-        cotisations_employeur_contributives = individu('cotisations_employeur_contributives', period)
-        cotisations_employeur_non_contributives = individu(
-            'cotisations_employeur_non_contributives', period)
-        cotisations_employeur_main_d_oeuvre = individu('cotisations_employeur_main_d_oeuvre', period)
-
-        return (
-            cotisations_employeur_contributives
-            + cotisations_employeur_non_contributives
-            + cotisations_employeur_main_d_oeuvre
-            )
-
-
-class cotisations_employeur_contributives(Variable):
-    value_type = float
-    entity = Individu
-    label = 'Cotisations sociales employeur contributives'
-    set_input = set_input_divide_by_period
-    definition_period = MONTH
-
-    def formula(individu, period, parameters):
+        # contributives
         ags = individu('ags', period)
-        agff_employeur = individu('agff_employeur', period, options = [ADD]) # ancien dispositif
-        agirc_arrco_employeur = individu('agirc_arrco_employeur', period)
-        agirc_employeur = individu('agirc_employeur', period, options = [ADD]) # ancien dispositif
-        agirc_gmp_employeur = individu('agirc_gmp_employeur', period, options = [ADD]) # ancien dispositif
+        agff_employeur = individu('agff_employeur', period, options = [ADD])
+        agirc_employeur = individu('agirc_employeur', period, options = [ADD])
+        agirc_gmp_employeur = individu('agirc_gmp_employeur', period, options = [ADD])
         apec_employeur = individu('apec_employeur', period)
-        arrco_employeur = individu('arrco_employeur', period) # ancien dispositif
+        arrco_employeur = individu('arrco_employeur', period)
         chomage_employeur = individu('chomage_employeur', period)
-        contribution_equilibre_general_employeur = individu('contribution_equilibre_general_employeur', period)
-        contribution_equilibre_technique_employeur = individu('contribution_equilibre_technique_employeur', period)
         cotisation_exceptionnelle_temporaire_employeur = individu(
-            'cotisation_exceptionnelle_temporaire_employeur', period) # ancien dispositif
+            'cotisation_exceptionnelle_temporaire_employeur', period)
         fonds_emploi_hospitalier = individu('fonds_emploi_hospitalier', period, options = [ADD])
         ircantec_employeur = individu('ircantec_employeur', period, options = [ADD])
         pension_employeur = individu('pension_employeur', period, options = [ADD])
@@ -53,39 +31,7 @@ class cotisations_employeur_contributives(Variable):
         vieillesse_deplafonnee_employeur = individu('vieillesse_deplafonnee_employeur', period, options = [ADD])
         vieillesse_plafonnee_employeur = individu('vieillesse_plafonnee_employeur', period, options = [ADD])
 
-        cotisations = (
-            # prive
-            ags
-            + agff_employeur
-            + agirc_employeur
-            + agirc_arrco_employeur
-            + agirc_gmp_employeur
-            + apec_employeur
-            + arrco_employeur
-            + chomage_employeur
-            + contribution_equilibre_general_employeur
-            + contribution_equilibre_technique_employeur
-            + cotisation_exceptionnelle_temporaire_employeur
-            + vieillesse_deplafonnee_employeur
-            + vieillesse_plafonnee_employeur
-            # public
-            + fonds_emploi_hospitalier
-            + ircantec_employeur
-            + pension_employeur
-            + rafp_employeur
-            )
-
-        return cotisations
-
-
-class cotisations_employeur_non_contributives(Variable):
-    value_type = float
-    entity = Individu
-    label = 'Cotisations sociales employeur non-contributives'
-    set_input = set_input_divide_by_period
-    definition_period = MONTH
-
-    def formula(individu, period, parameters):
+        # non contributives
         penibilite = individu('penibilite', period, options = [ADD])
         accident_du_travail = individu('accident_du_travail', period, options = [ADD])
         ati_atiacl = individu('ati_atiacl', period, options = [ADD])
@@ -95,8 +41,26 @@ class cotisations_employeur_non_contributives(Variable):
         taxe_salaires = individu('taxe_salaires', period, options = [ADD])
         forfait_social = individu('forfait_social', period, options = [ADD])
 
-        cotisations_employeur_non_contributives = (
-            ati_atiacl
+        cotisations = (
+            # contributives
+            ## prive
+            ags
+            + agff_employeur
+            + agirc_employeur
+            + agirc_gmp_employeur
+            + apec_employeur
+            + arrco_employeur
+            + chomage_employeur
+            + cotisation_exceptionnelle_temporaire_employeur
+            + vieillesse_deplafonnee_employeur
+            + vieillesse_plafonnee_employeur
+            ## public
+            + fonds_emploi_hospitalier
+            + ircantec_employeur
+            + pension_employeur
+            + rafp_employeur
+            # non contributives
+            + ati_atiacl
             + penibilite
             + accident_du_travail
             + contribution_solidarite_autonomie
@@ -106,7 +70,59 @@ class cotisations_employeur_non_contributives(Variable):
             + forfait_social
             )
 
-        return cotisations_employeur_non_contributives
+        return cotisations
+
+    def formula_2019_01_01(individu, period, parameters):
+        # contributives
+        ags = individu('ags', period)
+        agirc_arrco_employeur = individu('agirc_arrco_employeur', period)
+        apec_employeur = individu('apec_employeur', period)
+        chomage_employeur = individu('chomage_employeur', period)
+        contribution_equilibre_general_employeur = individu('contribution_equilibre_general_employeur', period)
+        contribution_equilibre_technique_employeur = individu('contribution_equilibre_technique_employeur', period)
+        fonds_emploi_hospitalier = individu('fonds_emploi_hospitalier', period, options = [ADD])
+        ircantec_employeur = individu('ircantec_employeur', period, options = [ADD])
+        pension_employeur = individu('pension_employeur', period, options = [ADD])
+        rafp_employeur = individu('rafp_employeur', period, options = [ADD])
+        vieillesse_deplafonnee_employeur = individu('vieillesse_deplafonnee_employeur', period, options = [ADD])
+        vieillesse_plafonnee_employeur = individu('vieillesse_plafonnee_employeur', period, options = [ADD])
+
+        # non contributives
+        accident_du_travail = individu('accident_du_travail', period, options = [ADD])
+        ati_atiacl = individu('ati_atiacl', period, options = [ADD])
+        contribution_solidarite_autonomie = individu('contribution_solidarite_autonomie', period)
+        famille_net_allegement = individu('famille_net_allegement', period)
+        mmid_employeur_net_allegement = individu('mmid_employeur_net_allegement', period, options = [ADD])
+        taxe_salaires = individu('taxe_salaires', period, options = [ADD])
+        forfait_social = individu('forfait_social', period, options = [ADD])
+
+        cotisations = (
+            # contributives
+            ## prive
+            ags
+            + agirc_arrco_employeur
+            + apec_employeur
+            + chomage_employeur
+            + contribution_equilibre_general_employeur
+            + contribution_equilibre_technique_employeur
+            + vieillesse_deplafonnee_employeur
+            + vieillesse_plafonnee_employeur
+            ## public
+            + fonds_emploi_hospitalier
+            + ircantec_employeur
+            + pension_employeur
+            + rafp_employeur
+            # non contributives
+            + ati_atiacl
+            + accident_du_travail
+            + contribution_solidarite_autonomie
+            + famille_net_allegement
+            + mmid_employeur_net_allegement
+            + taxe_salaires
+            + forfait_social
+            )
+
+        return cotisations
 
 
 class cotisations_salariales_contributives(Variable):
