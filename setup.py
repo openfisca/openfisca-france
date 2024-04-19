0b6188f3c5f1c442936cc288c7d0e6d5e0cd1876
Shamzic <shamery@free.fr>
2024-04-19 17:37:41 +0200

    chore: maj changelog & numéro de version du setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '164.1.4',
+    version = '164.1.5',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
