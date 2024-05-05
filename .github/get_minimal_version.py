79333051506abee23ffd2b5f725c0a1185584541
Matti Schneider <matti@openfisca.org>
2024-05-05 22:04:36 +0200

    Remove unused France version from version script
    The regexp did already not work in master

--- .github/get_minimal_version.py
+++ .github/get_minimal_version.py
@@ -1,10 +1,9 @@
 import re
 
-# This script fetches and prints the minimal versions of Openfisca-Core and Openfisca-France
-# dependencies in order to ensure their compatibility during CI testing
+# This script prints the minimal version of Openfisca-Core to ensure their compatibility during CI testing
 with open('./pyproject.toml') as file:
     for line in file:
-        version = re.search(r'(core|france)[^>]+>=\s*([\d\.]*)', line)
+        version = re.search(r'core[^>]+>=\s*([\d\.]*)', line)
         if version:
             print(f'OpenFisca-{version[1]}=={version[2]}')  # noqa: T201 <- This is to avoid flake8 print detection.
 
