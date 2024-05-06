5577ffbd9b2888d00acd805b8c44a99fc84a91df
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2024-05-06 14:46:31 +0200

    Fix build error https://github.com/openfisca/openfisca-france/actions/runs/8968992961/job/24629523261?pr=2298

--- .github/get_minimal_version.py
+++ .github/get_minimal_version.py
@@ -1,11 +1,7 @@
 import re
-
 # This script prints the minimal version of Openfisca-Core to ensure their compatibility during CI testing
 with open('./pyproject.toml') as file:
     for line in file:
-        version = re.search(r'core[^>]+>=\s*([\d\.]*)', line)
+        version = re.search(r'core\[([^\]]+)\]\s*>=\s*([\d\.]*)', line)
         if version:
-            print(f'OpenFisca-{version[1]}=={version[2]}')  # noqa: T201 <- This is to avoid flake8 print detection.
-
-        if pre:
-            print(f'{pre[2]}')  # noqa: T201 <- The same as supra.
+            print(f'Openfisca-core[{version[1]}]=={version[2]}')  # noqa: T201 <- This is to avoid flake8 print detection.
