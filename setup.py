c3e402fd919f559f82f7bed5505c4eb411fe80da
Brice Fabre <b.fabre@pse.local>
2024-06-10 18:01:41 +0200

    Actualise le CHANGELOG et le setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '166.1.5',
+    version = '166.2.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
