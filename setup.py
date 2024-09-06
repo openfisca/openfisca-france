16f9c31e1c6fada280bebbc739a068dd20568042
Dorine Lambinet <dorine.lambinet@gmail.com>
2024-09-06 11:21:42 +0200

    Change setup et changelog

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '168.0.13',
+    version = '168.0.14',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
