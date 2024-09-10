298d81deb0939e78d6c1c66cb22a22b1a2a99f80
DorineLam <46896006+DorineLam@users.noreply.github.com>
2024-09-10 14:39:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '168.0.14',
+    version = '168.0.15',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
