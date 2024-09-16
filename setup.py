c759a3638cf83d34a6a97733e22e6f1d964248ef
Martin Billard <billard.martin@imsa.msa.fr>
2024-09-16 08:25:43 +0200

    Récupération master

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = '168.0.15',
+    version = '168.0.16',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
