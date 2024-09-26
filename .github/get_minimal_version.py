6034bb9936c324693a0a638b57b6b7fb5ad55dcd
Mauko Quiroga <public@mauko.me>
2024-09-26 23:35:12 +0200

    feat: add pre-release to min version

--- .github/get_minimal_version.py
+++ .github/get_minimal_version.py
@@ -4,6 +4,14 @@ import re
 # dependencies in order to ensure their compatibility during CI testing
 with open('./setup.py') as file:
     for line in file:
+        #: Check for released versions.
         version = re.search(r'(Core|France)\s*>=\s*([\d\.]*)', line)
+
+        #: Check for pre-release versions.
+        pre = re.search(r'(Core|France)\s@\s(.*)\'\,$', line)
+
         if version:
-            print(f'Openfisca-{version[1]}=={version[2]}')  # noqa: T201 <- This is to avoid flake8 print detection.
+            print(f'OpenFisca-{version[1]}=={version[2]}')  # noqa: T201 <- This is to avoid flake8 print detection.
+
+        if pre:
+            print(f'{pre[2]}')  # noqa: T201 <- The same as supra.
