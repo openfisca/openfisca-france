4bf62024eb63581eb8b88a3806c27c1f877c9692
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2024-10-21 18:32:04 +0200

    fix lint

--- .github/get_version.py
+++ .github/get_version.py
@@ -1,31 +1,30 @@
+# Read package version in pyproject.toml and replace it in .conda/recipe.yaml
+
 import argparse
-import requests
 import logging
 import re
-# import tomli
-# Read package version in pyproject.toml and replace it in .conda/recipe.yaml
+
+logging.basicConfig(level=logging.INFO)
+
+
 def get_versions():
-    openfisca_core_api = None  
-    openfisca_france = None  
+    openfisca_core_api = None
+    openfisca_france = None
     with open('./pyproject.toml', 'r') as file:
         content = file.read()
-    
     # Extract the version of openfisca_france
     version_match = re.search(r'^version\s*=\s*"([\d.]*)"', content, re.MULTILINE)
     if version_match:
         openfisca_france = version_match.group(1)
-    
     # Extract dependencies
-    version = re.search(r'openfisca-core\[web-api\]\s*(>=\s*[\d\.]*,\s*<\d*)"',content, re.MULTILINE)
+    version = re.search(r'openfisca-core\[web-api\]\s*(>=\s*[\d\.]*,\s*<\d*)"', content, re.MULTILINE)
     if version:
         openfisca_core_api = version.group(1)
     return {
         'openfisca_france': openfisca_france,
         'openfisca_core_api': openfisca_core_api.replace(' ', ''),
-    }
+        }
 
-
-logging.basicConfig(level=logging.INFO)
 
 def replace_in_file(filepath: str, info: dict):
     '''
@@ -34,7 +33,7 @@ def replace_in_file(filepath: str, info: dict):
     '''
     with open(filepath, 'rt') as fin:
         meta = fin.read()
-    # Replace with info from PyPi
+    # Replace with info from pyproject.toml
     meta = meta.replace('888.888.888', info['openfisca_france'])
     meta = meta.replace('>=43,<44', info['openfisca_core_api'])
     with open(filepath, 'wt') as fout:
@@ -46,7 +45,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--replace', type=bool, default=False, required=False, help='replace in .conda/recipe.yaml')
     args = parser.parse_args()
-    
     info = get_versions()
     logging.info(f'Version : {info}')
     if args.replace:
