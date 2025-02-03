14989b340a11ed3489152d8c801dd26f05070961
b-michaud <b.michaud@outlook.com>
2025-02-03 10:47:58 +0100

    revert les changements d'arrondis aides logement

--- openfisca_france/model/prestations/aides_logement.py
+++ openfisca_france/model/prestations/aides_logement.py
@@ -96,7 +96,7 @@ class aide_logement_montant(Variable):
     def formula(famille, period):
         aide_logement_montant_brut = famille('aide_logement_montant_brut_crds', period)
         crds_logement = famille('crds_logement', period)
-        montant = floor(aide_logement_montant_brut + crds_logement)
+        montant = round_(aide_logement_montant_brut + crds_logement, 2)
 
         return montant
 
@@ -1411,7 +1411,7 @@ class crds_logement(Variable):
     def formula(famille, period, parameters):
         aide_logement_montant_brut = famille('aide_logement_montant_brut_crds', period)
         crds = parameters(period).prelevements_sociaux.contributions_sociales.crds.taux
-        return floor(-aide_logement_montant_brut * crds * 100) / 100
+        return -aide_logement_montant_brut * crds
 
 
 class TypesZoneApl(Enum):
