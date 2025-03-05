fix paths formula tf

openfisca_france/model/prestations/aides_logement.py
@@ -1291,34 +1291,35 @@ class aide_logement_taux_famille(Variable):
         return where(residence_dom, TF_dom, TF_metropole)
 
     def formula_2023_01_01(famille, period, parameters):
-        al = parameters(period).prestations_sociales.aides_logement.allocations_logement
+        al_tf = parameters(period).prestations_sociales.aides_logement.allocations_logement.secteur_locatif.pp_participation_personnelle.tp_taux_participation_personnelle_menage.tf_taux_selon_taille_famille
+
         couple = famille('al_couple', period)
         al_nb_pac = famille('al_nb_personnes_a_charge', period)
         residence_dom = famille.demandeur.menage('residence_dom', period)
 
         TF_metropole = (
-            al.al_loc2.tf.metropole.personnes_isolees * (not_(couple)) * (al_nb_pac == 0)
-            + al.al_loc2.tf.metropole.couples_sans_enfant * (couple) * (al_nb_pac == 0)
-            + al.al_loc2.tf.metropole.personnes_seules_couples_avec_1_enfant * (al_nb_pac == 1)
-            + al.al_loc2.tf.metropole.personnes_seules_couples_avec_2_enfants * (al_nb_pac == 2)
-            + al.al_loc2.tf.metropole.personnes_seules_couples_avec_3_enfants * (al_nb_pac == 3)
-            + al.al_loc2.tf.metropole.personnes_seules_couples_avec_4_enfants * (al_nb_pac >= 4)
-            + al.al_loc2.tf.metropole.personnes_seules_couples_avec_5_enfants * (al_nb_pac >= 5)
-            + al.al_loc2.tf.metropole.personnes_seules_couples_avec_6_enfants * (al_nb_pac >= 6)
-            + al.al_loc2.tf.metropole.variation_tf_par_enfant_supplementaire * (al_nb_pac > 6) * (al_nb_pac - 6)
+            al_tf.metropole.personnes_isolees * (not_(couple)) * (al_nb_pac == 0)
+            + al_tf.metropole.couples_sans_enfant * (couple) * (al_nb_pac == 0)
+            + al_tf.metropole.personnes_seules_couples_avec_1_enfant * (al_nb_pac == 1)
+            + al_tf.metropole.personnes_seules_couples_avec_2_enfants * (al_nb_pac == 2)
+            + al_tf.metropole.personnes_seules_couples_avec_3_enfants * (al_nb_pac == 3)
+            + al_tf.metropole.personnes_seules_couples_avec_4_enfants * (al_nb_pac >= 4)
+            + al_tf.metropole.personnes_seules_couples_avec_5_enfants * (al_nb_pac >= 5)
+            + al_tf.metropole.personnes_seules_couples_avec_6_enfants * (al_nb_pac >= 6)
+            + al_tf.metropole.variation_tf_par_enfant_supplementaire * (al_nb_pac > 6) * (al_nb_pac - 6)
             )
 
         TF_dom = (
-            al.al_loc2.tf.dom.personnes_isolees * (not_(couple)) * (al_nb_pac == 0)
-            + al.al_loc2.tf.dom.couples_sans_enfant * (couple) * (al_nb_pac == 0)
-            + al.al_loc2.tf.dom.personnes_seules_couples_avec_1_enfant * (al_nb_pac == 1)
-            + al.al_loc2.tf.dom.personnes_seules_couples_avec_2_enfants * (al_nb_pac == 2)
-            + al.al_loc2.tf.dom.personnes_seules_couples_avec_3_enfants * (al_nb_pac == 3)
-            + al.al_loc2.tf.dom.personnes_seules_couples_avec_4_enfants * (al_nb_pac == 4)
-            + al.al_loc2.tf.dom.personnes_seules_couples_avec_5_enfants * (al_nb_pac == 5)
-            + al.al_loc2.tf.dom.personnes_seules_couples_avec_6_enfants * (al_nb_pac == 6)
-            + al.al_loc2.tf.dom.personnes_seules_couples_avec_7_enfants * (al_nb_pac >= 7)
-            + al.al_loc2.tf.dom.majoration_par_pac_supplementaire * (al_nb_pac > 7) * (al_nb_pac - 7)
+            al_tf.dom.personnes_isolees * (not_(couple)) * (al_nb_pac == 0)
+            + al_tf.dom.couples_sans_enfant * (couple) * (al_nb_pac == 0)
+            + al_tf.dom.personnes_seules_couples_avec_1_enfant * (al_nb_pac == 1)
+            + al_tf.dom.personnes_seules_couples_avec_2_enfants * (al_nb_pac == 2)
+            + al_tf.dom.personnes_seules_couples_avec_3_enfants * (al_nb_pac == 3)
+            + al_tf.dom.personnes_seules_couples_avec_4_enfants * (al_nb_pac == 4)
+            + al_tf.dom.personnes_seules_couples_avec_5_enfants * (al_nb_pac == 5)
+            + al_tf.dom.personnes_seules_couples_avec_6_enfants * (al_nb_pac == 6)
+            + al_tf.dom.personnes_seules_couples_avec_7_enfants * (al_nb_pac >= 7)
+            + al_tf.dom.majoration_par_pac_supplementaire * (al_nb_pac > 7) * (al_nb_pac - 7)
             )
 
         return where(residence_dom, TF_dom, TF_metropole)