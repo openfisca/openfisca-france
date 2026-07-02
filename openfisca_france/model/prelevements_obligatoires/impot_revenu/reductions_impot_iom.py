b25f221a7fe1fb0c56b5f2ec14627025a808d483
Duchesne Sylvain <sylvain.duchesne@ipp.eu>
2026-07-02 13:35:42 +0200

    Modifie test

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot_iom.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot_iom.py
@@ -2106,7 +2106,8 @@ def formula_2025_01_01(foyer_fiscal, period, parameters):
     r_66_306_594 = nr_66_306_594 / (1 - P15.taux_retro_1) * P15.taux_retro_1
     r_56_306_38945 = nr_56_306_38945 / (1 - P15.taux_retro_2) * P15.taux_retro_2
 
-    inv = (nr_66_306_594 + nr_56_306_38945 + r_66_306_594 + r_56_306_38945)
+    inv = (r_66_306_594 + r_56_306_38945 + r_66_306_594 + r_56_306_38945 +
+        nr_66_306_594 + nr_56_306_38945 + nr_66_306_594 + nr_56_306_38945)
 
     fhgu = foyer_fiscal('fhgu', period)  # Report 2020
     fhhu = foyer_fiscal('fhhu', period)  # Report 2021
