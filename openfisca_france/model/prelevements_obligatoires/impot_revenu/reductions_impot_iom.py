4849d2fc62e7db369396598788f2559b91d27734
hernandezbaptiste77170-sys <hernandez.baptiste77170@gmail.com>
2026-07-07 09:29:06 +0200

    Update openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot_iom.py
    Co-authored-by: Sylvain Duchesne <121864904+sylvainipp@users.noreply.github.com>

--- openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot_iom.py
+++ openfisca_france/model/prelevements_obligatoires/impot_revenu/reductions_impot_iom.py
@@ -3596,7 +3596,7 @@ class domlog(Variable):
 
         ri_apres_2011 = (fhos + fhot + fhou + fhov + fhow + fhog + fhox + fhoy + fhoz
                         + fhud + fhue + fhuf + fhug + fhuk + fhul + fhum + fhun + fhur
-                        + fhus + fhut + fhuu + fhvd + fhve + fhvf + fhvg + fhvh + fhvi
+                        + fhus + fhut + fhuu + fhvd + fhve + fhvf + fhvg
                         + fhvj + fhvk + fhvl + fhvm + fhvn)
 
         P2010 = parameters('2010-01-01').impot_revenu.calcul_reductions_impots.outremer_investissement
